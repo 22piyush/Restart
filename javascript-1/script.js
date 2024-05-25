73aeddaf0a0e6147ee47c5b1030653df6533c911
22piyush <piyushaglawe05@gmail.com>
2024-05-25 12:37:00 +0530

    done

--- javascript-1/script.js
+++ javascript-1/script.js
@@ -65,4 +65,103 @@
 
 
 
-//4)---------------
+//4)---------------unordered list---------
+
+// let add = document.querySelector("#add");
+// let remove = document.querySelector("#remove");
+// let inp = document.querySelector("input");
+// let ul = document.querySelector('ul');
+
+
+// var li;
+// add.addEventListener("click",function(){
+
+//     if(inp.value.trim() === ''){}
+//     else{
+//         li = document.createElement("li");
+//         li.textContent = inp.value;
+//         ul.appendChild(li);
+//         inp.value = "";
+
+//     }
+// })
+
+// remove.addEventListener("click", function(){
+      
+//     ul.removeChild(li)
+    
+// })
+
+
+
+
+
+
+
+
+// 5)counter timer-----------------
+// var start = document.querySelector("#start");
+// var stop = document.querySelector("#stop");
+// var h4 = document.querySelector("h3")
+
+// var int;
+// start.addEventListener("click", function(){
+//     var count = 0;
+//     int = setInterval(function(){
+//         h4.textContent = count;
+//         count++;
+//     },100);
+// })
+
+// stop.addEventListener("click", function(){
+    
+//     clearInterval(int)
+// })
+
+
+
+// var home = document.querySelector('#home');
+// var about = document.querySelector('#about');
+// var contact = document.querySelector('#contact');
+
+// var hometext = document.querySelector('#hometext');
+// var abouttext = document.querySelector('#abouttext');
+// var contacttext = document.querySelector('#contacttext');
+
+
+// hometext.style.display = "block";
+// hometext.style.width = "50%"
+
+// home.addEventListener("click", function(){
+//     home.style.display = "block";
+//     home.style.width = "50%";
+
+// })
+
+// about.addEventListener("click", function(){
+//     home.style.display = "block";
+//     home.style.width = "50%";
+    
+// })
+
+// contact.addEventListener("click", function(){
+//     home.style.display = "block";
+//     home.style.width = "50%";   
+// })
+
+// function saaretexthatao(){
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
