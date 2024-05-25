82a5d09a5e8f9001d0257ec3fa3d018753d2d928
22piyush <piyushaglawe05@gmail.com>
2024-05-25 11:19:28 +0530

    done

---
+++ javascript-1/script.js
@@ -0,0 +1,68 @@
+//1)text cploore
+// let p = document.querySelector('body');
+// let btn = document.querySelector('button');
+
+// btn.addEventListener("click", function(){
+
+//     p.style.backgroundColor = "black";
+//     p.textContent = "hello piyush aglawe"
+
+// })/
+
+
+//2) IMAGES swapppp----------------
+
+
+// let img1 = document.querySelector('#img1');
+// let img2 = document.querySelector('#img2');
+// let btn = document.querySelector('button');
+
+// btn.addEventListener("click",function(){
+
+//     let src1 = img1.src;
+//     let src2 = img2.src;
+
+//     img1.src=src2;
+//     img2.src=src1;
+          
+// })
+
+
+
+
+
+
+//3) Imput fields empty or not
+
+
+// let form = document.querySelector("form");
+// let inps = document.querySelectorAll('input[type="text"]');
+// let h4 = document.querySelector("h4");
+
+// form.addEventListener("submit", function(ev)
+// {
+
+//     ev.preventDefault();
+
+//     for(let i=0; i<inps.length; i++)
+//         {
+//             if(inps[i].value.trim() === ' ' )
+//             {
+
+//                 h4.textContent = "Error, Some fields are blanks";
+//                 h4.style.color = "red";
+//                 break;
+
+//             }
+            
+//         }
+
+
+// })
+
+
+
+
+
+
+//4)---------------
