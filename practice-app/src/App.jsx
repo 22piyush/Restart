30ccc9aa8c8c9d36ed0b9d912a8b9c8d6ed05618
22piyush <piyushaglawe05@gmail.com>
2025-07-14 23:15:37 +0530

    developing new concepts

--- practice-app/src/App.jsx
+++ practice-app/src/App.jsx
@@ -1,14 +1,34 @@
 import React from 'react'
+import { Route , Routes} from 'react-router-dom'
+import Home from './components/Home'
+import About from './components/About'
+import Labs from './components/Labs'
 
 function App() {
   return (
     <div>
 
+      {/* <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+             <li>
+              <Link to="/about">About</Link>
+            </li>
+             <li>
+              <Link to="/labs">Labs</Link>
+            </li>
+          </ul>
+        </nav>
+      </div> */}
+
       <Routes>
-        <Route path="/" element={<div>Home Page</div>}></Route>
-        <Route path="/" element={<div>Support Page</div>}></Route>
-        <Route path="/" element={<div>About Page</div>}></Route>
-        <Route path="/" element={<div>Labs Page</div>}></Route>
+        <Route path="/" element={<Home/>}></Route>
+        <Route path="/about" element={<About/>}></Route>
+        <Route path="/labs" element={<Labs/>}></Route>
+        <Route path="*" element={<div>Not Found</div>}></Route>
       </Routes>
 
     </div>
