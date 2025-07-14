4c91b19d95be442ed4fa0dfb918e268ab59085bf
22piyush <piyushaglawe05@gmail.com>
2025-07-14 23:41:42 +0530

    developing new concepts

--- practice-app/src/components/About.jsx
+++ practice-app/src/components/About.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
 import Navbar from './Navbar'
+import { useNavigate } from 'react-router-dom'
 
 function About() {
+    const navigate = useNavigate();
   return (
     <div>
         {/* <Navbar/> */}
+        <button onClick={()=>{navigate(-1) }}>Go Back page</button>
         About
+
     </div>
   )
 }
