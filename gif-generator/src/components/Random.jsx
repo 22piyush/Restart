79cb96e526b81f849d42ddcb4326969abd8fda9c
22piyush <piyushaglawe05@gmail.com>
2025-07-17 23:24:46 +0530

    developing new concepts

--- gif-generator/src/components/Random.jsx
+++ gif-generator/src/components/Random.jsx
@@ -1,33 +1,14 @@
-import React, { useEffect, useState } from 'react';
-import axios from 'axios';
-import CircularProgress from '@mui/material/CircularProgress';
 
-const API_KEY = import.meta.env.VITE_GIPHY_API_KEY;
+import CircularProgress from '@mui/material/CircularProgress';
+import useGif from '../hooks/useGif';
 
 function Random() {
-  const [gif, setGif] = useState('');
-  const [loading , setLoading]  = useState(true);
 
-  async function clickHandler() {
-    setLoading(true);
-    const url = `https://api.giphy.com/v1/gifs/random?api_key=${API_KEY}`;
-    try {
-      const { data } = await axios.get(url);
-      const imageUrl = data.data.images.downsized_large.url;
-      setGif(imageUrl);
-      setLoading(false);
-    } catch (error) {
-      console.error('Error fetching random GIF:', error);
-    }
-  }
-
-  useEffect(()=>{
-    clickHandler();
-  },[])
+  const { gif, loading, fetchGif } = useGif(); 
 
   return (
     <div className="bg-white w-80 p-4 rounded-xl shadow-lg flex flex-col items-center">
-      <h2 className="text-xl font-bold mb-4">Tag GIF</h2>
+      <h2 className="text-xl font-bold mb-4">Random GIF</h2>
       
       <div className='w-64 h-48 flex items-center justify-center'>
         {
@@ -38,11 +19,8 @@ function Random() {
           )
         }
       </div>
-
-
-
       <button
-        onClick={clickHandler}
+        onClick={()=>fetchGif()}
         className=" cursor-pointer bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600 transition"
       >
         Generate
--- gif-generator/src/components/Tag.jsx
+++ gif-generator/src/components/Tag.jsx
@@ -1,35 +1,18 @@
-import React, { useEffect, useState } from 'react';
-import axios from 'axios';
-import CircularProgress from '@mui/material/CircularProgress';
 
-const API_KEY = import.meta.env.VITE_GIPHY_API_KEY;
+import CircularProgress from '@mui/material/CircularProgress';
+import useGif from '../hooks/useGif';
+import { useState } from 'react';
 
 function Tag() {
 
   const [tag, setTag] = useState('car');
-  const [gif, setGif] = useState('');
-  const [loading , setLoading]  = useState(true);
 
-  async function handleClick(){
-    setLoading(true);
-    const url = `https://api.giphy.com/v1/gifs/random?api_key=${API_KEY}&tag=${tag}`;
-    try {
-      const { data } = await axios.get(url);
-      const imageUrl = data.data.images.downsized_large.url;
-      setGif(imageUrl);
-      setLoading(false);
-    } catch (error) {
-      console.error('Error fetching random GIF:', error);
-    }
-  };
+  const { gif, loading, fetchGif } = useGif(tag); // dynamic tag
 
-  useEffect(()=>{
-    handleClick();
-  },[])
 
   return (
     <div className="bg-white w-80 p-4 rounded-xl shadow-lg flex flex-col items-center">
-      <h2 className="text-xl font-bold mb-4">Tag GIF</h2>
+      <h2 className="text-xl font-bold mb-4">Search GIF</h2>
       <div className='w-64 h-48 flex items-center justify-center'>
         {
           loading ? (
@@ -43,11 +26,11 @@ function Tag() {
         type="text"
         value={tag}
         onChange={(e) => setTag(e.target.value)}
-        placeholder="Enter a tag"
+        placeholder="Enter your GIF"
         className="w-full p-2 mb-3 border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-blue-400"
       />
       <button
-        onClick={handleClick}
+        onClick={()=>fetchGif()}
         className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600 transition"
       >
         Generate
