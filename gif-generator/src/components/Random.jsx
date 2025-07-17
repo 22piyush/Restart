dfbc5237f4bdce6e016d8fa5068f5812b5840f85
22piyush <piyushaglawe05@gmail.com>
2025-07-17 22:51:51 +0530

    developing new concepts

--- gif-generator/src/components/Random.jsx
+++ gif-generator/src/components/Random.jsx
@@ -1,35 +1,49 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import axios from 'axios';
+import CircularProgress from '@mui/material/CircularProgress';
 
 const API_KEY = import.meta.env.VITE_GIPHY_API_KEY;
-console.log(API_KEY);
-
 
 function Random() {
   const [gif, setGif] = useState('');
+  const [loading , setLoading]  = useState(true);
 
   async function clickHandler() {
-      const url = `https://api.giphy.com/v1/gifs/random?api_key=${API_KEY}`;
-console.log(API_KEY);
-      try {
-        const { data } = await axios.get(url);
-        const imageUrl = data.data.images.downsized_large.url;
-        setGif(imageUrl);
-      } catch (error) {
-        console.error('Error fetching random GIF:', error);
-      }
+    setLoading(true);
+    const url = `https://api.giphy.com/v1/gifs/random?api_key=${API_KEY}`;
+    try {
+      const { data } = await axios.get(url);
+      const imageUrl = data.data.images.downsized_large.url;
+      setGif(imageUrl);
+      setLoading(false);
+    } catch (error) {
+      console.error('Error fetching random GIF:', error);
     }
+  }
+
+  useEffect(()=>{
+    clickHandler();
+  },[])
+
   return (
     <div className="bg-white w-80 p-4 rounded-xl shadow-lg flex flex-col items-center">
       <h2 className="text-xl font-bold mb-4">Tag GIF</h2>
-      <img
-        src={gif}
-        alt="Tag Gif"
-        className="w-64 h-48 object-cover rounded mb-4"
-      />
+      
+      <div className='w-64 h-48 flex items-center justify-center'>
+        {
+          loading ? (
+            <CircularProgress />
+          ) : (
+            <img src={gif} alt="Tag Gif" className="w-64 h-48 object-cover rounded mb-4" />
+          )
+        }
+      </div>
+
+
+
       <button
         onClick={clickHandler}
-        className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600 transition"
+        className=" cursor-pointer bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600 transition"
       >
         Generate
       </button>
