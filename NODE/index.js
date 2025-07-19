64e5eb19733462e3a8d2a815061cb96f92241c07
22piyush <piyushaglawe05@gmail.com>
2025-07-19 21:14:41 +0530

    developing new concepts

--- NODE/index.js
+++ NODE/index.js
@@ -1,5 +1,15 @@
 const http = require('http');
 
+const PORT = 3009;
+
 const server = http.createServer(function listener(){
-    
+
+
+
 })
+
+server.listen(PORT, function exec(){
+
+    console.log(`Server is Running on PORT: ${PORT}`);
+    
+});
