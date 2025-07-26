ae61cc0af6598c4d077b06b24cb8203756e3658f
22piyush <piyushaglawe05@gmail.com>
2025-07-26 10:28:53 +0530

    developing new concepts

--- practice-app/src/App.jsx
+++ practice-app/src/App.jsx
@@ -1,27 +1,9 @@
-import React, { useState } from 'react'
-import Navbar from './components/Navbar'
-import { Route, Routes } from 'react-router-dom'
-import Home from './pages/Home'
-import Login from './pages/Login'
-import Signup from './pages/Signup'
-import Dashboard from './pages/Dashboard'
 
 function App() {
 
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
-
   return (
     <div>
-      <Navbar isLoggedIn={isLoggedIn} setIsLoggedIn={setIsLoggedIn}/>
 
-      <Routes>
-
-        <Route path='/' element={<Home/>} />
-        <Route path='/login' element={<Login/>} />
-        <Route path='/signup' element={<Signup/>} />
-        <Route path='/dashboard' element={<Dashboard/>} />
-
-      </Routes>
     </div>
   )
 }
