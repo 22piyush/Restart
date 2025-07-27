1e4749332b58d2a1af4f26f56366f2f83568a3f3
22piyush <piyushaglawe05@gmail.com>
2025-07-27 22:28:36 +0530

    developing new concepts

--- JAVASCRIPT/object.js
+++ JAVASCRIPT/object.js
@@ -1,2 +1,31 @@
+let obj = {
+    name : "Piyush",
+    age : 23,
+    social: {
+        facebook:{
+            ac1:"cbu@gmail.com",
+            ac2:"pbu@gmail.com"
+        },
+        twitter: {
+            free: {
+                ac1:"ksdbcd@gmail.com"
+            },
+            paid:{
+                ac1:"skdvncds@gamcm.com"
+            }
+        }
+    }
+}
+
+function makeDeepCopy(obj){
+
+    if(typeof obj !== 'object' || obj === null){
+        return obj;
+    }
+
+    Array.isArray(obj);
+
+}
+
 
 
