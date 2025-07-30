f5dbb5e757674de734d2ee84995795ec7827fe5b
22piyush <piyushaglawe05@gmail.com>
2025-07-30 21:32:56 +0530

    Done

---
+++ Flight-Booking-System/src/index.js
@@ -0,0 +1,8 @@
+const express = require('express');
+
+const { ServerConfig } = require('./config')
+const app = express();
+
+app.listen(ServerConfig.PORT, ()=>{
+    console.log(`Successfully started the server on PORT : ${ServerConfig.PORT}`); 
+})
