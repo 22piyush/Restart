005ab44c7df98b444bae2cb95d96aea966eba594
22piyush <piyushaglawe05@gmail.com>
2025-07-30 21:03:46 +0530

    New Changes Added

--- Flight-Booking-System/src/config/index.js
+++
@@ -1,7 +0,0 @@
-const Logger = require("./logger-config");
-const ServerConfig = require('./server-config')
-
-module.exports = {
-    Logger, 
-    ServerConfig
-}
--- Flight-Booking-System/src/index.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const {ServerConfig} = require('./config');
-
-const app = express();
-
-app.listen(ServerConfig.PORT, ()=> {
-     console.log(`Successfully started the server on PORT : ${ServerConfig.PORT}`);
-})
