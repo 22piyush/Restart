9b368f965cc6063816ee9573427fdc635c6e7ff9
22piyush <piyushaglawe05@gmail.com>
2025-08-01 22:32:10 +0530

    done

--- JAVASCRIPT/object.js
+++ JAVASCRIPT/object.js
@@ -1,5 +1,12 @@
-function User() {}
+function User(name , age) {
+    (this.name = name),
+    (this.age = age);
+}
 
-const user = new User();
+const user1 = new User("prakash", 101);
+const user2 = new User("ashish", 111);
 
-console.log(user);
+console.log(user1);
+console.log(user2);
+
+
