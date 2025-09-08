ab8df135f9030cd098fbbba76f4aa229c3b38834
22piyush <piyushaglawe05@gmail.com>
2025-09-08 21:38:57 +0530

    done

---
+++ practice-app/src/newState/New.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function New() {
+
+    function changeHandler(){
+
+    }
+
+  return (
+    <div>
+        <input type='text' onClick={changeHandler}/>
+    </div>
+  )
+}
+
+export default New
