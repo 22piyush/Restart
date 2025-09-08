60935cddf0563b8a4e3d7d6d5223f4ba783f99dd
22piyush <piyushaglawe05@gmail.com>
2025-09-08 21:53:12 +0530

    done

--- practice-app/src/newState/New.jsx
+++ practice-app/src/newState/New.jsx
@@ -1,14 +1,22 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 function New() {
 
-    function changeHandler(){
+    const [text , setText] = useState('');
 
+    useEffect(() => {
+        console.log("Render"); 
+    },[])
+
+    function changeHandler(event){
+        console.log(event.target.value);
+        setText(event.target.value);  
     }
 
   return (
-    <div>
-        <input type='text' onClick={changeHandler}/>
+    <div style={{display:'flex' , justifyContent:'center'}}>
+        <input type='text' onChange={changeHandler}/>
+        <div>{text}</div>
     </div>
   )
 }
