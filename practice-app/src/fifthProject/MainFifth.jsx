35cc6df8c05b1b23017d0f0fc7f98d78eae68b32
22piyush <piyushaglawe05@gmail.com>
2025-09-11 21:52:36 +0530

    done

--- practice-app/src/fifthProject/MainFifth.jsx
+++ practice-app/src/fifthProject/MainFifth.jsx
@@ -1,13 +1,34 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 function MainFifth() {
+
+    const [formData , setFormData] = useState({
+        firstName:'',
+        lastName:'',
+        email:''
+    })
+
+    const handleFormData = (event) =>{
+        setFormData(prevData => ({
+
+            ...prevData,
+            [event.target.name]:event.target.value
+
+        }))
+    }
+
   return (
     <div>
         <div>
-            
+
+            <input type="text" value={formData.firstName} placeholder='Enter Your First Name' name="firstName" id="firstName" onChange={handleFormData}/><br/><br/>
+            <input type="text" value={formData.lastName} placeholder='Enter Your Last Name' name="lastName" id="lastName" onChange={handleFormData}/><br/><br/>
+            <input type="email" value={formData.email} placeholder='Enter Your Email' name="email" id="email" onChange={handleFormData}/><br/><br/>
+
         </div>
     </div>
   )
 }
+
 
 export default MainFifth
