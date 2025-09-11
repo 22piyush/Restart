bda752f8b55469e9ece6937d7e858078fd772c41
22piyush <piyushaglawe05@gmail.com>
2025-09-11 22:12:13 +0530

    done

--- practice-app/src/fifthProject/MainFifth.jsx
+++ practice-app/src/fifthProject/MainFifth.jsx
@@ -5,7 +5,9 @@ function MainFifth() {
     const [formData , setFormData] = useState({
         firstName:'',
         lastName:'',
-        email:''
+        email:'',
+        comments:'',
+        isVisible:true
     })
 
     const handleFormData = (event) =>{
@@ -17,6 +19,16 @@ function MainFifth() {
         }))
     }
 
+    const handleFormSubmit = () =>{
+        console.log(formData);
+        setFormData({
+            firstName: '',
+            lastName: '',
+            email: '',
+            comments: ''
+        }) 
+    }
+
   return (
     <div>
         <div>
@@ -24,7 +36,12 @@ function MainFifth() {
             <input type="text" value={formData.firstName} placeholder='Enter Your First Name' name="firstName" id="firstName" onChange={handleFormData}/><br/><br/>
             <input type="text" value={formData.lastName} placeholder='Enter Your Last Name' name="lastName" id="lastName" onChange={handleFormData}/><br/><br/>
             <input type="email" value={formData.email} placeholder='Enter Your Email' name="email" id="email" onChange={handleFormData}/><br/><br/>
+            <textarea name="comments" value={formData.comments} placeholder='Enter Your Comments' id="comments" onChange={handleFormData}></textarea><br/><br/>
 
+            <input type="checkbox" name="isVisible" id="isVisible" onChange={handleFormData} checked={formData.isVisible}/>
+
+
+            <button onClick={handleFormSubmit}>Submit</button>
         </div>
     </div>
   )
