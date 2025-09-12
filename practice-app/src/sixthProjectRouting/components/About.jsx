86a9413d5fdf108527a0b1cf332701e4972d154d
22piyush <piyushaglawe05@gmail.com>
2025-09-12 22:59:27 +0530

    done

--- practice-app/src/sixthProjectRouting/components/About.jsx
+++ practice-app/src/sixthProjectRouting/components/About.jsx
@@ -1,8 +1,23 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
+
 
 function About() {
+
+  const navigate = useNavigate();
+
+  function clickHandler(){
+    navigate('/')
+  }
+
   return (
-    <div>About</div>
+    <div>
+      About
+
+      <button onClick={clickHandler}>Move to Home Page</button>
+       <button onClick={() => navigate(-1)}>Go Back</button>
+
+    </div>
   )
 }
 
