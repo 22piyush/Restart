b3ac5776a72155efd6fb73e2eeee7c444e7601bc
22piyush <piyushaglawe05@gmail.com>
2025-09-12 21:43:00 +0530

    done

--- practice-app/src/main.jsx
+++ practice-app/src/main.jsx
@@ -3,6 +3,7 @@ import App from './firstProject/App.jsx'
 import New from './secondProject/New.jsx'
 import ThirdMain from './thirdProject/ThirdMain.jsx'
 import MainFifth from './fifthProject/MainFifth.jsx'
+import MainSixth from './sixthProjectRouting/MainSixth.jsx'
 
-createRoot(document.getElementById('root')).render(<MainFifth/>)
+createRoot(document.getElementById('root')).render(<MainSixth/>)
 
---
+++ practice-app/src/sixthProjectRouting/MainSixth.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function MainSixth() {
+  return (
+    <div>MainSixth</div>
+  )
+}
+
+export default MainSixth
