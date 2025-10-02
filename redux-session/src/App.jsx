116ecb875fb44e707d2dfdc4cb89c6307006df6d
22piyush <piyushaglawe05@gmail.com>
2025-10-02 10:44:27 +0530

    done

--- redux-session/src/App.jsx
+++ redux-session/src/App.jsx
@@ -1,19 +1,8 @@
 import React from 'react'
-import Count from './components/Count'
-import { useDispatch } from 'react-redux'
 
 function App() {
-
-    const dispatch = useDispatch()
-
   return (
-    <div style={{display:'flex',justifyContent:'center',alignItems:'center'}}>
-        <div>
-      <button onClick={() => dispatch(increment())}>Increment</button>
-      <h2>{count}</h2>
-      <button onClick={() => dispatch(decrement())}>Decrement</button>
-        </div>
-    </div>
+    <div>App</div>
   )
 }
 
