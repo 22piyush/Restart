5b697a5ebddb635219254281037295c53d10b890
22piyush <piyushaglawe05@gmail.com>
2025-10-02 11:49:39 +0530

    done

--- redux-session/src/components/Todos.jsx
+++ redux-session/src/components/Todos.jsx
@@ -1,16 +1,26 @@
-import React from 'react'
-import { useSelector , useDispatch } from 'react-redux'
+import React from "react";
+import { useSelector, useDispatch } from "react-redux";
 import { removeTodo } from "../features/todo/todoSlice";
 
 function Todos() {
-
-    useSelector(state => )
+  const todos = useSelector((state) => state.todo.todos);
+  const dispatch = useDispatch();
 
   return (
     <div>
-
+      <h2>Todo List</h2>
+      <ul>
+        {todos.map((todo) => (
+          <li key={todo.id}>
+            {todo.text}{" "}
+            <button onClick={() => dispatch(removeTodo({ id: todo.id }))}>
+              ❌ Remove
+            </button>
+          </li>
+        ))}
+      </ul>
     </div>
-  )
+  );
 }
 
-export default Todos
+export default Todos;
