2d7dddc8c4dff0e85ebd95ad840c6e56bf6dd673
22piyush <piyushaglawe05@gmail.com>
2025-10-29 23:25:14 +0530

    done

--- BabbarBackend/server.js
+++ BabbarBackend/server.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const app = express();
 
-app.listen(4000, () => {
+app.listen(5000, () => {
     console.log("Server Started At 4000");
     
+})
+
+app.get('/',(req,res) => {
+    res.send("Hello jee, kaise ho saare")
 })
