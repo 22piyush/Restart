f39307f47f8c2fdd77d1e39657ce8e370077aa8a
22piyush <piyushaglawe05@gmail.com>
2025-10-30 22:26:28 +0530

    done

---
+++ BabbarBackend/One/server.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const app = express();
+
+const bodyParser = require('body-parser');
+
+// Specifically Parse JSON data & add to request.Body Object 
+app.use(bodyParser.json());
+
+// Middleware to parse JSON request bodies
+app.use(express.json());
+
+app.listen(5000, () => {
+    console.log("Server started at port 5000");
+});
+
+app.get('/', (res) => {
+    res.send("Hello jee, kaise ho saare");
+});
+
+app.post('/api/cars', (req, res) => {
+    const { name, brand } = req.body;
+    console.log(name);
+    console.log(brand);
+    
+    res.send("Car Submitted");
+});
