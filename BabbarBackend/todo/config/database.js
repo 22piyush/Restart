65e020161e235258ae79b125e7ccaa10f66a59dd
22piyush <piyushaglawe05@gmail.com>
2025-11-03 22:59:00 +0530

    done

--- BabbarBackend/todo/config/database.js
+++ BabbarBackend/todo/config/database.js
@@ -1,17 +1,17 @@
 const mongoose = require("mongoose");
-require("dotenv") .config();
+require("dotenv").config();
 
-const dbConnect =() => {
-    mongoose.connect(process.env.DATABASE_URL, {
-        useNewUrlParser:true,
-        useUnifiedTopology:true,
-    })
-    .then(() => console.log("DB Connection Successful"))
-    .catch((error) => {
-        console.log("Issue in DB Connection");
-        console.error(error.message);
-        process.exit(1);
-    })
-}
+const dbConnect = () => {
+  mongoose.connect(process.env.DATABASE_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("✅ DB Connection Successful"))
+  .catch((error) => {
+    console.log("❌ Issue in DB Connection");
+    console.error(error.message);
+    process.exit(1);
+  });
+};
 
-module.exportsc = dbConnect;
+module.exports = dbConnect;
