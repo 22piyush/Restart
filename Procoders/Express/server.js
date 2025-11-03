5be260a35677e0edc7ec2ed8e0f628c3b1260ace
22piyush <piyushaglawe05@gmail.com>
2025-11-03 22:24:06 +0530

    done

--- Procoders/Express/server.js
+++ Procoders/Express/server.js
@@ -2,12 +2,17 @@ import express from 'express';
 
 const app = express();
 
-app.get('/', (req, res) => {
+app.get('/',
+  (req, res, next) => {
+    res.write("Hello World1\n");
+    next();
+  },
+  (req, res, next) => {
+    res.write("Hello World2\n");
+    res.end();
+  }
+);
 
-    res.send("Hello World");
+app.listen(4000, () => {
+  console.log(`Server running on port 4000`);
 });
-
-app.listen(4000 , ()=>{
-    console.log(`Running Port 3000`);
-    
-})
