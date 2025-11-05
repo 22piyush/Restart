91b77dce0a64b975c98c14b42e11e278fa59827c
22piyush <piyushaglawe05@gmail.com>
2025-11-05 21:12:52 +0530

    done

--- BabbarBackend/todo/controllers/getTodo.js
+++ BabbarBackend/todo/controllers/getTodo.js
@@ -2,7 +2,14 @@ const Todo = require("../models/Todo");
 
 exports.getTodo = async(req , res) => {
     try {
-        
+
+        const todos = await Todo.find({});
+
+        res.status(200).json({
+            success:true,
+            data:todos,
+            message:"Entire Todo data is fetched."
+        })
     }
     catch(err){
         console.error(err);
