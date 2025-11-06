a157dc7c2cb9d5ece253c47ce7255746c5c4fa14
22piyush <piyushaglawe05@gmail.com>
2025-11-06 23:24:13 +0530

    done

--- BabbarBackend/blogapp/controllers/postController.js
+++ BabbarBackend/blogapp/controllers/postController.js
@@ -25,8 +25,16 @@ exports.getAllPosts = async (req,res) => {
     try{
 
         const posts = await Post.find().populate("likes").populate("comments").exec();
+        res.json({
+            posts,
+        })
+         
 
     }catch(error){
+
+        return res.status(400).json({
+            error: "Error while creating post",
+        })
 
     }
 
--- BabbarBackend/blogapp/routes/blog.js
+++ BabbarBackend/blogapp/routes/blog.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const { createComment } = require("../controllers/commentController");
-const { createPost } = require("../controllers/postController");
+const { createPost , getAllPosts } = require("../controllers/postController");
 const router = express.Router();
 
-router.post("/comments/create", createComment)
-router.post("/posts/create", createPost)
+router.post("/comments/create", createComment);
+router.post("/posts/create", createPost);
+router.get("/posts", getAllPosts);
 
 module.exports = router;
