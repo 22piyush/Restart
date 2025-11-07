9dd3294f0bb1ce0709a37f0801a45a7183077bcd
22piyush <piyushaglawe05@gmail.com>
2025-11-07 21:36:21 +0530

    done

---
+++ practice Node js/server.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const app = express();
+
+require("dotenv").config();
+const PORT = process.env.PORT || 4000;
+
+app.use(express.json());
+
+
+app.listen(PORT, () => {
+    console.log(`Server started at ${PORT}`);
+})
+
+app.get("/", (req,res) => {
+    res.send(`<h1>Home Page</h1>`);
+})
