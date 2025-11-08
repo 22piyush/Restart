3d7ccc753baea3f2499d261c7c33a8f19d6cc6c0
22piyush <piyushaglawe05@gmail.com>
2025-11-08 22:19:01 +0530

    done

--- practice-app/src/exportsLearn/ClassComponent.jsx
+++ practice-app/src/exportsLearn/ClassComponent.jsx
@@ -2,8 +2,28 @@ import React from "react";
 
 class ClassComponent extends React.Component{
 
+
+    constructor(){
+        super();
+        this.state = {
+            count : 0,
+
+        }
+    }
+
+    increment(){
+        this.setState(() => {
+            count: this.state.count + 1,
+        });
+    }
+
     render(){
-        return <h1>Class Components</h1>
+        return (
+            <div>
+                <button onClick={increment}>Click Me</button>
+                <p>{this.state.count}</p>
+            </div>
+        )
     }
 
 }
