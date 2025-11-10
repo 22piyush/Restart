8e64dbee7f7d9f6fff0a45d8214d9762fd29ca63
22piyush <piyushaglawe05@gmail.com>
2025-11-10 16:26:59 +0530

    done

--- BabbarBackend/blogapp/controllers/likeController.js
+++ BabbarBackend/blogapp/controllers/likeController.js
@@ -1,4 +1,32 @@
+const Post = require("../models/postModel");
+const Like = require("../models/likeModel");
 
-exports.dummyLink = (req,res) => {
-    res.send("This is Dummy Page")
-}
+
+exports.likePost = async (req , res) => {
+
+    try{
+
+        const {post, user} = req.body;
+        const like = new Like({
+            post, user,
+        });
+        const savedLike = await like.save();
+
+        const updatedPost = await Post.findByIdAndUpdate(post, {$push: {likes: savedLike._id}}, {new: true})
+                          .populate("likes")
+                          .exec();
+        
+        res.json({
+            post: updatedPost,
+        })                  
+
+    }catch(error){
+
+        console.log(error);
+        return res.status(500).json({
+            error: "Error while creating like",
+        })
+
+    }
+
+};
--- BabbarBackend/blogapp/routes/blog.js
+++ BabbarBackend/blogapp/routes/blog.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 
 // Import Controller 
-const {dummyLink} = require("../controllers/LikeController");
+const {likePost} = require("../controllers/LikeController");
 const {createComment} = require("../controllers/commentController");
 const { createPost , getAllPosts} = require("../controllers/PostController");
 
@@ -10,10 +10,11 @@ const { createPost , getAllPosts} = require("../controllers/PostController");
 
 
 //Mapping Create
-router.get("/dummyroute", dummyLink);
 router.post("/comments/create", createComment);
 router.post("/posts/create", createPost);
 router.get("/posts", getAllPosts);
+router.post("/likes/like", likePost);
+
 
 
 
