4d0ba014499914fe1b436d96049785244766e439
22piyush <piyushaglawe05@gmail.com>
2025-11-10 17:17:03 +0530

    done

--- BabbarBackend/blogapp/controllers/likeController.js
+++ BabbarBackend/blogapp/controllers/likeController.js
@@ -32,27 +32,45 @@ exports.likePost = async (req , res) => {
 };
 
 
-exports.unlikePost = async (req , res) => {
+exports.unlikePost = async (req, res) => {
 
-    try{
+    try {
 
-        const {post , like} = req.body;
-         
-        //Find And delete like Collection
-        const deletedLike = await Like.findOneAndDelete({post:post, _id:like});
+        const { post, like } = req.body;
 
-        //Update the post Collection
-        const updatedPost = await Post.findByIdAndDelete(post, {$pull: {likes: deletedLike._id}});
+        //Delete the like document
+        const deletedLike = await Like.findOneAndDelete({ post: post, _id: like });
 
-        res.json({
-            post:updatedPost,
+        if (!deletedLike) {
+            return res.status(404).json({
+                success: false,
+                message: "Like not found or already removed",
+            });
+        }
+
+        //Update the Post collection: remove like reference
+        const updatedPost = await Post.findByIdAndUpdate(
+        post,
+        { $pull: { likes: deletedLike._id } },
+        { new: true })
+        .populate("likes") // optional, if you want to see updated data
+        .populate("comments")
+        .exec();
+
+        //Respond
+        return res.status(200).json({
+            success: true,
+            message: "Post unliked successfully",
+            post: updatedPost,
         });
+        
+    } catch (error) {
 
-    }catch(error){
-
-        console.log(error);
+        console.error(error);
         return res.status(500).json({
-            error: "Error while Unlike",
+            success: false,
+            message: "Error while unliking the post",
+            error: error.message,
         });
 
     }
--- BabbarBackend/blogapp/routes/blog.js
+++ BabbarBackend/blogapp/routes/blog.js
@@ -14,7 +14,7 @@ router.post("/comments/create", createComment);
 router.post("/posts/create", createPost);
 router.get("/posts", getAllPosts);
 router.post("/likes/like", likePost);
-router.post("", unlikePost);
+router.post("/likes/unlike", unlikePost);
 
 
 
