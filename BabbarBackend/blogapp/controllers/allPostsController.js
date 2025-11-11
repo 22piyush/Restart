3af13dd69efe7cbe23a96a5b2de320ac73d88429
22piyush <piyushaglawe05@gmail.com>
2025-11-11 22:21:06 +0530

    done

---
+++ BabbarBackend/blogapp/controllers/allPostsController.js
@@ -0,0 +1,21 @@
+const Post = require("../models/postModel");
+
+exports.getAllPosts = async (req,res) => {
+
+    try{
+
+        const posts = await Post.find().populate("likes").populate("comments").exec();
+        res.json({
+            posts,
+        });
+
+    }catch(error){
+
+        console.log(error);
+        return res.status(500).json({
+            error: "Error while fetching Posts",
+        });
+
+    }
+
+};
