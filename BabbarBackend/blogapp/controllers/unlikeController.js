befd7d5949424afd71b98f33f588ee46dcc393b4
22piyush <piyushaglawe05@gmail.com>
2025-11-11 22:21:42 +0530

    done

---
+++ BabbarBackend/blogapp/controllers/unlikeController.js
@@ -0,0 +1,47 @@
+const Post = require("../models/postModel");
+const Like = require("../models/likeModel");
+
+exports.unlikePost = async (req, res) => {
+
+    try {
+
+        const { post, like } = req.body;
+
+        //Delete the like document
+        const deletedLike = await Like.findOneAndDelete({ post: post, _id: like });
+
+        if (!deletedLike) {
+            return res.status(404).json({
+                success: false,
+                message: "Like not found or already removed",
+            });
+        }
+
+        //Update the Post collection: remove like reference
+        const updatedPost = await Post.findByIdAndUpdate(
+        post,
+        { $pull: { likes: deletedLike._id } },
+        { new: true })
+        .populate("likes") // optional, if you want to see updated data
+        .populate("comments")
+        .exec();
+
+        //Respond
+        return res.status(200).json({
+            success: true,
+            message: "Post unliked successfully",
+            post: updatedPost,
+        });
+        
+    } catch (error) {
+
+        console.error(error);
+        return res.status(500).json({
+            success: false,
+            message: "Error while unliking the post",
+            error: error.message,
+        });
+
+    }
+
+};
