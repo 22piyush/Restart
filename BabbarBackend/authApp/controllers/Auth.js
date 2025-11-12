d251245dc0f3d2e54bf9deed28a5be35b610bda1
22piyush <piyushaglawe05@gmail.com>
2025-11-12 22:33:46 +0530

    done

---
+++ BabbarBackend/authApp/controllers/Auth.js
@@ -0,0 +1,22 @@
+const bcrypt = require("bcrypt");
+const User = require("../models/User");
+
+exports.signup = async (req,res) => {
+    try{
+
+        const {name, email, password, role} = req.body;
+
+        const existingUser = await User.findOne({email});
+
+        if(existingUser){
+            return res.status(400).json({
+                success:false,
+                message:'User already exists'
+            })
+        }
+
+    }
+    catch(error){
+
+    }
+}
