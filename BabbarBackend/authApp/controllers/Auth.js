9684af332f1e5bb3e4b3784f0ed691fbb53a2255
22piyush <piyushaglawe05@gmail.com>
2025-11-12 22:52:09 +0530

    done

--- BabbarBackend/authApp/controllers/Auth.js
+++ BabbarBackend/authApp/controllers/Auth.js
@@ -15,8 +15,38 @@ exports.signup = async (req,res) => {
             })
         }
 
-    }
-    catch(error){
+
+        let hashedPassword;
+        try{
+            hashedPassword = await bcrypt.hash(password, 10)
+        }
+        catch(error){
+         console.log(error);
+            return res.status(500).json({
+                success:false,
+                message:'Error Hashing Password'
+            })
+        
+        }
+
+
+        const user = await User.create({
+            name,email,password:hashedPassword, role
+        });
+
+        return res.status(200).json({
+            success:true,
+             message:'User created Successfully'
+        })
 
     }
+    catch(error){
+        console.log(error);
+        return res.status(500).json({
+            success:false,
+            message:'Something wend wrong while creating User'
+        })
+    }
 }
+
+
