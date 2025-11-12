021fe774cf41efb515d5bb3f8e754bd7b07d1aed
22piyush <piyushaglawe05@gmail.com>
2025-11-12 22:14:27 +0530

    done

--- BabbarBackend/authApp/index.js
+++ BabbarBackend/authApp/index.js
@@ -7,6 +7,10 @@ const PORT = process.env.PORT || 4000;
 app.use(express.json());
 
 
+const user = require("./routes/user");
+app.use("/api/v1", user);
+
+
 app.listen(PORT, () => {
     console.log(`Server started successfully ${PORT}`);
 });
