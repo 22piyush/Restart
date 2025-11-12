59668c31fbeb1981fb65f636265532c474955836
22piyush <piyushaglawe05@gmail.com>
2025-11-12 22:12:39 +0530

    done

--- BabbarBackend/authApp/index.js
+++ BabbarBackend/authApp/index.js
@@ -5,3 +5,15 @@ require("dotenv").config({ silent: true });
 const PORT = process.env.PORT || 4000;
 
 app.use(express.json());
+
+
+app.listen(PORT, () => {
+    console.log(`Server started successfully ${PORT}`);
+});
+
+const dbConnect = require("./config/database");
+dbConnect();
+
+app.get("/", (req,res) => {
+    res.send("<h1>This is Home Page</h1>")
+})
