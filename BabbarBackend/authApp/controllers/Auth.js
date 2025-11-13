af57ce5e67765b8a47a63ccb945b189ccfc24484
22piyush <piyushaglawe05@gmail.com>
2025-11-13 21:40:37 +0530

    done

--- BabbarBackend/authApp/controllers/Auth.js
+++ BabbarBackend/authApp/controllers/Auth.js
@@ -1,5 +1,7 @@
 const bcrypt = require("bcrypt");
 const User = require("../models/User");
+const jwt = require("jsonwebtoken");
+require("dotenv").config({ silent: true });
 
 exports.signup = async (req,res) => {
     try{
@@ -50,3 +52,66 @@ exports.signup = async (req,res) => {
 }
 
 
+exports.login = async (req,res) => {
+    try{
+
+        const {email, password} = req.body;
+
+        if(!email || !password){
+            return res.status(400).json({
+                success: false,
+                message:"Please fill all details carefully",
+            });
+        }
+
+        const user = await User.findOne({email});
+
+        if(!user){
+            return res.status(401).json({
+                success: false,
+                message:"User is not registered"
+            })
+        }
+
+        const payload = {
+            email:user.email,
+            id:user_id,
+            role:user.role,
+        }
+        if(await bcrypt.compare(password,user.password)){
+
+            let token = jwt.sign(payload, process.env.JWT_SECRET, {expiresIn:"2h", } );
+
+            user.token = token;
+            user.password = undefined;
+
+            const options = {
+
+               expires: new Date( Date.now() + 3 * 24 * 60 * 60 * 1000),
+               httpOnly: true,  
+                       
+            }
+
+            res.cookie("token", token, options).status(200).json({
+                success: true,
+                token,
+                user,
+                message:'User Logged in successfully'
+            });
+
+        }else{
+            return res.status(403).json({
+                success: false,
+                message:"Password Incorrect"
+            })
+        }
+
+    }
+    catch(error){
+        console.log(error);
+        return res.status(500).json({
+            success:false,
+            message:'Something wend wrong Login'
+        })
+    }
+}
