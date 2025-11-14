c640af26e652c0f4448c4a90f516cbc3037a19d9
22piyush <piyushaglawe05@gmail.com>
2025-11-14 22:23:08 +0530

    done

--- practicebabbar/blog/routes/blog.js
+++ practicebabbar/blog/routes/blog.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 
+
 const { createPost } = require("../controllers/postController");
 const {createComment} = require("../controllers/commentController");
 const { likePost } = require("../controllers/likeController");
@@ -8,10 +9,11 @@ const {unlikePost} = require("../controllers/unlikeController");
 const {getAllPosts} = require("../controllers/allPostsController");
 
 
-
-router.post("/posts/create", createPost);
+router.get("/posts", getAllPosts);
 router.post("/comments/create", createComment);
-
+router.post("/posts/create", createPost);
+router.post("/likes/like", likePost);
+router.post("/likes/unlike", unlikePost);
 
 
 module.exports = router;
