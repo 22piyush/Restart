67c3ec61ca4a2169cbfd25e968f9be8ef2debfd4
22piyush <piyushaglawe05@gmail.com>
2025-11-14 21:51:33 +0530

    done

---
+++ practicebabbar/blog/controllers/commentController.js
@@ -0,0 +1,31 @@
+const Post = require("../modules/postModel");
+const Comment = require("../modules/commentModel");
+
+exports.createComment = async (req,res) => {
+
+    try{
+
+        const {post, user, body} = req.body;
+        const comment = new Comment({
+            post,user,body
+        })
+
+        const savedComment = await comment.save();
+
+        const updatedPost = await Post.findByIdAndUpdate(post ,{$push: {comments: savedComment._id}}, {new: true})
+                           .populate("comment")
+                           .exec();
+
+        res.json({
+            post : updatedPost,
+        })                   
+
+    }catch(error){
+
+        return res.status(500).json({
+            error: "Error while creating comment"
+        })
+
+    }
+
+}
--- practicebabbar/blog/routes/blog.js
+++ practicebabbar/blog/routes/blog.js
@@ -2,14 +2,11 @@ const express = require("express");
 const router = express.Router();
 
 const { createPost } = require("../controllers/postController");
-
-
-
+const {createComment} = require("../controllers/commentController");
 
 
 router.post("/posts/create", createPost);
-
-
+router.post("/comments/create", createComment);
 
 
 
