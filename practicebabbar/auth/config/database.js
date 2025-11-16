369241c42aaeb56088e3c2097cd1d39cbb1397b4
22piyush <piyushaglawe05@gmail.com>
2025-11-16 22:02:10 +0530

    done

---
+++ practicebabbar/auth/config/database.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose");
+require("dotenv").config({ silent: true });
+
+const dbConnect = async () => {
+
+    try{
+
+        await mongoose.connect(process.env.DATABASE_URL);
+        console.log("DB Connection Successful");
+        
+    }catch(error){
+        console.error("Issue in DB Connection");
+        console.error(error.message);
+        process.exit(1);
+    }
+
+};
+
+module.exports = dbConnect;
