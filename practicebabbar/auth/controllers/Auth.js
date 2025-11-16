063813ea4552957bb04e83c399df15e4260f12bd
22piyush <piyushaglawe05@gmail.com>
2025-11-16 23:12:05 +0530

    done

--- practicebabbar/auth/controllers/Auth.js
+++ practicebabbar/auth/controllers/Auth.js
@@ -63,6 +63,37 @@ exports.login = async (req,res) => {
             });
         }
 
+        let user = await User.findOne({email});
+
+        if(!user){
+            return res.status(401).json({
+                success:false,
+                message:"User is not registered"
+            })
+        }
+
+        const payload = {
+
+            email: user.email,
+            id: user._id,
+            role:user.role
+
+        }
+
+        if(await bcrypt.compare(password, user.password)){
+
+            let token = jwt.sign(payload, process.env.JWT_SECRET, {expiresIn:"2h"});
+
+            user = user.toObject();
+
+
+        }else{
+            return res.status(403).json({
+                success: false,
+                message:"Password Incorrect"
+            })
+        }
+
 
 
     }catch(error){
