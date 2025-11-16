9feb669a42ed089ef679521909e3eb98cd8444b3
22piyush <piyushaglawe05@gmail.com>
2025-11-16 23:21:17 +0530

    done

--- practicebabbar/auth/controllers/Auth.js
+++ practicebabbar/auth/controllers/Auth.js
@@ -85,7 +85,20 @@ exports.login = async (req,res) => {
             let token = jwt.sign(payload, process.env.JWT_SECRET, {expiresIn:"2h"});
 
             user = user.toObject();
+            user.token = token;
+            user.password = null;
 
+            const options = {
+               expires: new Date( Date.now() + 3 * 24 * 60 * 60 * 1000),
+               httpOnly: true,  
+            }
+
+            res.cookie("token", token, options).status(200).json({
+                success: true,
+                token,
+                user,
+                message:"User Logged in successfully"
+            })
 
         }else{
             return res.status(403).json({
