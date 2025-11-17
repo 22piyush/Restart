88d8cc7a118b92bf14930fcb3b8494ff8ee36a94
22piyush <piyushaglawe05@gmail.com>
2025-11-17 22:57:37 +0530

    done

--- BabbarBackend/fileUpload/index.js
+++ BabbarBackend/fileUpload/index.js
@@ -2,5 +2,25 @@ const express = require("express");
 const app = express();
 
 require("dotenv").config();
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+const fileupload = require("express-fileupload");
+app.use(fileupload());
+
+const db = require("./config/database");
+db.connect();
+
+// Image Cloud Connection 
+const cloudinary = require("./config/cloudinary");
+cloudinary.cloudinaryConnect();
+
+const upload = require("./routes/FileUpload");
+app.use("api/v1/upload", upload);
+
+app.listen(PORT, () => {
+    console.log(`App is running on RORT ${PORT}`);
+})
+
+
 
