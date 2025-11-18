d63fb4541350d5896c7f93b4612245b083c4f659
22piyush <piyushaglawe05@gmail.com>
2025-11-18 21:13:15 +0530

    done

--- BabbarBackend/fileUpload/controllers/fileUpload.js
+++ BabbarBackend/fileUpload/controllers/fileUpload.js
@@ -28,4 +28,39 @@ exports.localFileUpload = async (req , res) => {
         
     }
 
+};
+
+
+function isFileTypeSupported(type, supportedTypes){
+    return supportedTypes.includes(type);
+}
+
+
+exports.imageUpload = async (req, res) =>{
+
+    try{
+
+        const {name, tags, email} = req.body;
+        console.log(name, tags, email);
+
+        const file = req.files.imageFile;
+        console.log(file);
+
+        const supportedTypes = ["jpg", "jpeg", "png"];
+        const fileType = file.name.split('.')[1].toLowerCase();
+
+        if(!isFileTypeSupported(fileType, supportedTypes)){
+            res.status(400).json({
+                success:false,
+                message:"File format not supported."
+            })
+        }
+
+        
+        
+    }
+    catch(error){
+
+    }
+
 }
