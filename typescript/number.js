aec9d11ec7d3d2d2b82bb6a2abec86b75ccd63aa
22piyush <piyushaglawe05@gmail.com>
2025-11-22 22:30:42 +0530

    done

--- typescript/number.js
+++ typescript/number.js
@@ -7,3 +7,8 @@ var oct = 1;
 var hexa = 1;
 var binary = 0x00001;
 console.log(oct + 10);
+// Conversion 
+var item = 100;
+var item2 = "50";
+var item2Converted = +item2;
+console.log(item + +item2);
--- typescript/number.ts
+++ typescript/number.ts
@@ -1,7 +1,6 @@
 var num1:number = 10;
 var num2:number = 30;
 var num3:number  = 30;
-
 var total:number = num1 + num3;
 console.log(total);
 
@@ -9,8 +8,16 @@ console.log(total);
 var oct:number =0o00001;
 var hexa:number =0b00001;
 var binary:number =0x00001;
-
 console.log(oct+10);
+
+
+// Conversion 
+var item:number = 100;
+var item2 = "50";
+var item2Converted =+ item2;
+console.log(item+ +item2);
+
+
 
 
 
