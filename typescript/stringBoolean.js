8d206ecd771dea012ff14d3af0f6a782cef2d70a
22piyush <piyushaglawe05@gmail.com>
2025-11-22 22:42:54 +0530

    done

---
+++ typescript/stringBoolean.js
@@ -0,0 +1,4 @@
+var str = "Piyush Bhau";
+var age = 30;
+var info = "My name is ".concat(str, " and age is ").concat(age);
+console.log(info);
---
+++ typescript/stringBoolean.ts
@@ -0,0 +1,6 @@
+var str:string = "Piyush Bhau";
+var age:number = 30;
+
+var info = `My name is ${str} and age is ${age}`;
+console.log(info);
+
