961cfd4788c2a6552a6b0a849317225bda173fc4
22piyush <piyushaglawe05@gmail.com>
2025-11-26 22:05:44 +0530

    done

--- server/controllers/Auth.js
+++ server/controllers/Auth.js
@@ -1,3 +1,11 @@
 const User = require("../models/User");
 const OTP = require("../models/OTP");
 
+exports.sendOTP = async (req, res) => {
+
+    const {email} = req.body;
+
+    const checkUserPresent = await User.findOne({email});
+    
+
+}
