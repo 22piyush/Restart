da812f303c98fcea49038f0e83358253116c732a
22piyush <piyushaglawe05@gmail.com>
2025-11-26 22:57:32 +0530

    done

--- server/controllers/Auth.js
+++ server/controllers/Auth.js
@@ -16,6 +16,29 @@ exports.sendOTP = async (req, res) => {
                 message:"User already register",
             })
         }
+
+        let otp = otpGenerator.generate(6, {
+            upperCaseAlphabets:false,
+            lowerCaseAlphabets:false,
+            specialChars:false,
+        });
+
+        console.log("OTP Generated", otp);
+
+        const result = await OTP.findOne({otp: otp});
+
+        while(result){
+            otp = otpGenerator(6, {
+                upperCaseAlphabets:false,
+                lowerCaseAlphabets:false,
+                specialChars:false,
+            });
+            result = await OTP.findOne({otp: otp});
+        }
+
+        const otpPayload = {email, otp};
+
+        
     }
     catch(error){
 
