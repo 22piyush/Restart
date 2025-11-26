03e228c88d329180b69ce6064425411d1c305df1
22piyush <piyushaglawe05@gmail.com>
2025-11-26 22:08:45 +0530

    done

--- server/controllers/Auth.js
+++ server/controllers/Auth.js
@@ -1,11 +1,24 @@
 const User = require("../models/User");
 const OTP = require("../models/OTP");
+const otpGenerator = require("otp-generator");
 
 exports.sendOTP = async (req, res) => {
 
-    const {email} = req.body;
+    try{
 
-    const checkUserPresent = await User.findOne({email});
-    
+        const {email} = req.body;
+
+        const checkUserPresent = await User.findOne({email});
+
+        if(checkUserPresent){
+            return res.Status(401).json({
+                success:false,
+                message:"User already register",
+            })
+        }
+    }
+    catch(error){
+
+    }
 
 }
