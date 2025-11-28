064cb39c8a2f71db8094aa2e8997ad5cf5e5f6f8
22piyush <piyushaglawe05@gmail.com>
2025-11-28 22:10:03 +0530

    done

--- server/middlewares/auth.js
+++ server/middlewares/auth.js
@@ -8,8 +8,8 @@ exports.auth = async (req, res, next) => {
     try{    
 
         const token = req.cookies.token 
-        || req.body.token 
-        || req.header("Authorisation").replace("Bearer","");
+            || req.body.token 
+            || req.header("Authorisation").replace("Bearer","");
 
         if(!token){
             return res.status(401).json({
@@ -18,11 +18,30 @@ exports.auth = async (req, res, next) => {
             });
         }
 
+        try{
+            const decode = jwt.verify(token, process.env.JWT_SECRET);
+            req.user = decode;
+        }
+        catch(error){
+            return res.status(401).json({
+                success:false,
+                message:'Token is invalid'
+            });
+        }
+
     }
     catch(error){
 
-
+        return res.status(401).json({
+            success:false,
+            message:"Something wend wrong while validating the token"
+        })
 
     }
+    
+};
+
+
+exports.isStudent = async (req, res, next) => {
     
 }
