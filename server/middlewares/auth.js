e0ca8a7fdc9634544ca385b3643e7191312042bb
22piyush <piyushaglawe05@gmail.com>
2025-11-28 22:24:06 +0530

    done

--- server/middlewares/auth.js
+++ server/middlewares/auth.js
@@ -3,6 +3,7 @@ require("dotenv").config();
 
 const User = require("../models/User");
 
+
 exports.auth = async (req, res, next) => {
 
     try{    
@@ -45,13 +46,67 @@ exports.auth = async (req, res, next) => {
 exports.isStudent = async (req, res, next) => {
 
     try{
+        
+        if(req.user.accountType !== "Student"){
+            return res.status(401).json({
+                success:false,
+                message:"This is a protected route for Students Only"
+            });
+        }
+        next();
 
     }
     catch(error){
         return res.status(500).json({
             success:false,
-            message:"User role cannot be verified"
+            message:"User role cannot be verified, please try again"
         })
     }
 
-}
+};
+
+
+exports.isInstructor = async (req, res, next) => {
+
+    try{
+        
+        if(req.user.accountType !== "Instructor"){
+            return res.status(401).json({
+                success:false,
+                message:"This is a protected route for Instructor Only"
+            });
+        }
+        next();
+
+    }
+    catch(error){
+        return res.status(500).json({
+            success:false,
+            message:"User role cannot be verified, please try again"
+        })
+    }
+
+};
+
+
+exports.isAdmin = async (req, res, next) => {
+
+    try{
+        
+        if(req.user.accountType !== "Admin"){
+            return res.status(401).json({
+                success:false,
+                message:"This is a protected route for Admin Only"
+            });
+        }
+        next();
+
+    }
+    catch(error){
+        return res.status(500).json({
+            success:false,
+            message:"User role cannot be verified, please try again"
+        })
+    }
+
+};
