8bc500236c9783e8883a162766c84da128f5ab4a
22piyush <piyushaglawe05@gmail.com>
2025-11-30 10:10:16 +0530

    done

---
+++ typescript/inheritance.ts
@@ -0,0 +1,40 @@
+class Student{
+    login(name:string,password:string){
+
+        if(name && password){
+            return "Student Login"
+        }else{
+            return "Not Login"
+        }
+
+    }
+    result(marks:number){
+        if(marks>33){
+            return "Pass"
+        }else{
+            return "failed"
+        }
+    }
+}
+
+var s1 = new Student();
+s1.result(60);
+
+
+class Teacher{
+    login(name:string,password:string){
+
+        if(name && password){
+            return "Student Login"
+        }else{
+            return "Not Login"
+        }
+
+    }
+    subject(subject:number){
+        return "he teach "+subject
+    }
+}
+
+var t1 = new Teacher();
+t1.login("sam","12345");
