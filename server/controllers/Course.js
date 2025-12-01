b25654d3267db37cb65f23102a60eac81d1b0ad1
22piyush <piyushaglawe05@gmail.com>
2025-12-01 22:24:20 +0530

    done

--- server/controllers/Course.js
+++ server/controllers/Course.js
@@ -3,3 +3,46 @@ const Tag = require("../models/tags");
 const User = require("../models/User");
 const {uploadImageToCloudinary} = require("../utils/imageUploader");
 
+
+exports.createCourse = async(req, res) => {
+    try{
+
+        const {courseName, courseDescription, whatYouWillLearn, price, tag} = req.body;
+        const thumbnail = req.files.thumbnailImage;
+
+        if(!courseName || !courseDescription || !whatYouWillLearn || !price || !tag || !thumbnail){
+            return res.status(400).json({
+                success:false,
+                message:"All fields are required",
+            });
+        }
+
+        const userId = req.user.id;
+        const instructorDetails = await User.findById(userId);
+        console.log("Instructor Details", instructorDetails);
+
+        if(!instructorDetails){
+            return res.status(404).json({
+                success:false,
+                message:"Instructor Detail not found",
+            })
+        }
+
+        const tagDetails = await Tag.findById(tag);
+        if(!tagDetails){
+            return res.status(404).json({
+                success:false,
+                message:"Tag Details not found"
+            })
+        }
+
+        
+
+    }
+    catch(error){
+        return res.status(500).json({
+            success:false,
+            message:"Error while creating course"
+        });
+    }
+}
