b7c5044738a0501015091668c8d3a9077003834f
22piyush <piyushaglawe05@gmail.com>
2025-12-01 21:53:20 +0530

    done

--- server/controllers/Course.js
+++ server/controllers/Course.js
@@ -1,3 +1,5 @@
 const Course = require("../models/Course");
 const Tag = require("../models/tags");
-const User = require("../")
+const User = require("../models/User");
+const {uploadImageToCloudinary} = require("../utils/imageUploader");
+
