e624684b375e3db179bf922ccb6a4b0c2ebcdcb4
22piyush <piyushaglawe05@gmail.com>
2025-12-03 07:18:38 +0530

    done

--- HOOKS-REACT/react-hooks/src/App.jsx
+++ HOOKS-REACT/react-hooks/src/App.jsx
@@ -1,15 +1,19 @@
-
-function App() {
-  
-  const anme = "Piyush Aglawe"
-
-  return (
-    <>
-      <div>
-          {anme}
-      </div>
-    </>
-  )
+function outsideFunction() {
+  console.log("Outside Function");
 }
 
-export default App
+function App() {
+
+  function insideFunction() {
+    console.log("Inside Function");
+  }
+
+  return (
+    <div>
+      <button onClick={insideFunction}>Inside Fn</button>
+      <button onClick={outsideFunction}>Outside Fn</button>
+    </div>
+  );
+}
+
+export default App;
