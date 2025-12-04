7f223dd9067b1b856332e0cbb5d0108fd8b395f9
22piyush <piyushaglawe05@gmail.com>
2025-12-04 21:45:34 +0530

    done

--- HOOKS-REACT/react-hooks/src/App.jsx
+++ HOOKS-REACT/react-hooks/src/App.jsx
@@ -9,7 +9,9 @@ function App() {
         <h1>Multiple conditions in React</h1>
         <button onClick={()=>setCount(count + 1)}>Counter</button>
         {
-           
+           count == 0?<h1>Condition 0</h1>
+           :count == 17?<h1>Condition 1</h1>
+           :<h1>Other Condition</h1>
         }
     </div>
   );
