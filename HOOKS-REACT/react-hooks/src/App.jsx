53d099abe73e67334d059635f5b5e8222ce95d01
22piyush <piyushaglawe05@gmail.com>
2025-12-04 22:44:30 +0530

    done

--- HOOKS-REACT/react-hooks/src/App.jsx
+++ HOOKS-REACT/react-hooks/src/App.jsx
@@ -2,9 +2,13 @@ import User from "./User";
 
 function App() {
 
+  let userName = "Anil Sidhu";
+  let age = 23;
+  let email = "piyush@1234";
+
   return (
     <div>
-        <User name="Anil Bhau"/>
+        <User name={userName}  age={age}  email={email}/>
     </div>
   );
 }
--- HOOKS-REACT/react-hooks/src/User.jsx
+++ HOOKS-REACT/react-hooks/src/User.jsx
@@ -3,6 +3,7 @@ function User(props) {
   return (
     <div>
         <h1>{props.name}</h1>
+        <h1>{props.age}</h1>
     </div>
   )
 }
