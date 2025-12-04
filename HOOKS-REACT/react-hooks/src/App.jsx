d984e42a5f5c3cbea7f3e2e70124ae8b76961c76
22piyush <piyushaglawe05@gmail.com>
2025-12-04 22:36:09 +0530

    done

--- HOOKS-REACT/react-hooks/src/App.jsx
+++ HOOKS-REACT/react-hooks/src/App.jsx
@@ -1,18 +1,10 @@
-import { useState } from "react";
+import User from "./User";
 
 function App() {
 
-  const [count, setCount] = useState(0);
-
   return (
     <div>
-        <h1>Multiple conditions in React</h1>
-        <button onClick={()=>setCount(count + 1)}>Counter</button>
-        {
-           count == 0?<h1>Condition 0</h1>
-           :count == 17?<h1>Condition 1</h1>
-           :<h1>Other Condition</h1>
-        }
+        <User/>
     </div>
   );
 }
