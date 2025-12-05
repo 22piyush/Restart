6b55ca7ec99ad01fcc314a4a98e6e3bd02ad6488
22piyush <piyushaglawe05@gmail.com>
2025-12-05 08:12:36 +0530

    done

--- HOOKS-REACT/react-hooks/src/App.jsx
+++ HOOKS-REACT/react-hooks/src/App.jsx
@@ -1,7 +1,19 @@
 function App() {
 
+  const [val, setVal] = ("Piyush")
+
+  const handleInput =(e)=> {
+      setVal(e.target.value);
+      
+  }
+
+
+
   return (
     <div>
+
+        <input type="text" onChange={handleInput}/>
+        <h1>{val}</h1>
     </div>
   );
 }
