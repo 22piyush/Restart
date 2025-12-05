cad85559370f89ef61165a88dbe9a302627b4f41
22piyush <piyushaglawe05@gmail.com>
2025-12-05 23:08:39 +0530

    done

--- element/index.js
+++ element/index.js
@@ -1,9 +1,82 @@
 const App = {
   data() {
     return {
-      message: "Hello Element Plus 🚀",
+        license_config: [
+        {
+            title: "Device",
+            id: "device_license",
+            disabled: false,
+            fields: [
+                {
+                    id: "device_type",
+                    label: "Select Device",
+                    input_type: "dropdown",
+                    value: "",
+                    options: [
+                        {
+                            id: "cloud_link",
+                            label: "Cloud Link",
+                            subfields: [
+                                {
+                                    id: "sub_type",
+                                    label: "Select Link Type",
+                                    input_type: "dropdown",
+                                    value: "",
+                                    options: [
+                                        { id: "virtual", label: "Virtual" },
+                                        { id: "hardware", label: "Hardware" },
+                                    ]
+                                }
+                            ],
+
+                            licenses: [
+                                {
+                                    id: "location_license",
+                                    label: "Location License",
+                                    input_type: "number",
+                                    value: 0,
+                                    min: 0,
+                                    max: Infinity,
+                                },
+                                {
+                                    id: "camera_license",
+                                    label: "Camera License",
+                                    input_type: "number",
+                                    value: 0,
+                                    min: 0,
+                                    max: Infinity,
+                                }
+                            ]
+                        }
+                    ]
+                }
+            ]
+        },    
+        { title: "Cloud", id: "cloud_license", config: [] },
+        { title: "ICCC", id: "icc_license", config: [] }
+        ],
+        formData: {},
+        activeLicenseId: "device_license"
+
     };
   },
+
+    
+    methods:{
+        initFormFields() {
+  const fields = this.deviceLicenseOptions;
+  fields.forEach(field => {
+    if (this.formData[field.id] === undefined) {
+      this.formData[field.id] = 1; // default value
+    }
+  });
+},
+
+        submitFormData(){
+            console.log(this.formData);
+        }
+    }
+
 };
 
 const app = Vue.createApp(App);
