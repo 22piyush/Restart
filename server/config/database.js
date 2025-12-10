3e811c65721afee24220f95d1878183bea471495
22piyush <piyushaglawe05@gmail.com>
2025-12-10 22:07:38 +0530

    done

--- server/config/database.js
+++ server/config/database.js
@@ -1,1 +1,28 @@
 const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+
+dotenv.config(); // Load .env variables
+
+const uri = process.env.MONGO_URL;
+
+if (!uri) {
+  throw new Error("MONGO_URL is not defined in .env");
+}
+
+async function connectDB() {
+  try {
+    await mongoose.connect(uri, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+
+    console.log("✅MongoDB connected successfully!");
+
+    return mongoose.connection; // return DB connection
+  } catch (err) {
+    console.error("❌MongoDB connection error:", err);
+    process.exit(1); // Exit on failure
+  }
+}
+
+module.exports = connectDB;
