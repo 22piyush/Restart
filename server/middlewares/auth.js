e3da2c5a1a415bbb10c52592efba0f87d192cc9f
22piyush <piyushaglawe05@gmail.com>
2025-12-12 21:07:47 +0530

    done

---
+++ server/middlewares/auth.js
@@ -0,0 +1,14 @@
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
+const user = require("../models/User");
+
+
+// auth 
+exports.auth = async (req, res, next) => {
+    try{
+        
+    }
+    catch(error){
+
+    }
+}
