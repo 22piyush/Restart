04ea94762d1397b00c5d104e5ee125d40b56142b
22piyush <piyushaglawe05@gmail.com>
2025-12-12 21:13:34 +0530

    done

--- server/middlewares/auth.js
+++ server/middlewares/auth.js
@@ -6,7 +6,15 @@ const user = require("../models/User");
 // auth 
 exports.auth = async (req, res, next) => {
     try{
-        
+        // extract token 
+        const token = req.cookies.token || req.body.token || req.header("Authorisation").replace("Bearer ","");
+   
+        if(!token){
+            return res.status(401).json({
+                success:false,
+                message:"Token is missing"
+            });
+        }
     }
     catch(error){
 
