5576904312292f02baeffba941b408d916889fa5
22piyush <piyushaglawe05@gmail.com>
2025-12-13 21:52:46 +0530

    done

--- server/controllers/Course.js
+++ server/controllers/Course.js
@@ -1,5 +1,11 @@
 const Course = require("../models/Course");
 const Tag = require("../models/Tags");
 const User = require("../models/User");
+const {uploadImageToCloudinary} = require("../utils/imageUploader");
 
+
+// create course handler function 
+exports.createCourse = async (req, res) => {
+    
+}
 
