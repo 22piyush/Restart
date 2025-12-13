8c2e7e9b2ecb9c6557a59b5784dfca599f7f1030
22piyush <piyushaglawe05@gmail.com>
2025-12-13 22:10:10 +0530

    done

--- server/controllers/Course.js
+++ server/controllers/Course.js
@@ -7,5 +7,51 @@ const {uploadImageToCloudinary} = require("../utils/imageUploader");
 // create course handler function 
 exports.createCourse = async (req, res) => {
     
+    try{
+
+        //fetch data 
+        const {courseName, courseDescription, whatYouWillLearn, price, tag} = req.body;
+
+        // get thumbnail 
+        const thumbnail = req.files.thumbnailImage;
+
+
+        // validation 
+        if(!courseName || !courseDescription || !whatYouWillLearn || !price || !tag || !thumbnail){
+            return res.status(400).json({
+                success: false,
+                message:"All fields are requireds",
+            });
+        }
+
+
+        // check for instructor 
+        const userId = req.user.id;
+        const instructorDetails = await User.findById(userId);
+
+        if(!instructorDetails){
+            return res.status(404).json({
+                success: false,
+                message: "Instructor Details not found",
+            });
+        }
+
+
+        const tagDetails = await Tag.findById(tag);
+        if(!tagDetails){
+            return res.status(401).json({
+                success: false,
+                message: "Tag Details not found",
+            });
+        }
+
+        // upload image to cloudinary 
+        const thumbnailImage = await uploadImageToCloudinary(thumbnail, process.env.FOLDER_NAME)
+
+    }
+    catch(error){
+
+    }
+    
 }
 
--- server/controllers/Tags.js
+++ server/controllers/Tags.js
@@ -1,4 +1,4 @@
-const Tag = require("../models/tags");
+const Tag = require("../models/Tags");
 
 // create Tag Handler function
 exports.createTag = async (req, res) => {
