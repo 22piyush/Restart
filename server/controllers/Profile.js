3eec70dbf5e370399e7b20e1a5ca33326f938982
22piyush <piyushaglawe05@gmail.com>
2025-12-15 21:28:57 +0530

    done

--- server/controllers/Profile.js
+++ server/controllers/Profile.js
@@ -3,13 +3,13 @@ const User = require("../models/User");
 
 
 exports.updateProfile = async (req, res) => {
-    try{
+    try {
         // get data 
-        const {dateOfBirth="", about="", contactNumber, gender} = req.body;
+        const { dateOfBirth = "", about = "", contactNumber, gender } = req.body;
         // get userId
         const id = req.user.id;
         // validation 
-        if(!contactNumber || !gender || !id){
+        if (!contactNumber || !gender || !id) {
             return res.status(400).json({
                 success: false,
                 message: "All fields are required",
@@ -28,15 +28,52 @@ exports.updateProfile = async (req, res) => {
 
         // return response 
         return res.status(200).json({
-            success:true,
-            message:"Profile updated successfully",
+            success: true,
+            message: "Profile updated successfully",
             profileDetails
         })
+    }
+    catch (error) {
+        return res.status(500).json({
+            success: false,
+            message: "Error while updating profile",
+            error: error.message
+        })
+    }
+};
+
+
+
+// delete account 
+exports.deleteAccount = async (req, res) => {
+    try{
+        // get id 
+        const id = req.user.id;
+        // validation 
+        const userDetails = await User.findById(id);
+        if(!userDetails){
+            return res.status(404).json({
+                success: false,
+                message: "User not found"
+            });
+        }
+        // delete profile 
+        await Profile.findByIdAndDelete({_id:userDetails.additionalDetails});
+        // delete user 
+        await User.findByIdAndDelete({_id:id});
+        // HW unenroll user from all enrolled courses 
+        // return response
+        return res.status(200).json({
+            success: true,
+            message: "User Deleted Successfully"
+        });
     }
     catch(error){
         return res.status(500).json({
             success: false,
-            message:"Error while updating profile"
-        })
+            message: "Error while deleting account"
+        });
     }
-};
+}
+
+
