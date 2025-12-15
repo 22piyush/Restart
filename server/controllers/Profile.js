e27322e6b060e8e75b0426521f6c598f6610c257
22piyush <piyushaglawe05@gmail.com>
2025-12-15 20:27:18 +0530

    done

--- server/controllers/Profile.js
+++ server/controllers/Profile.js
@@ -4,9 +4,38 @@ const User = require("../models/User");
 
 exports.updateProfile = async (req, res) => {
     try{
-        
+        // get data 
+        const {dateOfBirth="", about="", contactNumber, gender} = req.body;
+        // get userId
+        const id = req.user.id;
+        // validation 
+        if(!contactNumber || !gender || !id){
+            return res.status(400).json({
+                success: false,
+                message: "All fields are required",
+            });
+        }
+        // find profile 
+        const userDetails = await User.findById(id);
+        const profileId = userDetails.additionalDetails;
+        const profileDetails = await Profile.findById(profileId);
+        // update profile 
+        profileDetails.dateOfBirth = dateOfBirth;
+        profileDetails.about = about;
+        profileDetails.gender = gender;
+        profileDetails.contactNumber = contactNumber;
+        await profileDetails.save();
+
+        // return response 
+        return res.status(200).json({
+            success:true,
+            message:"Profile updated successfully"
+        })
     }
     catch(error){
-
+        return res.status(500).json({
+            success: false,
+            message:"Error while updating profile"
+        })
     }
 };
