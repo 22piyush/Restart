0b89a5532ef9e3df2e251acdcce200fb5a04405b
22piyush <piyushaglawe05@gmail.com>
2025-12-16 22:15:05 +0530

    done

--- server/controllers/Payments.js
+++ server/controllers/Payments.js
@@ -9,9 +9,41 @@ const {courseEnrollmentEmail} = require("../mail/templates/courseEnrollmentEmail
 // Capture the payment and initiate the Razorpay order 
 exports.capturePayment = async (req, res) =>{
     // get courseId and UserID 
+    const {course_id} = req.body;
+    const userId = req.user.id;
     // validation 
     // valid courseID 
-    // valid courseDetail 
-    // user already pay for the same course 
+    if(!course_id){
+        return res.json({
+            success: false,
+            message: "Please provide valid course ID"
+        });
+    }
+    // valid courseDetail
+    let course;
+    try{
+        course = await course.findById(course_id);
+        if(!course){
+            return res.json({
+                success : false,
+                message: "Could not find the course"
+            });
+        }
+        // user already pay for the same course 
+        const uid = new mongoose.Types.ObjectId(userId);
+        if(course.studentsEnrolled.includes(uid)){
+            return res.status(201).json({
+                success: false,
+                message: "Student is already enrolled"
+            });
+        }
+    } 
+    catch(error){
+        return res.status(500).json({
+            success: false,
+            message: error.message
+        });
+    }
     // order created 
+    // return response 
 }
