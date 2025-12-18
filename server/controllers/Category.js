101e60514dbc71746b56cf4de19533c5ab3bed24
22piyush <piyushaglawe05@gmail.com>
2025-12-18 22:59:36 +0530

    done

---
+++ server/controllers/Category.js
@@ -0,0 +1,1 @@
+// Tag Name Change to CATEGORY SO CHANGE IT 
--- server/controllers/Tags.js
+++ server/controllers/Tags.js
@@ -1,5 +1,7 @@
 const Tag = require("../models/Tags");
 
+// const Tag = require("../models/Tags");   Convert this into categgory 
+
 // create Tag Handler function
 exports.createTag = async (req, res) => {
     try {
@@ -50,6 +52,54 @@ exports.showAlltags = async (req, res) => {
         });
     }
 };
+
+
+// category page details 
+exports.categoryPageDetails = async (req, res) => {
+    try {
+        // get category id 
+        const { categoryId } = req.body;
+        // get courses for specified categoryId
+        const selectedCategory = await Category.findById(category)
+            .populate("courses")
+            .exec();
+
+        // validation
+        if (!selectedCategory) {
+            return res.status(404).json({
+                success: false,
+                message: "Data not found"
+            });
+        }
+
+        // get courses for different categories 
+        const differentCategories = await Category.find({
+            _id: { $ne: categoryId },
+        })
+        .populate("courses")
+        .exec();
+
+
+        // get top selling courses 
+        // HW - Write it on your own 
+
+        // return response 
+        return res.status(200).json({
+            success: true,
+            data:{
+                selectedCategory,
+                differentCategories,
+            }
+        })
+
+    }
+    catch (error) {
+        return res.status(500).json({
+            success: false,
+            message: error.message
+        });
+    }
+}
 
 
 
