9ac23ddd08247f41e4cafd2139ef4fd37ee5f2f8
22piyush <piyushaglawe05@gmail.com>
2025-12-18 21:09:26 +0530

    done

--- server/controllers/RatingAndReview.js
+++ server/controllers/RatingAndReview.js
@@ -2,19 +2,23 @@ const ratingAndreview = require("../models/RatingAndReview");
 const Course = require("../models/Course");
 
 // createRating 
-
-
-
-
-
-
-
-
-
-
-
+exports.createRating = async (req,res) => {
+    try{
+        
+    }
+    catch(error){
+        return res.status(500).json({
+            success: false,
+            message:error.message
+        })
+    }
+}
 
 
 
 // getAverageRating 
+
+
+
+// getAllRating 
 
