defaf1befcd44932009ddab7888b3b007cc50650
22piyush <piyushaglawe05@gmail.com>
2025-12-21 13:28:15 +0530

    done

---
+++ Cookies/index.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const app = express();
+const PORT = 3000;
+
+// Middleware to read JSON
+app.use(express.json());
+
+// Home route
+app.get("/", (req, res) => {
+  res.send("Node.js backend running without database 🚀");
+});
+
+// Server start
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
