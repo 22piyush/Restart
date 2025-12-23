90ad426e56d5b2457cd62264cdf20ae84367054a
22piyush <piyushaglawe05@gmail.com>
2025-12-23 23:04:12 +0530

    done

--- HOOKS-REACT/react-hooks/src/App.jsx
+++ HOOKS-REACT/react-hooks/src/App.jsx
@@ -1,22 +1,9 @@
-import { useState } from "react";
 
 function App() {
-
-  const [val, setVal] = useState("Piyush")
-
-  const handleInput =(e)=> {
-      setVal(e.target.value);
-      
-  }
-
-
-
   return (
-    <div>
-
-        <input type="text" onChange={handleInput}/>
-        <h1>{val}</h1>
-    </div>
+    <>
+        
+    </>
   );
 }
 
