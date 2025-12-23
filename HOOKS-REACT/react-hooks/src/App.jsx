4f6f11f09afa78d79f8a1b6b0785d127ee6fa62c
22piyush <piyushaglawe05@gmail.com>
2025-12-23 23:10:37 +0530

    done

--- HOOKS-REACT/react-hooks/src/App.jsx
+++ HOOKS-REACT/react-hooks/src/App.jsx
@@ -1,8 +1,27 @@
+import { useActionState } from "react";
 
 function App() {
+
+  const handleSubmit =(previousData, formData)=>{
+    let name = formData.get('name');
+    let password = formData.get('password');
+
+    console.log("Handle Submit called" , name , password);
+  }
+  const [data, action, pending] = useActionState(handleSubmit , undefined);
+
   return (
     <>
-        
+        <h1>Use Action State Hook</h1>
+
+        <form action={action}>
+            <input type="text" placeholder="Enter Name" name="name"/>
+            <br/><br/>
+             <input type="text" placeholder="Enter Password" name="password"/>
+            <br/><br/>
+
+            <button>Submit</button>
+        </form>
     </>
   );
 }
