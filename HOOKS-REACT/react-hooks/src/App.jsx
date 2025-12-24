cde9face86af725c5e9684e838ee437431e36d48
22piyush <piyushaglawe05@gmail.com>
2025-12-24 23:19:35 +0530

    done

--- HOOKS-REACT/react-hooks/src/App.jsx
+++ HOOKS-REACT/react-hooks/src/App.jsx
@@ -4,11 +4,28 @@ import UseMemo from "./hooks/UseMemo";
 
 
 function App() {
+
+  const fruit = ["Apple", "Banana", "Orange"];
+
   return(
     <>
      <UseActionState/>
      <UseCallback/>
      <UseMemo/> 
+
+
+     {/* MAP Fuction Array */}
+     <div>
+          <h2>Fruit List</h2>
+          <ul>
+              {
+                fruit.map((fruit, index) => {
+                  <li>{index} - {fruit}</li>
+                })
+              }
+          </ul>
+     </div>
+
     </>
   );
 }
--- HOOKS-REACT/react-hooks/src/hooks/UseMemo.jsx
+++ HOOKS-REACT/react-hooks/src/hooks/UseMemo.jsx
@@ -1,28 +1,9 @@
-import React, { useState } from 'react'
 
 function UseMemo() {
 
-    const [count , setCount] = useState(0);
-
-    function expensiveTask(num){
-        console.log("Inside Expensive Task");
-        for(let i=0 ; i<=10; i++){}
-        return num * 2; 
-    }
-
-    let doubleValue = expensiveTask(4);
-
   return (
     <div>
-        <button onClick={()=> setCount(count + 1)}>Increment</button>
-
-        <div>
-            Count: {count}
-        </div>
-
-        <div>
-            Double: {doubleValue}
-        </div>
+       
 
     </div>
   )
