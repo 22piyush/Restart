6e24b5e85f5d7e04542be4e2c51ee8bd676f2bda
22piyush <piyushaglawe05@gmail.com>
2025-12-24 23:56:30 +0530

    done

--- HOOKS-REACT/react-hooks/src/components/Countdown.jsx
+++ HOOKS-REACT/react-hooks/src/components/Countdown.jsx
@@ -1,8 +1,29 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 function Countdown() {
+
+    const [timer, setTimer] = useState({hours:0,minute:0,seconds:0})
+
+    const startTimer =()=>{
+            
+    }
+
   return (
-    <div>Countdown</div>
+    <div style={{display:'flex',justifyContent:'center', alignItems:'center'}}>
+        <div>
+           <div><h1>Countdown Timer</h1></div>
+            <div style={{width:'40%',display:'flex',justifyContent:'center', alignItems:'center'}}>
+                <input type="text"  value={timer.hours} id="hours" />
+                <input type="text"  value={timer.minute} id="minute" />
+                <input type="text"  value={timer.seconds} id="seconds" />
+            </div>
+            <div  style={{display:'flex',justifyContent:'center', alignItems:'center'}}>
+                <button onClick={startTimer}>Start</button>
+                <button>Pause</button>
+                <button>Resume</button>
+            </div>
+        </div>
+    </div>
   )
 }
 
