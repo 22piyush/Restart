de304c1b0698efc2ca6d774a53fa6865a856dcba
22piyush <piyushaglawe05@gmail.com>
2025-12-25 22:20:36 +0530

    done

--- redux-toolkit/src/features/todo/todoSlice.js
+++ redux-toolkit/src/features/todo/todoSlice.js
@@ -1,2 +1,18 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, nanoid} from "@reduxjs/toolkit";
 
+const initialState = {
+    todos: [{ id: 1,  text: "Hello World" }]
+}
+
+function sayHello(){
+    console.log();
+}
+
+export const todoSlice = createSlice({
+    name: 'todo',
+    initialState,
+    reducers: {
+        addTodo: () => {},
+        removeTodo: () => {}
+    }
+});
