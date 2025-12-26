7daed2535b535aec5872abae74640fb550739ef4
22piyush <piyushaglawe05@gmail.com>
2025-12-26 22:45:34 +0530

    done

--- HOOKS-REACT/react-hooks/src/components/FormHandling.jsx
+++ HOOKS-REACT/react-hooks/src/components/FormHandling.jsx
@@ -3,7 +3,33 @@ import React from 'react'
 function FormHandling() {
   return (
     <div>
-        
+        <h2>Form with checkbox, radio & select</h2>
+
+        <label>
+            <input type="radio" name="gender" value="Male"/>
+            Male
+        </label>
+
+        <label>
+            <input type="radio" name="gender" value="Female"/>
+            Female
+        </label>
+
+        Country: 
+        <select name="country">
+            <option value="India">India</option>
+            <option value="USA">USA</option>
+            <option value="UK">UK</option>
+        </select>
+
+        <label>
+            <input type='checkbox' name="agree"/>
+            I agree to terms and conditions
+        </label>
+        <br/>
+
+        <button type='submit'>Submit</button>
+
     </div>
   )
 }
