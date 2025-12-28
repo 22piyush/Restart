668b05040cb213dd8fe0a1d6a038ee921eb455f4
22piyush <piyushaglawe05@gmail.com>
2025-12-28 14:13:05 +0530

    done

---
+++ Authentication/tiny_tours/index.js
@@ -0,0 +1,19 @@
+import express from "express";
+import dotenv from "dotenv";
+import cors from "express";
+
+dotenv.config();
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+const PORT = process.env.PORT || 6000;
+
+app.get("/", (req, res) => {
+    res.send("Hello World");
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
