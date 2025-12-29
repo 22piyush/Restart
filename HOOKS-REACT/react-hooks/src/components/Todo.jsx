a16d3fc9cf9d8a36fb901dd08f5df26dc78d58a2
22piyush <piyushaglawe05@gmail.com>
2025-12-29 21:03:33 +0530

    done

--- HOOKS-REACT/react-hooks/src/components/Todo.jsx
+++ HOOKS-REACT/react-hooks/src/components/Todo.jsx
@@ -1,11 +1,45 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 function Todo() {
 
+    const [newTodo, setNewTodo] = useState("");
+    const [todos, setTodos] = useState([]);
     
+    const handleSubmit =(e)=> {
+        e.preventDefault();
+
+        if(newTodo){
+            setNewTodo([...todos , {text: e.target.value , completed: false}]);
+            setNewTodo("");
+        }
+    }
+
+    const handleDelete =(index) => {
+        
+    }
 
   return (
-    <div>Todo</div>
+    <div>
+        <div>
+            <h1>Todo App</h1>
+            <form onSubmit={handleSubmit}>
+                <input type='text' placeholder='Add new todo' onChange={(e)=> setNewTodo(e.target.value)} value={newTodo}/>
+                <button type="submit">Add Todo</button>
+            </form>
+        </div>
+        <div>
+            <ul>
+                {
+                    todos.map((todo, index) => {
+                        <li key={index}>
+                            <span>{todo.text}</span>
+                            <button onClick={()=>handleDelete(index)}>Delete</button>
+                        </li>
+                    })
+                }
+            </ul>
+        </div>
+    </div>
   )
 }
 
