9cbb4f7fa3abdbf2965e2db21e19839cb7c00ad0
22piyush <piyushaglawe05@gmail.com>
2025-12-29 22:35:27 +0530

    done

--- HOOKS-REACT/react-hooks/src/hooks/UseEffect.jsx
+++ HOOKS-REACT/react-hooks/src/hooks/UseEffect.jsx
@@ -2,16 +2,25 @@ import React, { useEffect, useState } from 'react'
 
 function UseEffect() {
 
-    const [count, setCount] = useState(0);
+    const [width, setWidth] = useState(window.innerWidth);
 
-   useEffect(()=>{
-        console.log("Component Re-rendered!");
-   }) 
+
+    useEffect(() => {
+        
+        const handleResize = () =>{
+            setWidth(window.innerWidth)
+        }
+        window.addEventListener("resize", handleResize);
+
+    }, []);
+
+
+
 
   return (
     <div>
-        <h2>Count: {count}</h2>
-        <button onClick={()=>setCount(count + 1)}>Click</button>
+        <h2>Window width tracker</h2>
+        <p>Current Width : {width}px</p>
     </div>
   )
 }
