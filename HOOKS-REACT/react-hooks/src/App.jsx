f9501d3844e0e0bf8c3f59db21e533bbe3b2423c
22piyush <piyushaglawe05@gmail.com>
2025-12-30 20:54:52 +0530

    done

--- HOOKS-REACT/react-hooks/src/App.jsx
+++ HOOKS-REACT/react-hooks/src/App.jsx
@@ -11,7 +11,7 @@ import UseEffect from "./hooks/UseEffect";
 import UseMemo from "./hooks/UseMemo";
 import UseState from "./hooks/useState";
 
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Link , useParams } from "react-router-dom";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Contact from "./pages/Contact";
@@ -19,8 +19,11 @@ import Contact from "./pages/Contact";
 
 function App() {
 
-  // const fruit = ["Apple", "Banana", "Orange"];
-  // let newName = "Piyush"
+  function User(){
+    console.log(useParams());
+    const { id } = useParams();
+    return <h2>User Profile for ID: {id}</h2>
+  }
 
   return(
     <>
@@ -56,10 +59,20 @@ function App() {
       {/* ROUTING  */}
 
       <BrowserRouter>
+
+          <h1>React Router Example</h1>
+          <nav>
+            <Link to={"/"}>Home</Link> |
+            <Link to={"/about"}>About</Link> |
+            <Link to={"/contact"}>Contact</Link> |
+            <Link to="/user/10">User</Link> 
+          </nav>
+
         <Routes>
           <Route path="/" element={<Home/>} />
           <Route path="/about" element={<About/>} />
           <Route path="/contact" element={<Contact/>} />
+          <Route path="/user/:id" element={<User/>} />
         </Routes>
       </BrowserRouter>
 
