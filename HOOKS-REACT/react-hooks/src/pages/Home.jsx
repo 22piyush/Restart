9fb610f3eede7ba8b4165891a1232ed40a63cb5f
22piyush <piyushaglawe05@gmail.com>
2025-12-30 21:28:56 +0530

    done

--- HOOKS-REACT/react-hooks/src/pages/Home.jsx
+++ HOOKS-REACT/react-hooks/src/pages/Home.jsx
@@ -1,8 +1,19 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
 
 function Home() {
+
+  const navigate = useNavigate();
+
+  const goToAbout =()=>{
+    navigate("/about");
+  }
+
   return (
-    <div>Home</div>
+    <div>
+      <h2>Welcome to Home Page</h2>
+      <button onClick={goToAbout}>Go to About</button>
+    </div>
   )
 }
 
