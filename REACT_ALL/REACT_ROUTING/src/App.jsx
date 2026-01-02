9e92d4624deca282cf7692df6e66058d9ff170cd
22piyush <piyushaglawe05@gmail.com>
2026-01-02 20:26:18 +0530

    done

--- REACT_ALL/REACT_ROUTING/src/App.jsx
+++ REACT_ALL/REACT_ROUTING/src/App.jsx
@@ -1,14 +1,19 @@
 import React from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Header from './components/Header'
 
 function App() {
   return (
     <div>
         <BrowserRouter>
             <Routes>
-                <Route>
-                    
-                </Route>
+              {/* Show Inside Header  */}
+              <Route path='/' element={<Header/>}>
+                  
+
+
+              </Route>
+
             </Routes>
         </BrowserRouter>
     </div>
--- REACT_ALL/REACT_ROUTING/src/components/Header.jsx
+++ REACT_ALL/REACT_ROUTING/src/components/Header.jsx
@@ -3,9 +3,15 @@ import React from 'react'
 function Header() {
   return (
     <div>
-        <nav>
-            
-        </nav>
+        <header>
+           <h4>React Router</h4>
+            <nav>
+               <Link to={"/"}>Home</Link>
+               <Link to={"/about"}>About</Link>
+               <Link to={"/contact"}>Contact</Link>
+               <Link to={"/user"}>User</Link>
+            </nav>
+        </header>
     </div>
   )
 }
