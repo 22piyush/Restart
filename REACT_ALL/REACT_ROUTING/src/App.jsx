92a15e71ea2db152cc45761901784a57257760be
22piyush <piyushaglawe05@gmail.com>
2026-01-02 20:43:46 +0530

    done

--- REACT_ALL/REACT_ROUTING/src/App.jsx
+++ REACT_ALL/REACT_ROUTING/src/App.jsx
@@ -1,6 +1,9 @@
 import React from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Header from './components/Header'
+import Home from "./pages/Home"
+import About from "./pages/About"
+import Contact from './pages/Contact'
 
 function App() {
   return (
@@ -10,10 +13,20 @@ function App() {
               {/* Show Inside Header  */}
               <Route path='/' element={<Header/>}>
                   
+                {/* Main Page added  */}
+                <Route path="/" element={<Home/>}/>
+
+                {/* Prefix */}
+                <Route path="in">
+                  <Route path="/in/user">
+                    <Route path='/in/user/about' element={<About/>}/>
+                  </Route>
+                </Route>
+
+                <Route path='/contact' element={<Contact/>}/>
 
 
               </Route>
-
             </Routes>
         </BrowserRouter>
     </div>
--- REACT_ALL/REACT_ROUTING/src/components/Header.jsx
+++ REACT_ALL/REACT_ROUTING/src/components/Header.jsx
@@ -1,17 +1,21 @@
 import React from 'react'
+import { Link, Outlet } from 'react-router-dom'
 
 function Header() {
   return (
     <div>
-        <header>
+        <header style={{height:'60px',background:'gray',display:'flex',justifyContent:'space-between',alignItems:'center',padding:'0px 20px 0px 20px'}}>
            <h4>React Router</h4>
-            <nav>
+            <nav style={{display:'flex',gap:'30px'}}>
                <Link to={"/"}>Home</Link>
-               <Link to={"/about"}>About</Link>
+               <Link to={"/in/user/about"}>About</Link>
                <Link to={"/contact"}>Contact</Link>
                <Link to={"/user"}>User</Link>
             </nav>
         </header>
+        <main>
+          <Outlet/>
+        </main>
     </div>
   )
 }
