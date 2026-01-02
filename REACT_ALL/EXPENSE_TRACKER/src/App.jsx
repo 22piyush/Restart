974c4d446bed801ee80241348facdb4b9b2cc650
22piyush <piyushaglawe05@gmail.com>
2026-01-02 21:50:20 +0530

    done

--- REACT_ALL/EXPENSE_TRACKER/src/App.jsx
+++ REACT_ALL/EXPENSE_TRACKER/src/App.jsx
@@ -1,10 +1,29 @@
 import React from 'react'
+import { useState, useEffect } from 'react';
 
 function App() {
+
+
     return (
-        <div>
-            
-        </div>
+        <>
+            <div>
+                <h3>🫰 Expense Tracker</h3> 
+                <div>
+                    <form>
+                        <input type='text' placeholder='Expense Title' />
+                        <input type='text' placeholder='Amount' />
+                        <button type='submit'>Add Expense</button>
+                    </form>
+                   <div>
+                    <h3>Total Expense: Rs {750.00}</h3>
+                    <div>
+                        <div><span>Internet Charges Rs. {500}</span></div>
+                        <div><span>Train Ticket Rs. {700}</span></div>
+                    </div>
+                   </div>
+                </div> 
+            </div>   
+        </>
     )
 }
 
