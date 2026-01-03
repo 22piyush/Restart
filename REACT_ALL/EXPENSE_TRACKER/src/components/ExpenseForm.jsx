f5ef97a326220eb66bcfc84b6e9c0f8af6d6b094
22piyush <piyushaglawe05@gmail.com>
2026-01-03 11:42:14 +0530

    done

--- REACT_ALL/EXPENSE_TRACKER/src/components/ExpenseForm.jsx
+++ REACT_ALL/EXPENSE_TRACKER/src/components/ExpenseForm.jsx
@@ -1,16 +1,51 @@
-import React from 'react'
-import { useState, useEffect } from 'react'
+import React from "react";
+import { useState, useEffect, useRef } from "react";
 
-function ExpenseForm() {
+function ExpenseForm({ onAddExpense }) {
+  const [title, setTitle] = useState("");
+  const [amount, setAmount] = useState("");
+  const titleRef = useRef();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (!title || !amount) return alert("Please fill all fields.");
+
+    const newExpense = {
+      id: Date.now(),
+      // we use dirsct if name same of veriable and key
+      title,
+      amount: parseFloat(amount),
+    };
+
+    onAddExpense(newExpense);
+
+    setTitle("")
+    setAmount("")
+
+    titleRef.current.focus();
+  };
+
   return (
     <div>
-        <form>
-            <input type='text' placeholder='Expense Title' />
-            <input type='text' placeholder='Amount' />
-            <button type='submit'>Add Expense</button>
-        </form>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="Expense Title"
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+          ref={titleRef}
+        />
+        <input
+          type="text"
+          placeholder="Amount"
+          value={amount}
+          onChange={(e) => setAmount(e.target.value)}
+        />
+        <button type="submit">Add Expense</button>
+      </form>
     </div>
-  )
+  );
 }
 
-export default ExpenseForm
+export default ExpenseForm;
