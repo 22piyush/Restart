53a38d844c7475a49104ee63a5e7e7e0d7d83562
22piyush <piyushaglawe05@gmail.com>
2026-01-04 17:11:21 +0530

    done

--- Storage App/backend/middleware/authMiddleware.js
+++ Storage App/backend/middleware/authMiddleware.js
@@ -3,6 +3,7 @@ const User = require("../modules/Users");
 exports.checkAuth = async(req, res, next) => {
   const { token } = req.signedCookie;
   if (!token) {
+    res.clearCookie("token");
     return res.status(401).json({ error: "Not logged in!" });
   }
 
