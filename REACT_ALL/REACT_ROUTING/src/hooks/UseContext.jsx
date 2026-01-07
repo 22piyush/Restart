865ab47ae4caa53116a0b7ba5f7a9dc70385e0e4
22piyush <piyushaglawe05@gmail.com>
2026-01-07 21:53:19 +0530

    done

--- REACT_ALL/REACT_ROUTING/src/hooks/UseContext.jsx
+++ REACT_ALL/REACT_ROUTING/src/hooks/UseContext.jsx
@@ -1,11 +1,33 @@
 import React from 'react'
+import { createContext, useContext } from 'react'
+
+const UserContext = createContext();
 
 function UseContext() {
+
+ const user = "Piyush Aglawe"
+
   return (
-    <div>
-            
-    </div>
+    <>
+        <UserContext.Provider value={user}>
+            <Parent/>
+        </UserContext.Provider>
+    </>
   )
+}
+
+function Parent() {
+    const user = useContext(UserContext);
+    return (<div><Child/> <h3>Name is {user}</h3></div>)
+}
+
+function Child (){
+    return <GrandChild/>
+}
+
+function GrandChild(){
+    const user = useContext(UserContext);
+    return <h3>Name is {user}</h3>
 }
 
 export default UseContext
