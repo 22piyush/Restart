48c512e4c635a052fca6fb2fa8751d58f1c8d7e8
22piyush <piyushaglawe05@gmail.com>
2026-01-07 22:07:07 +0530

    done

--- REACT_ALL/REACT_ROUTING/src/hooks/UseContext.jsx
+++ REACT_ALL/REACT_ROUTING/src/hooks/UseContext.jsx
@@ -1,36 +1,84 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { createContext, useContext } from 'react'
 
-const UserContext = createContext();
+const ThemeContext = createContext();
 
 function UseContext() {
 
- const user = {
-    name:"Piyush",
-    role:"Admin"
- }
+    const [theme, setTheme] = useState("light");
 
-  return (
-    <>
-        <UserContext.Provider value={user}>
-            <Parent/>
-        </UserContext.Provider>
-    </>
-  )
+    return (
+        <>
+            <ThemeContext.Provider value={{theme, setTheme}}>
+                <Toolbar/>
+            </ThemeContext.Provider>
+        </>
+    )
 }
 
-function Parent() {
-    const user = useContext(UserContext);
-    return (<div> <h3>Name is {user.name}</h3> <Child/></div>)
+function Toolbar() {
+    return (<div><ThemeButton/></div>)
 }
 
-function Child (){
-    return <GrandChild/>
+function ThemeButton (){
+
+    const {theme, setTheme} = useContext(ThemeContext);
+
+    return(
+        <button 
+        onClick={() => setTheme(theme == "light" ? "dark" : "light")}
+        style={{
+            backgroundColor:theme == "light" ? "#ffffff" : "#333",
+            color:theme == "light" ? "#333" : "#ffffff"
+        }}
+        >Current Theme: {theme}</button>
+    )
 }
 
-function GrandChild(){
-    const user = useContext(UserContext);
-    return <h3>Name is {user.role}</h3>
-}
 
-export default UseContext
+export default UseContext;
+
+
+
+
+
+
+
+
+
+// import React from 'react'
+// import { createContext, useContext } from 'react'
+
+// const UserContext = createContext();
+
+// function UseContext() {
+
+//  const user = {
+//     name:"Piyush",
+//     role:"Admin"
+//  }
+
+//   return (
+//     <>
+//         <UserContext.Provider value={user}>
+//             <Parent/>
+//         </UserContext.Provider>
+//     </>
+//   )
+// }
+
+// function Parent() {
+//     const user = useContext(UserContext);
+//     return (<div> <h3>Name is {user.name}</h3> <Child/></div>)
+// }
+
+// function Child (){
+//     return <GrandChild/>
+// }
+
+// function GrandChild(){
+//     const user = useContext(UserContext);
+//     return <h3>Name is {user.role}</h3>
+// }
+
+// export default UseContext;
