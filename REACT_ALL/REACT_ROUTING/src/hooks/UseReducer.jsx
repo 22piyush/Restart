577107a61acabee63fcc3433050f8eefe4b0c595
22piyush <piyushaglawe05@gmail.com>
2026-01-09 21:00:12 +0530

    done

--- REACT_ALL/REACT_ROUTING/src/hooks/UseReducer.jsx
+++ REACT_ALL/REACT_ROUTING/src/hooks/UseReducer.jsx
@@ -1,4 +1,4 @@
-import React, { useReducer, useState } from 'react'
+import React, { act, useReducer, useState } from 'react'
 
 
 // function Test(){
@@ -15,14 +15,47 @@ import React, { useReducer, useState } from 'react'
 // }
 
 
+function Counter(){
+
+    function reducer(state, action){
+        switch(action.type){
+            case "increment" :
+                return{count: state.count + 1}
+                
+            case "decrement" :
+                return{count: state.count > 0 ? state.count - 1 : state.count}
+
+            case "reset" :
+                return{count: 0}
+            default:
+                return state;
+        }
+    }
+
+    const [state, dispatch] = useReducer(reducer, {count:0});
+
+    return(
+        <div>
+            <h2>Count: {state.count}</h2>
+
+            <button onClick={() => dispatch({ type : "increment" })}>+</button>
+            <button onClick={() => dispatch({ type : "decrement" })}>-</button>
+            <button onClick={() => dispatch({ type : "reset" })}>Reset</button>
+        </div>
+    )
+}
+
+
+
 function UseReducer() {
     const [checked, toggle] = useReducer((checked) => !checked, false);
 
     return(
         <div>
             <input type="checkbox" checked={checked} onChange={toggle}/>
-
              {checked ? "checked" : "Not Checked"}
+
+            <Counter/>
         </div>
     )
 }
