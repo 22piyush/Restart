f37194e8ac66bd4e8649f65ca052d311cfd03275
22piyush <piyushaglawe05@gmail.com>
2026-01-09 20:50:52 +0530

    done

--- REACT_ALL/REACT_ROUTING/src/hooks/UseReducer.jsx
+++ REACT_ALL/REACT_ROUTING/src/hooks/UseReducer.jsx
@@ -1,26 +1,30 @@
-import React, { useState } from 'react'
+import React, { useReducer, useState } from 'react'
 
 
-function Test(){
-    const [checked, setChecked] = useState(false);
+// function Test(){
+//     const [checked, setChecked] = useState(false);
+
+//     return(
+//         <div>
+//             <input type="checkbox" checked={checked}
+//              onChange={() => setChecked(!checked)}/>
+
+//              {checked ? "checked" : "Not Checked"}
+//         </div>
+//     )
+// }
+
+
+function UseReducer() {
+    const [checked, toggle] = useReducer((checked) => !checked, false);
 
     return(
         <div>
-            <input type="checkbox" checked={checked}
-             onChange={() => setChecked(!checked)}/>
+            <input type="checkbox" checked={checked} onChange={toggle}/>
 
              {checked ? "checked" : "Not Checked"}
         </div>
     )
-}
-
-
-function UseReducer() {
-  return (
-    <div>UseReducer
-        <Test/>
-    </div>
-  )
 }
 
 export default UseReducer
