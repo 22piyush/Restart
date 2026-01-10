52d585679e8240ffa405e6013e886175d1b49061
22piyush <piyushaglawe05@gmail.com>
2026-01-10 20:40:22 +0530

    done

---
+++ REACT_ALL/REACT_ROUTING/src/hooks/USECallback/Counter.jsx
@@ -0,0 +1,20 @@
+import { useCallback, useState } from "react"
+
+
+function Counter() {
+
+    const [count, setCount] = useState(0);
+
+    const increment = useCallback(() => {
+        setCount((prev) => prev + 1)
+    }, []);
+
+  return (
+    <div>
+        <h2>Count: {count}</h2>
+        <button onClick={increment}>Increase</button>
+    </div>
+  )
+}
+
+export default Counter
---
+++ REACT_ALL/REACT_ROUTING/src/hooks/USECallback/UseCallback.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Counter from './Counter'
+import Parent from './Parent'
+
+function UseCallback() {
+  return (
+    <div>
+        {/* <Counter/> */}
+        <Parent/>
+    </div>
+  )
+}
+
+export default UseCallback
