ee0e2e64291b621ef4aa07b4d5fdd7b753dbbc5f
22piyush <piyushaglawe05@gmail.com>
2026-01-10 23:26:20 +0530

    done

--- REACT_ALL/REACT_ROUTING/src/hooks/Hooks.jsx
+++ REACT_ALL/REACT_ROUTING/src/hooks/Hooks.jsx
@@ -2,13 +2,13 @@ import React from 'react'
 import USeRef from './USeRef'
 import UseReducer from './UseReducer'
 import UseMemoHook from './MEMOUseHooks/UseMemoHook'
-// import UseCallback from './USECallback/UseCallback'
+import UseCallback from './USECallback/UseCallback'
 import New from './MEMOUseHooks/New/New'
-import UseCallBack from './CallBack/UseCallBack'
+import UseLayoutEffect from './USELAYOUTEFFECT/useLayoutEffect'
 
 function Hooks() {
   return (
-    <div><UseCallBack/></div>
+    <div><UseLayoutEffect/></div>
   )
 }
 
--- REACT_ALL/REACT_ROUTING/src/hooks/USELAYOUTEFFECT/useLayoutEffect.jsx
+++ REACT_ALL/REACT_ROUTING/src/hooks/USELAYOUTEFFECT/useLayoutEffect.jsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import {useLayoutEffect, useRef, useState} from 'react'
+import Example2 from './Example2';
 
-function useLayoutEffect() {
+function UseLayoutEffect() {
+
+    const boxRef = useRef(null);
+    const [height, setHeight] = useState(0);
+
+    useLayoutEffect(() => {
+       setHeight(boxRef.current.clientHeight);
+    },[]); 
+
   return (
-    <div>useLayoutEffect</div>
+    <div>
+        <div ref={boxRef} style={{width:"200px", padding:"20px", background:"#eeee"}}>
+            Hellow World! Measure Me
+        </div>
+        <p>Height: {height}px</p>
+
+        <Example2/>
+    </div>
   )
 }
 
-export default useLayoutEffect
+export default UseLayoutEffect
