29091cf92c3f9759582a3349c1e2eb0f49b1c7b8
22piyush <piyushaglawe05@gmail.com>
2026-01-11 22:38:21 +0530

    done

--- REACT_ALL/REACT_ROUTING/src/hooks/Hooks.jsx
+++ REACT_ALL/REACT_ROUTING/src/hooks/Hooks.jsx
@@ -5,10 +5,11 @@ import UseMemoHook from './MEMOUseHooks/UseMemoHook'
 import UseCallback from './USECallback/UseCallback'
 import New from './MEMOUseHooks/New/New'
 import UseLayoutEffect from './USELAYOUTEFFECT/useLayoutEffect'
+import CustomHook from './CustomHook/CustomHook'
 
 function Hooks() {
   return (
-    <div><UseLayoutEffect/></div>
+    <div><CustomHook/></div>
   )
 }
 
