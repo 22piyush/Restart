767b2dd8a33aeb7794e932a1b171abf96d0bf26a
22piyush <piyushaglawe05@gmail.com>
2026-01-11 23:54:55 +0530

    done

--- REACT_ALL/REACT_ROUTING/src/hooks/CustomHook/useForm.js
+++ REACT_ALL/REACT_ROUTING/src/hooks/CustomHook/useForm.js
@@ -1,6 +1,6 @@
 import { useState } from "react"
 
-function useForm(initialValues = {}) {
+function useForm(initialValues = {}, callback) {
  
     const [values, setValues] = useState();
 
@@ -17,9 +17,14 @@ function useForm(initialValues = {}) {
     // Handle form submit 
     const handleSubmit =(e)=> {
         e.preventDefault();
-
-        
+        if(callback) callback(values);
     }
+
+
+    // Reset Form 
+    const resetForm =()=> setValues(initialValues)
+
+    return {values, handleChange , handleSubmit, resetForm}
 }
 
 export default useForm
