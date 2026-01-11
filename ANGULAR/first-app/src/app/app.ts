bb9c12008f36784e5f0bacabbe17799bffe9908b
22piyush <piyushaglawe05@gmail.com>
2026-01-11 13:46:32 +0530

    done

--- ANGULAR/first-app/src/app/app.ts
+++ ANGULAR/first-app/src/app/app.ts
@@ -1,16 +1,20 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Profile } from './profile/profile';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Profile],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   title = 'first-app';
+  count = 0;
+  counter(type:string){
 
-  updateName(){
+    type == 'minus' ? this.count > 0 && this.count-- : this.count++;
     
   }
+  
 }
