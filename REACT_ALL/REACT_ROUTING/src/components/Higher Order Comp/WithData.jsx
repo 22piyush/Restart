633d185acb607370efb42ffe6010c94597c1a550
22piyush <piyushaglawe05@gmail.com>
2026-01-12 22:16:56 +0530

    done

---
+++ REACT_ALL/REACT_ROUTING/src/components/Higher Order Comp/WithData.jsx
@@ -0,0 +1,19 @@
+import React, { useEffect, useState } from 'react'
+
+function WithData({WrappedComponent, url}) {
+  return function EnhancedComponent(){
+    const [data, setData] = useState([]);
+
+    useEffect(() => {
+
+        fetch(url)
+        .then(res => res.json())
+        .then(data => setData(data)) 
+
+    }, [])
+
+    return <WrappedComponent data={data} />
+  }
+}
+
+export default WithData
