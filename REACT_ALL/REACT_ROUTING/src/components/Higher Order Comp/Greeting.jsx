7cd6a9db23ae90719c104ed1e689a0ffa96239ca
22piyush <piyushaglawe05@gmail.com>
2026-01-12 22:03:13 +0530

    done

---
+++ REACT_ALL/REACT_ROUTING/src/components/Higher Order Comp/Greeting.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Greeting({name}) {
+  return (
+    <div>
+        <div>Name :- {name}</div>
+    </div>
+  )
+}
+
+export default Greeting
--- REACT_ALL/REACT_ROUTING/src/components/Higher Order Comp/Higher.jsx
+++ REACT_ALL/REACT_ROUTING/src/components/Higher Order Comp/Higher.jsx
@@ -1,10 +1,15 @@
-import React from 'react'
+import React from "react";
+import Greeting from "./Greeting";
+import GreetingWithStyle from "./GreetingWithStyle";
 
-// ABOUT Page 
+// ABOUT Page
 function Higher() {
   return (
-    <div>Higher</div>
-  )
+    <div>
+      <Greeting name={"Piyush"} />
+      <GreetingWithStyle name="Sanchit"/>
+    </div>
+  );
 }
 
-export default Higher
+export default Higher;
