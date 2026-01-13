68a46c505743b03b7a53dc03935f1282fa13fccc
22piyush <piyushaglawe05@gmail.com>
2026-01-13 23:04:49 +0530

    done

--- REACT_ALL/QUIZ_APP/src/App.jsx
+++ REACT_ALL/QUIZ_APP/src/App.jsx
@@ -1,18 +1,27 @@
-import React from 'react'
-import Home from './pages/Home'
-import Quiz from './pages/Quiz'
-import Result from './pages/Result'
-import LeaderBoard from './pages/LeaderBoard'
+import React from "react";
+import Home from "./pages/Home";
+import Quiz from "./pages/Quiz";
+import Result from "./pages/Result";
+import LeaderBoard from "./pages/LeaderBoard";
+import { Suspense, lazy } from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+const Result = lazy(() => import("./pages/Result"));
 
 function App() {
   return (
-    <div>
-      {/* <Home/> */}
-      {/* <Quiz/> */}
-      {/* <Result/>
-      <LeaderBoard/> */}
-    </div>
-  )
+    <>
+      <BrowserRouter>
+        <Suspense fallback={<h2 className="text-center mt-5">Loading....</h2>}>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/quiz" element={<Quiz />} />
+            <Route path="/result" element={<Result />} />
+            <Route path="/leaderboard" element={<LeaderBoard />} />
+          </Routes>
+        </Suspense>
+      </BrowserRouter>
+    </>
+  );
 }
 
-export default App
+export default App;
