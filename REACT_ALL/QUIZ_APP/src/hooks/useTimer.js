cf2c3e745c0f2d23b3bb2d548006fe9a7dcdfca0
22piyush <piyushaglawe05@gmail.com>
2026-01-14 21:29:36 +0530

    done

--- REACT_ALL/QUIZ_APP/src/hooks/useTimer.js
+++ REACT_ALL/QUIZ_APP/src/hooks/useTimer.js
@@ -4,14 +4,13 @@ function useTimer({seconds}) {
 
     const [time, setTime] = useState(seconds);
 
-
     useEffect(() => {
         if(time === 0) return;
         const interval = setInterval(() => setTime(prev => prev - 1), 1000);
         return () => clearInterval(interval);
     }, [time]);
 
-  return { time }
+  return { time, reset: () => setTime() }
 }
 
 export default useTimer;
