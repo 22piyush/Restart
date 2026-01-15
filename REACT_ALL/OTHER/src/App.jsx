2e83acd4e4d3d5d0a526642118b9d06b038d97ed
22piyush <piyushaglawe05@gmail.com>
2026-01-15 21:05:28 +0530

    done

--- REACT_ALL/OTHER/src/App.jsx
+++ REACT_ALL/OTHER/src/App.jsx
@@ -1,10 +1,19 @@
-import { createPortal } from "react-dom"
+// import { createPortal } from "react-dom"
+import { useState } from "react"
+
+import PortalTest from "./PortalTest"
 
 function App() {
+
+    const [open, setOpen] = useState(false);
+
   return (
     <div>
         <h1>App Component</h1>
-        { createPortal( <h1>Portal</h1>, document.body ) }
+        {/* { createPortal( <h1>Portal</h1>, document.body ) } */}
+        <PortalTest/>
+
+        <button onClick={() => setOpen(true)}>Open Modal</button>
     </div>
   )
 }
