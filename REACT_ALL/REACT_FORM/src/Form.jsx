f13dbb0132a1570e9f25072e4fc833fe80cd2c83
22piyush <piyushaglawe05@gmail.com>
2026-01-16 11:50:39 +0530

    done

--- REACT_ALL/REACT_FORM/src/Form.jsx
+++ REACT_ALL/REACT_FORM/src/Form.jsx
@@ -1,31 +1,50 @@
 import React from 'react'
-import { useForm } from 'react-hook-form'
+import { useForm } from 'react-hook-form';
+import { DevTool } from "@hookform/devtools";
 
 function Form() {
 
-    const { register, handleSubmit } = useForm();
+    const { register, handleSubmit, control } = useForm();
     
     console.log(useForm());
     
     const onSubmit = (data) => {
-
+        console.log(data);
     }
 
   return (
     <div style={{marginTop:"100px"}}>
-        <form onSubmit={handleSubmit(onSubmit)}>
-            <label htmlFor="">Name</label><br/>
-            <input type="text" id="name" {...register("name")}/>
-            <br/><br/>
-            <label htmlFor="">Email</label><br/>
-            <input type="email" id="email" {...register("email")}/>
-            <br/><br/>
-            <label htmlFor="">Age</label><br/>
-            <input type="text" id="age" {...register("age")}/>
-            <br/><br/>
+        <div>
+            <form onSubmit={handleSubmit(onSubmit)}>
+                <label htmlFor="">Name</label><br/>
+                <input type="text" id="name" 
+                {...register("name", {
+                    required: "Name is required",
 
-            <button type='submit'>Submit</button>
-        </form>
+                })}/>
+                <br/><br/>
+                <label htmlFor="">Email</label><br/>
+                <input type="email" id="email" 
+                {...register("email", {
+                    required: "Email is required",
+                    pattern: {
+                        value : /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
+                        message: "Invalid email address"
+                    }
+                })}/>
+                <br/><br/>
+                <label htmlFor="">Age</label><br/>
+                <input type="text" id="age" 
+                {...register("age", {
+                    required: "Age is required"
+                })}/>
+                <br/><br/>
+
+                <button type='submit'>Submit</button>
+            </form>
+
+            <DevTool control={control} placement='top-left'/>
+        </div>
     </div>
   )
 }
