12449e00b3f05b4538f2b78299fd52945a1cb734
22piyush <piyushaglawe05@gmail.com>
2026-01-17 10:25:00 +0530

    start backend server

--- Javascript/promise.js
+++ Javascript/promise.js
@@ -6,3 +6,25 @@ let p1 = new Promise((resolve, reject) => {
     }, 1 * 1000);
 
 });
+
+let p2 = new Promise((resolve, reject) => {
+
+    setTimeout(() => {
+        console.log("The Second promise has resolve");
+        resolve(20)
+    }, 2 * 1000);
+
+});
+
+let p3 = new Promise((resolve, reject) => {
+
+    setTimeout(() => {
+        console.log("The Second promise has resolve");
+        resolve(30)
+    }, 3 * 1000);
+
+});
+
+
+Promise.all([p1,p2,p3]).then().catch()
+
