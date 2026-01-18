25b8052e8688b334151140bb45a5056898bb90f3
22piyush <piyushaglawe05@gmail.com>
2026-01-18 12:24:12 +0530

    start backend server

--- redux-toolkit/src/App.jsx
+++ redux-toolkit/src/App.jsx
@@ -1,44 +1,9 @@
-import React, { useState } from 'react';
-import Header from './components/Header';
-import Product from './components/Product';
-import Cart from './components/Cart';
-import { useDispatch } from 'react-redux';
+import React from 'react'
 
 function App() {
-  const [cartCount, setCartCount] = useState(0);
-
-  const dispatch = useDispatch();
-
-
-  const products = [
-    { id: 1, name: 'Product 1', price: 100 },
-    { id: 2, name: 'Product 2', price: 150 },
-    { id: 3, name: 'Product 3', price: 200 },
-  ];
-
-  const addToCart = () => setCartCount(cartCount + 1);
-
   return (
-    <div>
-      <Header cartCount={cartCount} />
-      <Cart cartCount={cartCount} />
-      <div style={{
-        display: 'flex',
-        gap: '20px',
-        justifyContent: 'center',
-        marginTop: '100px'
-      }}>
-        {products.map(product => (
-          <Product
-            key={product.id}
-            name={product.name}
-            price={product.price}
-            addToCart={dispatch(addToCart)}
-          />
-        ))}
-      </div>
-    </div>
-  );
+    <div>App</div>
+  )
 }
 
-export default App;
+export default App
