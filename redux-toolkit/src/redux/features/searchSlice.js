b46bb368d477cd12dfc8b5ff55218243744b397a
22piyush <piyushaglawe05@gmail.com>
2026-01-19 22:48:31 +0530

    start frontend devlopment

---
+++ redux-toolkit/src/redux/features/searchSlice.js
@@ -0,0 +1,27 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+
+const searchSlice = createSlice({
+    name:"search",
+    initialState:{
+        query:"",
+        activeTab:"photos",
+        results:[],
+        loading:false,
+        error:null
+    },
+    reducers:{
+        setQuery(state, action){
+
+        },
+        setActiveTabs(state,action){
+
+        },
+        setLoading(state,action){
+            
+        },
+        setError(state, action){
+            
+        }
+    }
+})
