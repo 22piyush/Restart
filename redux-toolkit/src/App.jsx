cd1946b4847579127b343072546e491cca42f671
22piyush <piyushaglawe05@gmail.com>
2026-01-19 23:08:54 +0530

    start redux

--- redux-toolkit/src/App.jsx
+++ redux-toolkit/src/App.jsx
@@ -7,7 +7,7 @@ function App() {
 
 
   return (
-    <div className="h-screen w-full bg-gray-500 text-white px-5">
+    <div className="h-screen w-full bg-gray-500 text-white">
         <SearchBar/>
     </div>
   )
--- redux-toolkit/src/components/SearchBar.jsx
+++ redux-toolkit/src/components/SearchBar.jsx
@@ -1,14 +1,23 @@
-import React from 'react'
+import React, { useState } from "react";
 
 const SearchBar = () => {
+  const [text, setText] = useState("");
+
   return (
     <div>
-        <form>
-            <input type='text' placeholder='Search anything...' />
-            <button>Search</button>
-        </form>
+      <form className="flex gap-5 p-10 bg-gray-900">
+        <input
+          type="text"
+          required
+          placeholder="Search anything..."
+          className="border-2 px-4 py-2 text-xl rounded outline-none w-full"
+        />
+        <button className="active:scale-95 border-2 px-4 py-2 text-xl rounded outline-none cursor-pointer">
+          Search
+        </button>
+      </form>
     </div>
-  )
-}
+  );
+};
 
-export default SearchBar
+export default SearchBar;
