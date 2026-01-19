86f520fd0dbae2e7d7a924a2321f78c1eed44d4e
22piyush <piyushaglawe05@gmail.com>
2026-01-19 21:35:02 +0530

    start frontend devlopment

--- redux-toolkit/src/App.jsx
+++ redux-toolkit/src/App.jsx
@@ -1,26 +1,8 @@
-import React, { useState } from 'react'
-import { useDispatch, useSelector } from 'react-redux'
-import { decrement, increment, incrementByAmount } from './redux/features/counterSlice';
+import React from 'react'
 
 function App() {
-
-  const dispatch = useDispatch();
-  const count = useSelector((state) => state.counter.value);
-
-  const [number, setNumber] = useState(5);
-
   return (
-    <div>
-      <h1>{ count }</h1>
-
-      <button onClick={()=>{ dispatch(increment()) }}>Increment</button>
-      <button onClick={()=>{ dispatch(decrement()) }}>Decrement</button>
-
-
-      <input type="number" value={number} onChange={(e) => {setNumber(e.target.value)}}/>
-
-       <button onClick={()=>{ dispatch(incrementByAmount(Number(number))) }}>Increment by {number}</button>
-    </div>
+    <div>App</div>
   )
 }
 
