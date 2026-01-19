21a9316f1a266def0a755cbee9d43f3bf9799c91
22piyush <piyushaglawe05@gmail.com>
2026-01-19 22:43:10 +0530

    start frontend devlopment

--- redux-toolkit/src/App.jsx
+++ redux-toolkit/src/App.jsx
@@ -1,10 +1,35 @@
 import React from 'react'
+import { fetchPhotos, fetchVideos } from './api/mediaApi';
 
 function App() {
+
+  function getPhotos(){
+    fetchPhotos('cat');
+
+  }
+
+  function getVideos(){
+    fetchVideos('cat');
+  }
+
+
   return (
-    <div className='h-screen w-full bg-gray-500 text-white'>
-      
+    <div className="h-screen w-full bg-gray-500 text-white px-5">
+      <button
+        onClick={getPhotos}
+        className="px-5 py-2 bg-green-400 rounded mr-5 cursor-pointer"
+      >
+        Get Photos
+      </button>
+
+      <button
+        onClick={getVideos}
+        className="px-5 py-2 bg-blue-400 rounded cursor-pointer"
+      >
+        Get Videos
+      </button>
     </div>
+
   )
 }
 
