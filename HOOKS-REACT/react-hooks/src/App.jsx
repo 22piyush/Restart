370d0f1690160af566501826c78f5800f4ef1f10
22piyush <piyushaglawe05@gmail.com>
2026-01-22 00:28:12 +0530

    start backend

--- HOOKS-REACT/react-hooks/src/App.jsx
+++ HOOKS-REACT/react-hooks/src/App.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import UserContextProvider from './context/UserContextProvider'
 
 function App() {
   return (
-    <div>App</div>
+    <UserContextProvider>
+      
+    </UserContextProvider>
   )
 }
 
