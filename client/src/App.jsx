550afd760839e00a80ee7e25950cda7f27370a18
22piyush <piyushaglawe05@gmail.com>
2026-01-26 19:53:48 +0530

    done

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,27 @@
 import React from "react";
 
 function App() {
-  return <div className="m-1 p-4 text-2xl">App</div>;
+  return (
+    <>
+      <div className="h-full">
+        <div className="flex h-full flex-col">
+          {/* Header */}
+          <div className="bg- h-[60px] flex-shrink-0 bg-black text-white">
+            Header
+          </div>
+
+          {/* Content */}
+          <div className="flex-1 overflow-y-auto">
+            <div className="h-[300px] bg-red-500 text-gray-900">App</div>
+            <div className="h-[300px] bg-red-500">App</div>
+            <div className="h-[300px] bg-red-500">App</div>
+            <div className="h-[300px] bg-red-500">App</div>
+            <div className="h-[300px] bg-red-500">App</div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default App;
