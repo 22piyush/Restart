3305512c0cb47794d378a645a27e8e1f7808766e
22piyush <piyushaglawe05@gmail.com>
2026-01-26 19:29:01 +0530

    done

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,7 @@
-import React from 'react'
+import React from "react";
 
 function App() {
-  return (
-    <div>App</div>
-  )
+  return <div className="m-1 p-4 text-2xl">App</div>;
 }
 
-export default App
+export default App;
