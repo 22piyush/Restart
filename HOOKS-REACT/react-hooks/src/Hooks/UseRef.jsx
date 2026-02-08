4ba00e55b09cf6d9cf0728860f8bcc6572347a2b
22piyush <piyushaglawe05@gmail.com>
2026-02-08 15:21:37 +0530

    done

--- HOOKS-REACT/react-hooks/src/Hooks/UseRef.jsx
+++ HOOKS-REACT/react-hooks/src/Hooks/UseRef.jsx
@@ -1,15 +1,21 @@
 import React, { useEffect, useRef, useState } from "react";
 
 function UseRef() {
-  const [count, setCount] = useState(0);
+  const [count, setCount] = useState(0); 
+  // State can Persist the value.
 
   const countRef = useRef(0);
 
+  let counter = 0;
+
   const handleIncrement = () => {
-    // setCount(count + 1);
+    setCount(count + 1);
 
     countRef.current += 1;
-    console.log(countRef.current);
+    console.log(countRef.current, "REF");
+
+    counter += 1;
+    console.log(counter, "Counter");
   };
 
   useEffect(() => {
