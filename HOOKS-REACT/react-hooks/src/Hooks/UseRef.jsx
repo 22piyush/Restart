1eca6e83f7ec42446b62d45eb12b6cb056928b67
22piyush <piyushaglawe05@gmail.com>
2026-02-08 15:12:03 +0530

    done

--- HOOKS-REACT/react-hooks/src/Hooks/UseRef.jsx
+++ HOOKS-REACT/react-hooks/src/Hooks/UseRef.jsx
@@ -1,18 +1,19 @@
-import React, { useRef, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 
 function UseRef() {
+  const [count, setCount] = useState(0);
 
+  const handleIncrement = () => {
+    setCount(count + 1);
+  };
 
-    const [count, setCount] = useState(0);
-
-    const handleIncrement =()=>{
-        setCount(count + 1);
-    }
-
+  useEffect(() => {
+    console.log("re Render hua hai");
+  });
 
   return (
     <div>
-        <h1>{count}</h1>
+      <h1>{count}</h1>
       <button onClick={handleIncrement}>Increment</button>
     </div>
   );
