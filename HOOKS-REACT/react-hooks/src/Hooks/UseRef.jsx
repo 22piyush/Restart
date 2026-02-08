c6981869c421da9025339b9e43a923ecd742f120
22piyush <piyushaglawe05@gmail.com>
2026-02-08 15:09:02 +0530

    done

--- HOOKS-REACT/react-hooks/src/Hooks/UseRef.jsx
+++ HOOKS-REACT/react-hooks/src/Hooks/UseRef.jsx
@@ -1,32 +1,11 @@
 import React, { useRef } from "react";
 
 function UseRef() {
-  const boxRef = useRef();
-  const isRound = useRef(false); // toggle flag
 
-  const handleFocus = () => {
-    if (!isRound.current) {
-      boxRef.current.style.borderRadius = "50%";
-    } else {
-      boxRef.current.style.borderRadius = "0";
-    }
-
-    isRound.current = !isRound.current; // toggle value
-  };
 
   return (
     <div>
-      <div
-        ref={boxRef}
-        style={{
-          width: "300px",
-          aspectRatio: "1/1",
-          background: "red",
-          transition: "0.3s"
-        }}
-      ></div>
-
-      <button onClick={handleFocus}>Toggle Shape</button>
+      
     </div>
   );
 }
