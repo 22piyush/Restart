e54135322d60a793d95712e89f00bfdcbca05072
22piyush <piyushaglawe05@gmail.com>
2026-02-08 15:37:40 +0530

    done

--- HOOKS-REACT/react-hooks/src/Hooks/UseRef.jsx
+++ HOOKS-REACT/react-hooks/src/Hooks/UseRef.jsx
@@ -1,7 +1,8 @@
 import React, { useEffect, useRef, useState } from "react";
 
 function UseRef() {
-  const [count, setCount] = useState(0);
+  
+    const renderCountRef = useRef(0);
   // State can Persist the value.
 
   const [randomNumber, setRandomNumber] = useState(0);
@@ -13,11 +14,13 @@ function UseRef() {
 
   useEffect(() => {
     console.log("Re Render Triggred");
+    renderCountRef.current += 1 ;
   });
 
   return (
     <div>
       <h1>{randomNumber}</h1>
+      <h1>Re Render Count {renderCountRef.current}</h1>
       <button onClick={generateRandomNumber}>Generate</button>
     </div>
   );
