5fdcbe3d820a230b87d891f4845b5f9220744482
22piyush <piyushaglawe05@gmail.com>
2026-02-18 21:56:53 +0530

    done

--- redux-toolkit/src/App.jsx
+++ redux-toolkit/src/App.jsx
@@ -1,9 +1,30 @@
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
 
 function App() {
+
+  const [count, setCount] = useState(0);
+  // const countRef = useRef(0);
+
+  let countvar = 0;
+
+  const handleFocus = () => {
+    setCount(count + 1);
+    // countRef.current += 1;
+    // console.log(countRef.current);
+
+    console.log(countvar += 1);
+    
+    
+  };
+  
+  useEffect(()=>{
+    console.log("Re Render Huva hai");
+  });
+
   return (
     <div>
-      <input type="text" placeholder="Enter Name" />
+      {/* <h1>{countRef.current}</h1> */}
+      <button onClick={handleFocus}>Focus</button>
     </div>
   );
 }
