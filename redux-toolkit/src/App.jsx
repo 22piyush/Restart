232233fe72e4840c7061d152949c0e92a1e07ca9
22piyush <piyushaglawe05@gmail.com>
2026-02-18 21:21:55 +0530

    done

--- redux-toolkit/src/App.jsx
+++ redux-toolkit/src/App.jsx
@@ -1,9 +1,11 @@
-import React from 'react'
+import React from "react";
 
 function App() {
   return (
-    <div>App</div>
-  )
+    <div>
+      <input type="text" placeholder="Enter Name" />
+    </div>
+  );
 }
 
-export default App
+export default App;
