d9bc065472beb40f9a3633397d6d86639faa9de6
22piyush <piyushaglawe05@gmail.com>
2026-02-22 19:52:52 +0530

    done

--- REACT_ALL/REACT_ROUTING/src/App.jsx
+++ REACT_ALL/REACT_ROUTING/src/App.jsx
@@ -1,65 +1,9 @@
-import React from "react";
-import {
-  createBrowserRouter,
-  RouterProvider,
-  Outlet,
-} from "react-router-dom";
-
-import Navbar from "./components/Navbar";
-import Home from "./pages/Home";
-import About from "./pages/About";
-import Contact from "./pages/Contact";
-import DetailContact from "./pages/DetailContact";
-import A1 from "./components/A1";
-import A2 from "./components/A2";
-
-// Layout Component (Navbar + Outlet)
-function Layout() {
-  return (
-    <>
-      <Navbar />
-      <Outlet />
-    </>
-  );
-}
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Layout />,
-    children: [
-      {
-        path: "/",
-        element: <Home />,
-      },
-      {
-        path: "about",
-        element: <About />,
-        children: [
-          {
-            path: "a1",
-            element: <A1 />,
-          },
-          {
-            path: "a2",
-            element: <A2 />,
-          },
-        ],
-      },
-      {
-        path: "contact",
-        element: <Contact />,
-      },
-      {
-        path: "contact/:id",
-        element: <DetailContact />,
-      },
-    ],
-  },
-]);
+import React from 'react'
 
 function App() {
-  return <RouterProvider router={router} />;
+  return (
+    <div>App</div>
+  )
 }
 
-export default App;
+export default App
--- REACT_ALL/REACT_ROUTING/src/main.jsx
+++ REACT_ALL/REACT_ROUTING/src/main.jsx
@@ -1,10 +1,27 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
-import { BrowserRouter } from "react-router-dom";
+import {
+  BrowserRouter,
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Products from "./components/Products.jsx";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+  {
+    path: "/products",
+    element: <Products />,
+  },
+]);
 
 createRoot(document.getElementById("root")).render(
-  // <BrowserRouter>
+  <>
+    <RouterProvider router={router} />
     <App />
-  // </BrowserRouter>,
+  </>,
 );
