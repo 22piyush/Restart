fb0e202eb0606b3690a27655c069a8c158a6c601
22piyush <piyushaglawe05@gmail.com>
2026-02-22 17:52:30 +0530

    done

--- REACT_ALL/REACT_ROUTING/src/components/Navbar.jsx
+++ REACT_ALL/REACT_ROUTING/src/components/Navbar.jsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom'
 
 function Navbar() {
   return (
-    <div>
+    <div style={{display:'flex', gap:'20px'}}>
         <Link to={"/home"}>Home</Link>
         <Link to={"/about"}>About</Link>
         <Link to={"/page"}>Page</Link>
