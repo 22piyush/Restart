859b468827b73b002ab8a5aaafad8ca6b3ffb86c
22piyush <piyushaglawe05@gmail.com>
2026-02-22 22:42:29 +0530

    done

--- REACT_ALL/REACT_CONTEXT/src/Hooks/MyContext.jsx
+++ REACT_ALL/REACT_CONTEXT/src/Hooks/MyContext.jsx
@@ -1,11 +1,9 @@
-import { createContext } from "react";
+import { createContext, useContext } from "react";
 
 const MyContext = createContext();
 
-
-
-
-
-
+const useMyContext = () => {
+  useContext(MyContext);
+};
 
 export { MyContext };
