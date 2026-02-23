17797e1d1d80a8554ea4ddc2b36f576d3e649e77
22piyush <piyushaglawe05@gmail.com>
2026-02-23 22:11:29 +0530

    done

--- REACT_ALL/EXPENSE_TRACKER/src/App.jsx
+++ REACT_ALL/EXPENSE_TRACKER/src/App.jsx
@@ -1,42 +1,9 @@
 import React from 'react'
-import { useState, useEffect } from 'react';
-import ExpenseForm from './components/ExpenseForm';
-import ExpenseList from './components/ExpenseList';
 
 function App() {
-
-    const [expenses, setExpenses] = useState(()=>{
-        const saved = localStorage.getItem("storage_expenses")
-        return saved ? JSON.parse(saved) : []
-    });
-
-    useEffect(()=>{
-        localStorage.setItem("storage_expenses", JSON.stringify(expenses))
-    },[expenses]);
-
-    const addExpense = (expense) => {
-        setExpenses((prev)=>[...prev, expense])
-    }
-
-    const deleteExpense = (id) => {
-        setExpenses(prev => prev.filter(expense => expense.id !== id))
-    }
-
-    const totalExpenses = expenses.reduce((sum, item) => sum + item.amount , 0)
-
-
-    return (
-        <>
-            <div>
-                <h2>🫰 Expense Tracker</h2> 
-                <ExpenseForm  onAddExpense={addExpense}/>  
-                <div>
-                    <h3>Total Expense: Rs {totalExpenses.toFixed(3)}</h3>
-                    <ExpenseList expenses={expenses} ondelete={deleteExpense}/>
-                </div>
-            </div>   
-        </>
-    )
+  return (
+    <div>App</div>
+  )
 }
 
 export default App
