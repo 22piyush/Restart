879e5d18420f48c0f4d0e20b72eeedf70ce10108
22piyush <piyushaglawe05@gmail.com>
2026-02-24 23:30:26 +0530

    done

--- Javascript/index.js
+++ Javascript/index.js
@@ -2,3 +2,13 @@ let add2 = num => num + 2 ;
 let multiply5 = num => num * 5;
 let subtract10 = num => num - 10;
 
+
+
+function compose(...fns){
+
+    
+
+}
+
+
+compose(subtract10, multiply5, add2)
