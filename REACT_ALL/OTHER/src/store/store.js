65c2e67592ff17cb4097cf12c8650578a0ff00f6
22piyush <piyushaglawe05@gmail.com>
2026-02-25 23:24:28 +0530

    done

--- REACT_ALL/OTHER/src/store/store.js
+++ REACT_ALL/OTHER/src/store/store.js
@@ -1,6 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 
-const store = configureStore({});
+const store = configureStore({
+    reducer:{
+        
+    }
+});
 
 
 export default store;
