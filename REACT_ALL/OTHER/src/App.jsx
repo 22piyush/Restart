f928f6f01a997d3b94bc5a28ab31d4e5ed814d59
22piyush <piyushaglawe05@gmail.com>
2026-02-25 23:11:17 +0530

    done

--- REACT_ALL/OTHER/src/App.jsx
+++ REACT_ALL/OTHER/src/App.jsx
@@ -1,51 +1,8 @@
-// import { createPortal } from "react-dom"
-import { useState } from "react"
-
-// import PortalTest from "./PortalTest"
-import Model from "./Model";
-import UserProfile from "./Error Boundries/UserProfile";
-import ErrorBoundry from "./Error Boundries/ErrorBoundry";
+import React from 'react'
 
 function App() {
-
-    const [open, setOpen] = useState(false);
-
-    const userData = {
-      name: "John Doe",
-      age: 25
-    }
-
-    const userData1 = null;
-
   return (
-    <div>
-      <div>
-        <h1>App Component</h1>
-        {/* { createPortal( <h1>Portal</h1>, document.body ) } */}
-        {/* <PortalTest/> */}
-
-        <button onClick={() => setOpen(true)}>Open Modal</button>
-
-        <Model isOpen={open} onClose={() => setOpen(false)}>
-            <h2>Hello from Modal</h2>
-            <p>This is a simple modal without context.</p>
-        </Model>
-      </div>
-
-      <div style={{marginTop:"50px"}}>
-        {/* Error Boundries  */}
-
-        {/* <ErrorBoundry> */}
-          <UserProfile userData={userData}/> 
-        {/* </ErrorBoundry> */}
-
-        {/* <ErrorBoundry fallback={<p>Error in user Profile</p>}> */}
-          <UserProfile userData={userData1}/> 
-        {/* </ErrorBoundry> */}
-
-      </div>
-  
-    </div>
+    <div>App</div>
   )
 }
 
--- REACT_ALL/OTHER/src/main.jsx
+++ REACT_ALL/OTHER/src/main.jsx
@@ -1,11 +1,4 @@
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import ErrorBoundary from './Error Boundries/ErrorBoundry.jsx'
+import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
 
-createRoot(document.getElementById('root')).render( 
-
-<ErrorBoundary>
-    <App /> 
-</ErrorBoundary>
-
-)
+createRoot(document.getElementById("root")).render(<App />);
