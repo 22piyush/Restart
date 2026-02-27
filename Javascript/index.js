d10de4111b9d071a18d8f1b27bc575283d226844
22piyush <piyushaglawe05@gmail.com>
2026-02-27 22:04:16 +0530

    done

---
+++ Javascript/index.js
@@ -0,0 +1,11 @@
+class car {
+    constructor(name, color, milege) {
+        this.name = name;
+        this.color = color;
+        this.milege = milege;
+    }
+
+    start(){
+        console.log(this.name);
+    }
+}
