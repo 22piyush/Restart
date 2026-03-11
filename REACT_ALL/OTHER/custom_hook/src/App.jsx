bcd01b19929bb59662ce656a5b09f30535835f55
22piyush <piyushaglawe05@gmail.com>
2026-03-11 23:13:28 +0530

    done

--- REACT_ALL/OTHER/custom_hook/src/App.jsx
+++ REACT_ALL/OTHER/custom_hook/src/App.jsx
@@ -1,12 +1,16 @@
-import React from 'react'
+import React from "react";
+import { useCounter } from "./customHook/useCounter";
 
 function App() {
+  const { count, increment, decrement, setByValue } = useCounter(0);
 
   return (
     <div>
-        
+      <h1>{count}</h1>
+      <button onClick={increment}>Add</button>
+      <button onClick={decrement}>Remove</button>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
