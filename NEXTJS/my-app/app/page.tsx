5da02240bb7d6a4c6f1cd224666a28ecc92da9d9
22piyush <piyushaglawe05@gmail.com>
2026-03-13 23:47:28 +0530

    done

--- NEXTJS/my-app/app/page.tsx
+++ NEXTJS/my-app/app/page.tsx
@@ -1,8 +1,13 @@
+import Link from 'next/link'
 import React from 'react'
 
 function Page() {
   return (
-    <div>Page Content</div>
+    <div>Page Content
+
+<Link href="/about">About</Link>
+
+    </div>
   )
 }
 
