9766b59e331a4bf1d187559a597ad49f20dbccf0
22piyush <piyushaglawe05@gmail.com>
2026-03-14 20:57:20 +0530

    done

--- REACT_ALL/OTHER/custom_hook/src/App.jsx
+++ REACT_ALL/OTHER/custom_hook/src/App.jsx
@@ -1,14 +1,9 @@
 import React, { useState } from 'react'
-import Alpha from './Alpha'
 
 function App() {
 
-    const [count , setCount] = useState(0)
-    
   return (
     <div>
-        <Alpha data={count}/>
-        <button onClick={()=>setCount(count + 1)}>Click:{count}</button>
     </div>
   )
 }
