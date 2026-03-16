d78ef3de501c16f0ce4774212675a901fec4188e
22piyush <piyushaglawe05@gmail.com>
2026-03-16 22:38:07 +0530

    done

--- NEXTJS/my-app/app/about/page.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function About() {
-  return (
-    <div>Wlcome to about page</div>
-  )
-}
-
-export default About
--- NEXTJS/my-app/app/services/page.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function Services() {
-  return (
-    <div>Wlcome to Services page</div>
-  )
-}
-
-export default Services
