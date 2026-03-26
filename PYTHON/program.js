a3a453bd726ead9e07147a5a8f0fafe27205c221
22piyush <piyushaglawe05@gmail.com>
2026-03-26 20:57:38 +0530

    done

--- PYTHON/program.js
+++
@@ -1,8 +0,0 @@
-let l1 = [1,2,3];
-let l2 = l1;
-l1[0] = 33;
-
-let l2 = [1,2,3]
-
-console.log(l1);
-console.log(l2);
--- PYTHON/program.py
+++ PYTHON/program.py
@@ -1,12 +1,12 @@
 
 l1 = [1,2,3]
 l2 = l1
-l1[0] = 33
 
 l2 = [1,2,3]
 
-print(l1)
-print(l2)
+print(l1 == l2)
+print(l1 is l2)
+
 
 
 
