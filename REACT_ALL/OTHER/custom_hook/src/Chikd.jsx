fa857a454c0854f8107a9c4733168bbf3988b813
22piyush <piyushaglawe05@gmail.com>
2026-04-24 22:23:59 +0530

    done delete

--- REACT_ALL/OTHER/custom_hook/src/Chikd.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-function Chikd() {
-  console.log("Child render");
-
-  return <div>Chikd</div>;
-}
-
-export default React.memo(Chikd);
--- REACT_ALL/REACT_CONTEXT/src/main.jsx
+++
@@ -1,9 +0,0 @@
-import { createRoot } from "react-dom/client";
-import App from "./App.jsx";
-import { MyContextProvider } from "./Hooks/MyContext.jsx";
-
-createRoot(document.getElementById("root")).render(
-  <MyContextProvider>
-    <App />
-  </MyContextProvider>,
-);
