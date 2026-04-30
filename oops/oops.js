5564269c5863def4f6fb562716052e6e37afb3bc
22piyush <piyushaglawe05@gmail.com>
2026-04-30 20:59:13 +0530

    done delete

--- oops/oops.js
+++ oops/oops.js
@@ -1,5 +1,19 @@
 
 
-class CreateUser{
-    
+class CreateUser {
+
+    constructor(a, b, c) {
+        console.log(a, b, 23);
+    }
+
+    getBirthYear() {
+        return new Date().getFullYear() - this.age;
+    }
+    getFullYear() {
+        return this.firstName + ' ' + this.lastName
+    }
+
 }
+
+const user1 = new CreateUser("Piyush", "Aglawe", 23)
+
