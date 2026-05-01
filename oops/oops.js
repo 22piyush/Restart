5d54b11804ac034c3424480b201e4f4a63dd79c7
22piyush <piyushaglawe05@gmail.com>
2026-05-01 06:45:49 +0530

    done delete

--- oops/oops.js
+++ oops/oops.js
@@ -1,27 +1,36 @@
 
 
-class CreateUser {
-    constructor(a, b, c) {
-        this.firstName = a
-        this.lastName = b
-        this.age = c
-    }
+// class CreateUser {
+//     constructor(a, b, c) {
+//         this.firstName = a
+//         this.lastName = b
+//         this.age = c
+//     }
 
-    static hi = "Hello"
+//     static hi = "Hello"
 
-    static {
-        var a = 20
+//     static {
+//         var a = 20
 
-    }
+//     }
 
-    getBirthYear() {
-        return new Date().getFullYear() - this.age;
-    }
-    getFullYear() {
-        return this.firstName + ' ' + this.lastName
-    }
+//     getBirthYear() {
+//         return new Date().getFullYear() - this.age;
+//     }
+//     getFullYear() {
+//         return this.firstName + ' ' + this.lastName
+//     }
 
+// }
+
+// const user1 = new CreateUser("Piyush", "Aglawe", 23)
+
+
+
+
+
+const user = {
+    firstName: 'Anurag',
+    lastName: "Singh"
 }
-
-const user1 = new CreateUser("Piyush", "Aglawe", 23)
 
