49f5143e0e08da4f4b4e10427f71b830b54d5437
22piyush <piyushaglawe05@gmail.com>
2026-05-05 21:32:49 +0530

    done delete

--- my-app/src/app/app.ts
+++ my-app/src/app/app.ts
@@ -1,9 +1,12 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Navbar } from "./components/navbar/navbar";
+import { Footer } from "./components/footer/footer";
+import { Body } from "./components/body/body";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Navbar, Footer, Body],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
