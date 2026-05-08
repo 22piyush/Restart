126ba98721c9f9bf354f49644a319eccd595cd32
22piyush <piyushaglawe05@gmail.com>
2026-05-08 07:14:43 +0530

    done delete

--- my-app/src/app/components/assignments/assignments.ts
+++ my-app/src/app/components/assignments/assignments.ts
@@ -17,6 +17,13 @@ export class Assignments {
   fahValue:number = 0;
   selectedColor:string = ''
   fontSize:number = 20;
+  countText:string = '';
+  countWord:number = 0;
+  countCharacter:number = 0;
+  firstValue:number = 0;
+  secondValue:number = 0;
+  operatorValue:string = '';
+  calculatedValue:number = 0
 
   toggleText(){
     this.showText = !this.showText;
@@ -48,6 +55,29 @@ export class Assignments {
     }
      if(type == 'remove'){
       this.fontSize--;
+    }
+  }
+
+  countTextArea(){
+    console.log(this.countText);
+
+    this.countWord = this.countText.split(' ').filter(item => item !== '').length;
+    this.countCharacter = this.countText.split('').filter(item => item !== ' ').length;
+    
+  }
+
+  calculation(){
+    if(this.operatorValue == '+'){
+      this.calculatedValue = this.firstValue + this.secondValue;
+    }
+    if(this.operatorValue == '-'){
+      this.calculatedValue = this.firstValue - this.secondValue;
+    }
+    if(this.operatorValue == '*'){
+      this.calculatedValue = this.firstValue * this.secondValue;
+    }
+    if(this.operatorValue == '/'){
+      this.calculatedValue = this.firstValue / this.secondValue;
     }
   }
 }
