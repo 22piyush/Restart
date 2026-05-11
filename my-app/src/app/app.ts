bc475a7d466e78815d11af5136b3b2081d522265
22piyush <piyushaglawe05@gmail.com>
2026-05-11 07:07:11 +0530

    done delete

--- my-app/src/app/app.ts
+++ my-app/src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { Assignments } from './components/assignments/assignments';
 import { AssignmentsTwo } from './components/assignments-two/assignments-two';
+import { UserList } from './components/user-list/user-list';
 
 @Component({
   selector: 'app-root',
-  imports: [AssignmentsTwo, Assignments ],
+  imports: [AssignmentsTwo, Assignments , UserList],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
