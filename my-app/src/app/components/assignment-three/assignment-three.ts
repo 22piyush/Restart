5d2f7ccbb1c3c73e8329a4c97845bda17ebc8c41
22piyush <piyushaglawe05@gmail.com>
2026-05-11 23:12:44 +0530

    done delete

--- my-app/src/app/components/assignment-three/assignment-three.ts
+++ my-app/src/app/components/assignment-three/assignment-three.ts
@@ -4,10 +4,11 @@ import { CommonModule } from '@angular/common';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { faTrash, faPenToSquare, faEye } from '@fortawesome/free-solid-svg-icons';
 import Snackbar from 'awesome-snackbar';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-assignment-three',
-  imports: [CommonModule, FontAwesomeModule],
+  imports: [CommonModule, FontAwesomeModule, FormsModule],
   templateUrl: './assignment-three.html',
   styleUrl: './assignment-three.css',
 })
@@ -18,9 +19,57 @@ export class AssignmentThree {
   editIcon = faPenToSquare;
   viewIcon = faEye;
 
-  showModel:boolean = false
+  selectedEmployee: any;
+  showModel: boolean = false;
+  showViewModel: boolean = false;
+  showEmpModel: boolean = false;
 
-  filterEmployee(data: any) {
-    this.empArr = this.empArr.filter((e: any) => e.email !== data.email);
+  employee = {
+    name: '',
+    username: '',
+    email: '',
+  };
+
+  addEmployee() {
+    const newEmp = {
+      id: this.empArr.length + 1,
+      name: this.employee.name,
+      username: this.employee.username,
+      email: this.employee.email,
+    };
+
+    this.empArr.push(newEmp);
+
+    // reset form
+    this.employee = {
+      name: '',
+      username: '',
+      email: '',
+    };
+  }
+
+  openEmpBox(data: any, type: string) {
+    this.selectedEmployee = data;
+    if (type == 'delete') {
+      this.showViewModel = false;
+      this.showModel = true;
+    } else {
+      this.showModel = false;
+      this.showViewModel = true;
+    }
+  }
+
+  confirmDelete() {
+    this.empArr = this.empArr.filter((e: any) => e.email !== this.selectedEmployee.email);
+
+    this.showModel = false;
+
+    new Snackbar('Employee deleted successfully');
+  }
+
+  cancelDelete() {
+    this.showModel = false;
+    this.showViewModel = false;
+    this.showEmpModel = false;
   }
 }
