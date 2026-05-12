3c1365f33e1e708abb972e17e2e681b3588e76ef
22piyush <piyushaglawe05@gmail.com>
2026-05-12 08:45:39 +0530

    done delete

--- my-app/src/app/components/pipes-demo/pipes-demo.ts
+++ my-app/src/app/components/pipes-demo/pipes-demo.ts
@@ -1,9 +1,10 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-pipes-demo',
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './pipes-demo.html',
   styleUrl: './pipes-demo.css',
 })
@@ -14,4 +15,9 @@ export class PipesDemo {
   slary:number = 1200
 
   dataObj = new Date();
+
+  cars = ['Tata', 'Honda', 'Maruti' , 'Toyota']
+
+  msg:string  = "Hello Angular"
+
 }
