ff361e7a10f56e0d046bedfd5418d0fcf6377f55
22piyush <piyushaglawe05@gmail.com>
2026-05-12 07:56:56 +0530

    done delete

--- my-app/src/app/components/pipes-demo/pipes-demo.ts
+++ my-app/src/app/components/pipes-demo/pipes-demo.ts
@@ -1,9 +1,14 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-pipes-demo',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './pipes-demo.html',
   styleUrl: './pipes-demo.css',
 })
-export class PipesDemo {}
+export class PipesDemo {
+
+  userName:string = 'saCHIN TenDulkar'
+
+}
