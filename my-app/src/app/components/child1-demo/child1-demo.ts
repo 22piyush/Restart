13953a1c5246daea85f60d69e3d2f6efdbacb625
22piyush <piyushaglawe05@gmail.com>
2026-05-13 08:46:04 +0530

    done delete

--- my-app/src/app/components/child1-demo/child1-demo.ts
+++ my-app/src/app/components/child1-demo/child1-demo.ts
@@ -3,7 +3,10 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-child1-demo',
   imports: [],
+  inputs:['parent'],
   templateUrl: './child1-demo.html',
   styleUrl: './child1-demo.css',
 })
-export class Child1Demo {}
+export class Child1Demo {
+
+}
--- my-app/src/app/components/parent-demo/parent-demo.ts
+++ my-app/src/app/components/parent-demo/parent-demo.ts
@@ -1,9 +1,15 @@
 import { Component } from '@angular/core';
+import { Child1Demo } from '../child1-demo/child1-demo';
+import { Child2Demo } from '../child2-demo/child2-demo';
 
 @Component({
   selector: 'app-parent-demo',
-  imports: [],
+  imports: [Child1Demo,Child2Demo],
   templateUrl: './parent-demo.html',
   styleUrl: './parent-demo.css',
 })
-export class ParentDemo {}
+export class ParentDemo {
+
+  parent:string = "Parent"
+
+}
