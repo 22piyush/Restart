b7d47445959b861d2b0c78f4d88959ada8d52b66
22piyush <piyushaglawe05@gmail.com>
2026-05-16 14:20:37 +0530

    done

--- angular-new-project/src/app/body/body/body.ts
+++ angular-new-project/src/app/body/body/body.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { Category } from '../../components/category/category';
 import { EmployeeCrud } from '../../components/employee-crud/employee-crud';
+import { DataBinding } from '../../components/data-binding/data-binding';
 
 @Component({
   selector: 'app-body',
-  imports: [Category, EmployeeCrud],
+  imports: [Category, EmployeeCrud, DataBinding],
   templateUrl: './body.html',
   styleUrl: './body.css',
 })
