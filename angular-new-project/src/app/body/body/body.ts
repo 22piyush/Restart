140b2ad8266e617d6137d92f243545408a95494a
22piyush <piyushaglawe05@gmail.com>
2026-05-16 17:20:14 +0530

    done

--- angular-new-project/src/app/body/body/body.ts
+++ angular-new-project/src/app/body/body/body.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { DataBinding } from '../../components/data-binding/data-binding';
 import { Directives } from '../../components/directives/directives';
+import { UserList } from '../../components/user-list/user-list';
 
 @Component({
   selector: 'app-body',
-  imports: [DataBinding, Directives],
+  imports: [DataBinding, Directives, UserList],
   templateUrl: './body.html',
   styleUrl: './body.css',
 })
---
+++ angular-new-project/src/app/components/user-list/user-list.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import userData from './user_data.json';
+import data from './product_data'
+
+@Component({
+  selector: 'app-user-list',
+  imports: [],
+  templateUrl: './user-list.html',
+  styleUrl: './user-list.css',
+})
+export class UserList {
+  productList = userData;
+}
