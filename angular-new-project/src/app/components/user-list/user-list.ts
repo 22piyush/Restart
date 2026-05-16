7548101ecaa6bab958c0dd841f88c08f057a727f
22piyush <piyushaglawe05@gmail.com>
2026-05-16 17:54:58 +0530

    done

--- angular-new-project/src/app/components/user-list/user-list.ts
+++ angular-new-project/src/app/components/user-list/user-list.ts
@@ -1,13 +1,17 @@
 import { Component } from '@angular/core';
 import userData from './user_data.json';
 import data from './product_data'
+import { NgxPaginationModule } from 'ngx-pagination';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-user-list',
-  imports: [],
+  imports: [NgxPaginationModule, CommonModule],
   templateUrl: './user-list.html',
   styleUrl: './user-list.css',
 })
 export class UserList {
   productList = userData;
+
+  p:any;
 }
