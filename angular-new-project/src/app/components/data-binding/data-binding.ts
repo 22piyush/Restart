0cfc1d6ff94b95fce21cd5788ca1d609a1817f8b
22piyush <piyushaglawe05@gmail.com>
2026-05-16 14:52:17 +0530

    done

--- angular-new-project/src/app/components/data-binding/data-binding.ts
+++ angular-new-project/src/app/components/data-binding/data-binding.ts
@@ -1,9 +1,20 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-data-binding',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './data-binding.html',
   styleUrl: './data-binding.css',
 })
-export class DataBinding {}
+export class DataBinding {
+  courseName: string = 'Angular';
+  img_url:string = 'ddwddw'
+
+  flag:boolean = true;
+
+  form_data = "Angular"
+
+  num1 = 1
+  num2 = 2
+}
