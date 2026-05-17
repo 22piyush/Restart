b281220f31319e3db6d226bfa8a91fd1d159d06b
22piyush <piyushaglawe05@gmail.com>
2026-05-17 13:06:06 +0530

    done

--- angular-new-project/src/app/body/body/body.ts
+++ angular-new-project/src/app/body/body/body.ts
@@ -4,10 +4,11 @@ import { Directives } from '../../components/directives/directives';
 import { UserList } from '../../components/user-list/user-list';
 import { CustomDirective } from '../../components/custom-directive/custom-directive';
 import { PipesDemo } from '../../components/pipes-demo/pipes-demo';
+import { PureImpurePipes } from '../../components/pure-impure-pipes/pure-impure-pipes';
 
 @Component({
   selector: 'app-body',
-  imports: [DataBinding, Directives, CustomDirective, PipesDemo],
+  imports: [DataBinding, Directives, CustomDirective, PipesDemo, PureImpurePipes],
   templateUrl: './body.html',
   styleUrl: './body.css',
 })
---
+++ angular-new-project/src/app/components/pure-impure-pipes/pure-impure-pipes.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pure-impure-pipes',
+  imports: [],
+  templateUrl: './pure-impure-pipes.html',
+  styleUrl: './pure-impure-pipes.css',
+})
+export class PureImpurePipes {}
