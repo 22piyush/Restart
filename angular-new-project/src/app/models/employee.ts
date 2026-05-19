33460dc016e1732e594068c98eee878b958749dc
22piyush <piyushaglawe05@gmail.com>
2026-05-19 08:30:37 +0530

    done

---
+++ angular-new-project/src/app/models/employee.ts
@@ -0,0 +1,17 @@
+export class Employee {
+  id!: number;
+  firstName!: string;
+  lastName!: string;
+  email!: string;
+  gender!: string;
+
+  constructor(data: any) {
+    Object.assign(this, data);
+  }
+
+  fullName() {
+    return this.firstName + ' ' + this.lastName;
+  }
+
+
+}
