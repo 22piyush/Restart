0aafb6ce67a19593ef9a03a04bea58e515a7b8f2
22piyush <piyushaglawe05@gmail.com>
2026-05-20 21:32:46 +0530

    done

--- angular-new-project/src/app/components/observable/observable.ts
+++ angular-new-project/src/app/components/observable/observable.ts
@@ -1,6 +1,7 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
-import { filter, from, interval, take } from 'rxjs';
+import { Component, inject } from '@angular/core';
+import { filter, forkJoin, from, interval, map, take } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-observable',
@@ -9,9 +10,12 @@ import { filter, from, interval, take } from 'rxjs';
   styleUrl: './observable.css',
 })
 export class Observable {
+  httpClient = inject(HttpClient);
+
   ngOnInit() {
     // this.from_demo();
-    this.interval_demo();
+    // this.interval_demo();
+    this.forkjoin_demo();
   }
 
   from_demo() {
@@ -28,6 +32,20 @@ export class Observable {
   num$ = interval(1000);
   even_num$ = this.num$.pipe(filter((val) => val % 2 == 0));
   first_five = this.num$.pipe(take(5));
+
+  num_square = this.num$.pipe(map((val) => val * val));
+
+  currentTime$ = interval(1000).pipe(map(() => new Date().toLocaleTimeString()));
+
+  forkjoin_demo() {
+    let api_1 = this.httpClient.get('https://jsonplaceholder.typicode.com/users');
+    let api_2 = this.httpClient.get('https://jsonplaceholder.typicode.com/posts');
+    let api_3 = this.httpClient.get('https://jsonplaceholder.typicode.com/todos');
+
+    forkJoin([api_1, api_2, api_3]).subscribe((responseArr) => {
+      console.log(responseArr);
+    });
+  }
 
   interval_demo() {
     this.num$.subscribe((val) => console.log(val));
