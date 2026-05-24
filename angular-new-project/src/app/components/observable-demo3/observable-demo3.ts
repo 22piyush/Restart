563313090edd7db3d8990e9127f2e7219ae27779
22piyush <piyushaglawe05@gmail.com>
2026-05-24 13:44:02 +0530

    done

--- angular-new-project/src/app/components/observable-demo3/observable-demo3.ts
+++ angular-new-project/src/app/components/observable-demo3/observable-demo3.ts
@@ -1,9 +1,26 @@
+// import { CommonModule } from '@angular/common';
+// import { HttpClient } from '@angular/common/http';
+// import { Component } from '@angular/core';
+// import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+
+// import { Observable, switchMap, map } from 'rxjs';
+
 import { CommonModule } from '@angular/common';
 import { HttpClient } from '@angular/common/http';
-import { Component } from '@angular/core';
+
+import { Component, ElementRef, ViewChild } from '@angular/core';
+
 import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
-import { Observable, switchMap, map } from 'rxjs';
+import {
+  Observable,
+  switchMap,
+  map,
+  fromEvent,
+  debounceTime,
+  distinctUntilChanged,
+  exhaustMap,
+} from 'rxjs';
 
 @Component({
   selector: 'app-observable-demo3',
@@ -22,9 +39,28 @@ export class ObservableDemo3 {
 
   ngOnInit() {
     this.searchResult$ = this.searchForm.get('searchField')!.valueChanges.pipe(
+      debounceTime(200),
+
+      distinctUntilChanged(),
+
       switchMap((term: any) => this.http.get<any>(`https://dummyjson.com/users/search?q=${term}`)),
 
       map((response: any) => (response.users.length > 0 ? response.users : [])),
     );
+  }
+
+  @ViewChild('loginBtn') loginBtn!: ElementRef;
+
+  ngAfterViewInit() {
+    fromEvent(this.loginBtn.nativeElement, 'click')
+      .pipe(
+        exhaustMap((val) => {
+          return this.http.get('https://httpbin.org/delay/5');
+        }),
+      )
+      .subscribe({
+        next: (res) => console.log('Response:', res),
+        error: (err) => console.error('Error:', err),
+      });
   }
 }
