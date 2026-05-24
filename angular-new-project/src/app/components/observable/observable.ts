da4cf275fbed7303ddb818300f3ed6fffa117d18
22piyush <piyushaglawe05@gmail.com>
2026-05-24 09:48:05 +0530

    done

--- angular-new-project/src/app/components/observable/observable.ts
+++ angular-new-project/src/app/components/observable/observable.ts
@@ -13,7 +13,10 @@ export class Observable {
   ngOnInit() {
     this.from_demo();
     this.interval_demo();
+    this.fork_join();
   }
+
+  httpClient = inject(HttpClient);
 
   from_demo() {
     let cars = ['Tata', 'Honda', 'Maruti', 'Toyota'];
@@ -31,11 +34,17 @@ export class Observable {
 
   first_5 = this.num$.pipe(take(5));
 
-
-  num_square = this.num$.pipe(map((val)=>val * val));
-
+  num_square = this.num$.pipe(map((val) => val * val));
 
   interval_demo() {
     this.num$.subscribe((val) => console.log(val));
+  }
+
+  currentTime$ = interval(1000).pipe(map(() => new Date().toLocaleTimeString()));
+
+  fork_join() {
+    let api_1 = this.httpClient.get('https://jsonplaceholder.typicode.com/users');
+    let api_2 = this.httpClient.get('https://jsonplaceholder.typicode.com/posts');
+    let api_3 = this.httpClient.get('https://jsonplaceholder.typicode.com/comments');
   }
 }
