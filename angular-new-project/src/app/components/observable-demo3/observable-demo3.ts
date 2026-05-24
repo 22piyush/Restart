939bb308b3aa4bf9f0ba2a2afd7980f02102affa
22piyush <piyushaglawe05@gmail.com>
2026-05-24 13:10:19 +0530

    done

--- angular-new-project/src/app/components/observable-demo3/observable-demo3.ts
+++ angular-new-project/src/app/components/observable-demo3/observable-demo3.ts
@@ -1,11 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
-import {
-  FormControl,
-  FormGroup,
-  ReactiveFormsModule,
-} from '@angular/forms';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 import { Observable, switchMap, map } from 'rxjs';
 
@@ -16,7 +12,6 @@ import { Observable, switchMap, map } from 'rxjs';
   styleUrl: './observable-demo3.css',
 })
 export class ObservableDemo3 {
-
   searchResult$!: Observable<any[]>;
 
   searchForm = new FormGroup({
@@ -26,19 +21,10 @@ export class ObservableDemo3 {
   constructor(private http: HttpClient) {}
 
   ngOnInit() {
+    this.searchResult$ = this.searchForm.get('searchField')!.valueChanges.pipe(
+      switchMap((term: any) => this.http.get<any>(`https://dummyjson.com/users/search?q=${term}`)),
 
-    this.searchResult$ =
-      this.searchForm.get('searchField')!.valueChanges.pipe(
-
-        switchMap((term: any) =>
-          this.http.get<any>(
-            `https://dummyjson.com/users/search?q=${term}`
-          )
-        ),
-
-        map((response: any) =>
-          response.users.length > 0 ? response.users : []
-        )
-      );
+      map((response: any) => (response.users.length > 0 ? response.users : [])),
+    );
   }
 }
