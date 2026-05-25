aef5e903c86bd30347c609275e63275048a4818f
22piyush <piyushaglawe05@gmail.com>
2026-05-25 22:33:22 +0530

    done

--- angular-new-project/src/app/services/todo-service.ts
+++ angular-new-project/src/app/services/todo-service.ts
@@ -1,10 +1,28 @@
 import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+
+export interface Todo {
+  id: any;
+  value: string;
+}
+
+const initialTodos = [
+  { id: 1, value: 'Complete Angular Assignment' },
+  { id: 2, value: 'Learn Java Stream API' },
+];
 
 @Injectable({
   providedIn: 'root',
 })
 export class TodoService {
+  private todoSubject = new BehaviorSubject<Todo[]>(initialTodos);
+  readonly todoObs = this.todoSubject.asObservable();
+  private todoArr: Todo[] = this.todoSubject.value;
+  private nextId = 3;
 
-    
-
+  addNewTodo(item: any) {
+    item.id = this.nextId++;
+    this.todoArr.push(item);
+    this.todoSubject.next([...this.todoArr]);
+  }
 }
