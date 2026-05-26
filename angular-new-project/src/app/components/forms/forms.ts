2a22c48b5383eb43491adab430c63b80828e0ad1
22piyush <piyushaglawe05@gmail.com>
2026-05-26 23:24:45 +0530

    done

--- angular-new-project/src/app/components/forms/forms.ts
+++ angular-new-project/src/app/components/forms/forms.ts
@@ -1,13 +1,13 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-forms',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './forms.html',
   styleUrl: './forms.css',
 })
 export class Forms {
-  
-                            
-  
+  name = '';
+  age = null;
 }
