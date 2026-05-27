720b791e778ffc54d62ed186343d8f70e13fc4be
22piyush <piyushaglawe05@gmail.com>
2026-05-27 21:50:56 +0530

    done

--- angular-new-project/src/app/components/signal-form-demo/signal-form-demo.ts
+++ angular-new-project/src/app/components/signal-form-demo/signal-form-demo.ts
@@ -1,11 +1,15 @@
 import { Component, signal } from '@angular/core';
+import { form, FormField } from '@angular/forms/signals';
 
 @Component({
   selector: 'app-signal-form-demo',
-  imports: [],
+  imports: [FormField],
   templateUrl: './signal-form-demo.html',
   styleUrl: './signal-form-demo.css',
 })
 export class SignalFormDemo {
   loginModel = signal({ email: '', password: '' });
+  loginForm = form(this.loginModel);
+
+
 }
