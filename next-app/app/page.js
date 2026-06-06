b9331ed5529299c6ff2c2c144d65c69da2efd945
22piyush <piyushaglawe05@gmail.com>
2026-06-06 22:04:14 +0530

    done

---
+++ next-app/app/page.js
@@ -0,0 +1,14 @@
+
+import Link from "next/link"
+export default function Home() {
+  return (
+    <>WelCome  <br></br>
+
+
+      <Link href="/services">Services</Link>
+
+      <Link href="/components/about">About</Link>
+
+    </>
+  );
+}
