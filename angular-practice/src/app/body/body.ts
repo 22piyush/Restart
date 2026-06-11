de5dcb902131471bdbeea60d44746d760541c2f8
22piyush <piyushaglawe05@gmail.com>
2026-06-11 08:02:26 +0530

    done

--- angular-practice/src/app/body/body.ts
+++ angular-practice/src/app/body/body.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import { Navbar } from '../components/navbar/navbar';
+import { Categories } from '../components/categories/categories';
+import { Footer } from '../components/footer/footer';
 
 @Component({
   selector: 'app-body',
-  imports: [],
+  imports: [Navbar, Categories, Footer],
   templateUrl: './body.html',
   styleUrl: './body.css',
 })
