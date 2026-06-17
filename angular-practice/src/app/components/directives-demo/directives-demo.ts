b4c04156a28e32d83ad16ba223d2cb2a27c57360
22piyush <piyushaglawe05@gmail.com>
2026-06-17 07:29:56 +0530

    done

--- angular-practice/src/app/components/directives-demo/directives-demo.ts
+++ angular-practice/src/app/components/directives-demo/directives-demo.ts
@@ -1,14 +1,13 @@
-import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-directives-demo',
-  imports: [FormsModule, CommonModule],
+  imports: [FormsModule],
   templateUrl: './directives-demo.html',
   styleUrl: './directives-demo.css',
 })
 export class DirectivesDemo {
   num: number = 2;
-  courseArr = ['Angular', 'React', 'Web Development', 'Java', 'Python'];
+  courseArr = [];
 }
