edab1ac046923dd5e570c4d0289c06cb5ea24a27
22piyush <piyushaglawe05@gmail.com>
2026-06-18 08:04:42 +0530

    auth Ui Complete

--- angular-practice/src/app/body/body.ts
+++ angular-practice/src/app/body/body.ts
@@ -1,13 +1,10 @@
 import { Component } from '@angular/core';
-import { Navbar } from '../components/navbar/navbar';
-import { Categories } from '../components/categories/categories';
-import { Footer } from '../components/footer/footer';
 import { DataBinding } from '../components/data-binding/data-binding';
 import { DirectivesDemo } from '../components/directives-demo/directives-demo';
 
 @Component({
   selector: 'app-body',
-  imports: [Navbar, Categories, Footer, DataBinding, DirectivesDemo],
+  imports: [DataBinding, DirectivesDemo],
   templateUrl: './body.html',
   styleUrl: './body.css',
 })
