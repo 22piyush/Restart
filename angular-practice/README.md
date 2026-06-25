bbb5a165d31808ebce34591a6a07d8f9d7ea4fc7
22piyush <piyushaglawe05@gmail.com>
2026-06-25 07:24:11 +0530

    Authentication Done

--- angular-practice/README.md
+++ angular-practice/README.md
@@ -3,4 +3,4 @@
 17 ---->  Bootsrap Component & Explain Folder Structure
 18 ---->  Data Binding
 19 ----> Directives
-20 ----> Read data from JSON & TS file.
+20 ----> Read data from JSON & TS file./ Use Font Awesome./ Pagination
--- angular-practice/src/app/components/user-list/user-list.ts
+++ angular-practice/src/app/components/user-list/user-list.ts
@@ -1,15 +1,16 @@
 import { Component } from '@angular/core';
 import * as data from './user_data.json';
 import employee from './employee';
+import { NgxPaginationModule } from 'ngx-pagination';
 
 @Component({
   selector: 'app-user-list',
-  imports: [],
+  imports: [NgxPaginationModule],
   templateUrl: './user-list.html',
   styleUrl: './user-list.css',
 })
 export class UserList {
   userArr = (data as any).default;
-
   empArr = employee;
+  p: number = 1;
 }
