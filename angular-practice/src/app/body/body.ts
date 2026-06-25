c65f89b678491fcac3e7d8e282d5c28df3be8d8e
22piyush <piyushaglawe05@gmail.com>
2026-06-25 06:56:26 +0530

    Authentication Done

--- angular-practice/src/app/body/body.ts
+++ angular-practice/src/app/body/body.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { DataBinding } from '../components/data-binding/data-binding';
 import { DirectivesDemo } from '../components/directives-demo/directives-demo';
+import { UserList } from '../components/user-list/user-list';
 
 @Component({
   selector: 'app-body',
-  imports: [DataBinding, DirectivesDemo],
+  imports: [DataBinding, DirectivesDemo, UserList],
   templateUrl: './body.html',
   styleUrl: './body.css',
 })
