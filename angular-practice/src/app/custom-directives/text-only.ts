58e9fc5a358c8f4c4d2b50ecb2e7f3f8394cacee
22piyush <piyushaglawe05@gmail.com>
2026-06-26 21:42:07 +0530

    Authentication Done

--- angular-practice/src/app/custom-directives/text-only.ts
+++ angular-practice/src/app/custom-directives/text-only.ts
@@ -1,8 +1,19 @@
-import { Directive } from '@angular/core';
+import { Directive, HostBinding, HostListener } from '@angular/core';
 
 @Directive({
   selector: '[appTextOnly]',
 })
 export class TextOnly {
-  constructor() {}
+  @HostBinding('style.background-color')
+  myBgColor: string = 'red';
+
+  @HostListener('keyup', ['$event.target.value'])
+  handleKeyUp(event: KeyboardEvent) {
+    const target = event.target as HTMLInputElement;
+    const value = target.value ?? '';
+
+    const regex = /^[a-zA-Z]*$/;
+
+    this.myBgColor = regex.test(value) ? 'cyan' : 'red';
+  }
 }
