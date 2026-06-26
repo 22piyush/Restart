a3e52fcd55e60ee5fb9f706b9200113f2e388896
22piyush <piyushaglawe05@gmail.com>
2026-06-26 21:25:00 +0530

    Authentication Done

--- angular-practice/README.md
+++ angular-practice/README.md
@@ -3,4 +3,5 @@
 17 ---->  Bootsrap Component & Explain Folder Structure
 18 ---->  Data Binding
 19 ----> Directives
-20 ----> Read data from JSON & TS file./ Use Font Awesome./ Pagination
+20 ----> Read data from JSON & TS file./ Use Font Awesome./ Pagination.. HW ----> Add search Input / Sort ASC & DESC
+21 ----> Custom Directives
--- angular-practice/src/app/body/body.ts
+++ angular-practice/src/app/body/body.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { DataBinding } from '../components/data-binding/data-binding';
 import { DirectivesDemo } from '../components/directives-demo/directives-demo';
 import { UserList } from '../components/user-list/user-list';
+import { CustomDirectives } from '../components/custom-directives/custom-directives';
 
 @Component({
   selector: 'app-body',
-  imports: [DataBinding, DirectivesDemo, UserList],
+  imports: [DataBinding, DirectivesDemo, UserList, CustomDirectives],
   templateUrl: './body.html',
   styleUrl: './body.css',
 })
