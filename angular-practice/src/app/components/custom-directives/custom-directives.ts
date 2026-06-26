4bd0b805398ad8a0184496613a6fb0723e77adb5
22piyush <piyushaglawe05@gmail.com>
2026-06-26 21:29:30 +0530

    Authentication Done

--- angular-practice/src/app/components/custom-directives/custom-directives.ts
+++ angular-practice/src/app/components/custom-directives/custom-directives.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
+import { TextOnly } from '../../custom-directives/text-only';
 
 @Component({
   selector: 'app-custom-directives',
-  imports: [],
+  imports: [TextOnly],
   templateUrl: './custom-directives.html',
   styleUrl: './custom-directives.css',
 })
 export class CustomDirectives {
-  
+
 }
---
+++ angular-practice/src/app/custom-directives/text-only.ts
@@ -0,0 +1,8 @@
+import { Directive } from '@angular/core';
+
+@Directive({
+  selector: '[appTextOnly]',
+})
+export class TextOnly {
+  constructor() {}
+}
