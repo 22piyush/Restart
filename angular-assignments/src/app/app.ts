693521d799237fc2889359559f09087cd3d6aacf
22piyush <piyushaglawe05@gmail.com>
2026-06-28 22:06:43 +0530

    Authentication Done

--- angular-assignments/src/app/app.ts
+++ angular-assignments/src/app/app.ts
@@ -1,10 +1,16 @@
+import { CommonModule } from '@angular/common';
 import { Component, signal } from '@angular/core';
-import { Body } from './body/body';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
-  imports: [Body],
+  imports: [CommonModule, FormsModule],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
-export class App {}
+export class App {
+  showPara:boolean = true;
+  state = ""
+
+  count = 0;
+}
