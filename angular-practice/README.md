175c848dece51e4052dd24910d1fb10badc99151
22piyush <piyushaglawe05@gmail.com>
2026-06-29 22:14:48 +0530

    Done...

--- angular-practice/README.md
+++ angular-practice/README.md
@@ -5,3 +5,4 @@
 19 ----> Directives
 20 ----> Read data from JSON & TS file./ Use Font Awesome./ Pagination.. HW ----> Add search Input / Sort ASC & DESC
 21 ----> Custom Directives
+22 ----> Angular Pipes
--- angular-practice/src/app/body/body.ts
+++ angular-practice/src/app/body/body.ts
@@ -3,10 +3,11 @@ import { DataBinding } from '../components/data-binding/data-binding';
 import { DirectivesDemo } from '../components/directives-demo/directives-demo';
 import { UserList } from '../components/user-list/user-list';
 import { CustomDirectives } from '../components/custom-directives/custom-directives';
+import { AngularPipes } from '../components/angular-pipes/angular-pipes';
 
 @Component({
   selector: 'app-body',
-  imports: [DataBinding, DirectivesDemo, UserList, CustomDirectives],
+  imports: [DataBinding, DirectivesDemo, UserList, CustomDirectives, AngularPipes],
   templateUrl: './body.html',
   styleUrl: './body.css',
 })
