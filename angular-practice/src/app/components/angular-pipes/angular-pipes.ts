e7ae9bc85d4b578cba350189ecbf12e007da24e9
22piyush <piyushaglawe05@gmail.com>
2026-06-29 22:18:28 +0530

    Done...

--- angular-practice/src/app/components/angular-pipes/angular-pipes.ts
+++ angular-practice/src/app/components/angular-pipes/angular-pipes.ts
@@ -1,9 +1,12 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-angular-pipes',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './angular-pipes.html',
   styleUrl: './angular-pipes.css',
 })
-export class AngularPipes {}
+export class AngularPipes {
+  userName = "piyush22"
+}
