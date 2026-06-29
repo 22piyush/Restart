26e662fab374854c8588be22448a49d510951715
22piyush <piyushaglawe05@gmail.com>
2026-06-29 22:50:46 +0530

    Done...

--- angular-practice/src/app/components/angular-pipes/angular-pipes.ts
+++ angular-practice/src/app/components/angular-pipes/angular-pipes.ts
@@ -1,10 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { RemainingPipe } from '../../custom_pipes/remaining-pipe';
 
 @Component({
   selector: 'app-angular-pipes',
-  imports: [CommonModule, FormsModule],
+  imports: [CommonModule, FormsModule, RemainingPipe],
   templateUrl: './angular-pipes.html',
   styleUrl: './angular-pipes.css',
 })
