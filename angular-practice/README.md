954355dd8448f8385f608fdce5778a525be40f46
22piyush <piyushaglawe05@gmail.com>
2026-06-29 22:48:51 +0530

    Done...

--- angular-practice/README.md
+++ angular-practice/README.md
@@ -5,4 +5,4 @@
 19 ----> Directives
 20 ----> Read data from JSON & TS file./ Use Font Awesome./ Pagination.. HW ----> Add search Input / Sort ASC & DESC
 21 ----> Custom Directives
-22 ----> Angular Pipes
+22 ----> Angular Pipes / Custom Pipes
--- angular-practice/src/app/components/angular-pipes/angular-pipes.ts
+++ angular-practice/src/app/components/angular-pipes/angular-pipes.ts
@@ -1,14 +1,13 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-angular-pipes',
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './angular-pipes.html',
   styleUrl: './angular-pipes.css',
 })
 export class AngularPipes {
-  userName = "piyush22"
-
-  salary = 5000
+  msg = 'Hello';
 }
