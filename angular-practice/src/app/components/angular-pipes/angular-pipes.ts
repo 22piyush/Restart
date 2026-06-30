7ea5ded150fd09ceca514308fbe33333472b2b90
22piyush <piyushaglawe05@gmail.com>
2026-06-30 22:25:01 +0530

    Done...

--- angular-practice/src/app/components/angular-pipes/angular-pipes.ts
+++ angular-practice/src/app/components/angular-pipes/angular-pipes.ts
@@ -2,13 +2,16 @@ import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { RemainingPipe } from '../../custom_pipes/remaining-pipe';
+import { MySortPipe } from '../../custom_pipes/my-sort-pipe';
 
 @Component({
   selector: 'app-angular-pipes',
-  imports: [CommonModule, FormsModule, RemainingPipe],
+  imports: [CommonModule, FormsModule, RemainingPipe, MySortPipe],
   templateUrl: './angular-pipes.html',
   styleUrl: './angular-pipes.css',
 })
 export class AngularPipes {
   msg = 'Hello';
+
+  numArr = [30,40,10,20,50];
 }
