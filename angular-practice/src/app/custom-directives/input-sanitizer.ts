c523d1044a6b50491b3322fbcf4b50d7cb5b8cc5
22piyush <piyushaglawe05@gmail.com>
2026-06-30 22:07:29 +0530

    Done...

--- angular-practice/src/app/custom-directives/input-sanitizer.ts
+++ angular-practice/src/app/custom-directives/input-sanitizer.ts
@@ -1,8 +1,22 @@
-import { Directive } from '@angular/core';
+import { Directive, HostListener, Renderer2, ElementRef } from '@angular/core';
 
 @Directive({
   selector: '[appInputSanitizer]',
 })
 export class InputSanitizer {
-  constructor() {}
+  constructor(
+    private renderer: Renderer2,
+    private el: ElementRef,
+  ) {}
+
+  @HostListener('input', ['$event'])
+  onInput(event: Event) {
+    const input = event.target as HTMLInputElement;
+
+    // Allow only letters, numbers, and spaces
+    const sanitizedValue = input.value.replace(/[^a-zA-Z0-9 ]/g, '');
+
+    // Update the input value
+    this.renderer.setProperty(this.el.nativeElement, 'value', sanitizedValue);
+  }
 }
