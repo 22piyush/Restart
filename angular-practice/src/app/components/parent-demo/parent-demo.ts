abd65db2b85573e6ca9ec5f63f3839d46fb60069
22piyush <piyushaglawe05@gmail.com>
2026-07-01 23:17:07 +0530

    Done Component Communication

--- angular-practice/src/app/components/parent-demo/parent-demo.ts
+++ angular-practice/src/app/components/parent-demo/parent-demo.ts
@@ -4,11 +4,15 @@ import { Child2Demo } from '../child2-demo/child2-demo';
 
 @Component({
   selector: 'app-parent-demo',
-  imports: [Child1Demo,Child2Demo],
+  imports: [Child1Demo, Child2Demo],
   templateUrl: './parent-demo.html',
   styleUrl: './parent-demo.css',
 })
 export class ParentDemo {
   a = 100;
-  
+  c_parent: number;
+
+  receiveDataFromChild2(c_child2: number) {
+    this.c_parent = c_child2;
+  }
 }
