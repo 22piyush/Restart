08c8abb9929166500edac3fb4d7235157de065e9
22piyush <piyushaglawe05@gmail.com>
2026-07-02 21:54:24 +0530

    Start Component binding

--- angular-practice/src/app/components/categories/categories.ts
+++ angular-practice/src/app/components/categories/categories.ts
@@ -6,4 +6,57 @@ import { Component } from '@angular/core';
   templateUrl: './categories.html',
   styleUrl: './categories.css',
 })
-export class Categories {}
+export class Categories {
+  categories = [
+    {
+      id: 1,
+      name: 'Electronics',
+      image: 'electronics.jpg',
+    },
+    {
+      id: 2,
+      name: 'Fashion',
+      image: 'fashion.jpg',
+    },
+    {
+      id: 3,
+      name: 'Home & Kitchen',
+      image: 'home-kitchen.jpg',
+    },
+    {
+      id: 4,
+      name: 'Books',
+      image: 'books.jpg',
+    },
+    {
+      id: 5,
+      name: 'Sports',
+      image: 'sports.jpg',
+    },
+    {
+      id: 6,
+      name: 'Beauty',
+      image: 'beauty.jpg',
+    },
+    {
+      id: 7,
+      name: 'Toys',
+      image: 'toys.jpg',
+    },
+    {
+      id: 8,
+      name: 'Automobiles',
+      image: 'automobiles.jpg',
+    },
+    {
+      id: 9,
+      name: 'Groceries',
+      image: 'groceries.jpg',
+    },
+    {
+      id: 10,
+      name: 'Health',
+      image: 'health.jpg',
+    },
+  ];
+}
