ce94bb0cd3210a4a04afa3601c49a3c3213bb5da
22piyush <piyushaglawe05@gmail.com>
2026-07-04 15:20:01 +0530

    Start Component Communication

---
+++ angular-assignments/src/app/components/child-one/child-one.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-child-one',
+  imports: [],
+  templateUrl: './child-one.html',
+  styleUrl: './child-one.css',
+})
+export class ChildOne {}
---
+++ angular-assignments/src/app/components/child-two/child-two.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-child-two',
+  imports: [],
+  templateUrl: './child-two.html',
+  styleUrl: './child-two.css',
+})
+export class ChildTwo {}
