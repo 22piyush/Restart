5f203fa1197c6c8de66e276b12fe0f8059c323c4
22piyush <piyushaglawe05@gmail.com>
2026-07-04 15:44:08 +0530

    Start Component Communication

--- angular-assignments/src/app/app.ts
+++ angular-assignments/src/app/app.ts
@@ -1,18 +1,14 @@
 import { CommonModule } from '@angular/common';
 import { Component, signal } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { ReversePipePipe } from './customPipes/reverse-pipe-pipe';
-import { OrdinalPipe } from './customPipes/ordinal-pipe';
-import { RemainingPipe } from './customPipes/remaining-pipe';
 
 @Component({
+  standalone: true,
   selector: 'app-root',
-  imports: [CommonModule, FormsModule,RemainingPipe, ReversePipePipe , OrdinalPipe],
+  imports: [CommonModule, FormsModule],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
 export class App {
-
- val = 123456789123
 
 }
--- angular-assignments/src/app/components/child-two/child-two.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-child-two',
-  imports: [],
-  templateUrl: './child-two.html',
-  styleUrl: './child-two.css',
-})
-export class ChildTwo {}
