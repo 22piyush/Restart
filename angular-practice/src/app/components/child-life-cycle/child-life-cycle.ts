85c7b9273e4f28c8d7850343ccfae75ee0faa15d
22piyush <piyushaglawe05@gmail.com>
2026-07-06 17:11:13 +0530

    Start Component lifecycle ngOnChnage..

--- angular-practice/src/app/components/child-life-cycle/child-life-cycle.ts
+++ angular-practice/src/app/components/child-life-cycle/child-life-cycle.ts
@@ -1,37 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef } from '@angular/core';
 
 @Component({
+  standalone: true,
   selector: 'app-child-life-cycle',
   imports: [],
   templateUrl: './child-life-cycle.html',
   styleUrl: './child-life-cycle.css',
+  inputs: ['name'],
 })
 export class ChildLifeCycle {
-  constructor() {
-    console.log('child constructor');
-  }
+  name: any;
+
+  // constructor() {
+  //   console.log('child constructor');
+  //   this.courseName = 'Angular';
+  // }
   ngOnChanges() {
     console.log('child ngOnChanges');
   }
   ngOnInit() {
     console.log('child ngOnInit');
   }
-  ngDoCheck() {
-    console.log('child ngDoCheck');
-  }
-  ngAfterContentInit() {
-    console.log('child ngAfterContentInit');
-  }
-  ngAfterContentChecked() {
-    console.log('child ngAfterContentChecked');
-  }
-  ngAfterViewInit() {
-    console.log('child ngAfterViewInit');
-  }
-  ngAfterViewChecked() {
-    console.log('child ngAfterViewChecked');
-  }
-  ngOnDestroy() {
-    console.log('child ngOnDestory');
-  }
+  // ngDoCheck() {
+  //   console.log('child ngDoCheck');
+  // }
+  // ngAfterContentInit() {
+  //   console.log('child ngAfterContentInit');
+  // }
+  // ngAfterContentChecked() {
+  //   console.log('child ngAfterContentChecked');
+  // }
+  // ngAfterViewInit() {
+  //   console.log('child ngAfterViewInit');
+  // }
+  // ngAfterViewChecked() {
+  //   console.log('child ngAfterViewChecked');
+  // }
+  // ngOnDestroy() {
+  //   console.log('child ngOnDestory');
+  // }
 }
--- angular-practice/src/app/components/life-cycle/life-cycle.ts
+++ angular-practice/src/app/components/life-cycle/life-cycle.ts
@@ -2,37 +2,40 @@ import { Component } from '@angular/core';
 import { ChildLifeCycle } from '../child-life-cycle/child-life-cycle';
 
 @Component({
+  standalone: true,
   selector: 'app-life-cycle',
   imports: [ChildLifeCycle],
   templateUrl: './life-cycle.html',
   styleUrl: './life-cycle.css',
 })
 export class LifeCycle {
-  constructor() {
-    console.log('Parent constructor');
-  }
+  name = 12;
+
+  // constructor() {
+  //   console.log('Parent constructor');
+  // }
   ngOnChanges() {
     console.log('Parent ngOnChanges');
   }
   ngOnInit() {
     console.log('Parent ngOnInit');
   }
-  ngDoCheck() {
-    console.log('Parent ngDoCheck');
-  }
-  ngAfterContentInit() {
-    console.log('Parent ngAfterContentInit');
-  }
-  ngAfterContentChecked() {
-    console.log('Parent ngAfterContentChecked');
-  }
-  ngAfterViewInit() {
-    console.log('Parent ngAfterViewInit');
-  }
-  ngAfterViewChecked() {
-    console.log('Parent ngAfterViewChecked');
-  }
-  ngOnDestroy() {
-    console.log('Parent ngOnDestory');
-  }
+  // ngDoCheck() {
+  //   console.log('Parent ngDoCheck');
+  // }
+  // ngAfterContentInit() {
+  //   console.log('Parent ngAfterContentInit');
+  // }
+  // ngAfterContentChecked() {
+  //   console.log('Parent ngAfterContentChecked');
+  // }
+  // ngAfterViewInit() {
+  //   console.log('Parent ngAfterViewInit');
+  // }
+  // ngAfterViewChecked() {
+  //   console.log('Parent ngAfterViewChecked');
+  // }
+  // ngOnDestroy() {
+  //   console.log('Parent ngOnDestory');
+  // }
 }
