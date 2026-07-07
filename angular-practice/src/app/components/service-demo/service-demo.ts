8c9f755f9509bff5afd0797f145845fa258aff12
22piyush <piyushaglawe05@gmail.com>
2026-07-07 22:44:48 +0530

    Start Services examples...

--- angular-practice/src/app/components/service-demo/service-demo.ts
+++ angular-practice/src/app/components/service-demo/service-demo.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { MathService } from '../../services/math-service';
+import { ObjectUtilService } from '../../services/object-util-service';
 
 @Component({
   selector: 'app-service-demo',
@@ -6,4 +8,24 @@ import { Component } from '@angular/core';
   templateUrl: './service-demo.html',
   styleUrl: './service-demo.css',
 })
-export class ServiceDemo {}
+export class ServiceDemo {
+
+  // Both Are use to achieve DI
+  // mathService = inject(MathService);
+  // objectService = inject(ObjectUtilService);
+  // constructor(
+  //   private mathService: MathService ,
+  //   private objectService:ObjectUtilService
+  // ) {}
+  
+
+  ngOnInit() {
+    this.mathServiceFunc();
+  }
+
+  mathServiceFunc() {
+    console.log('Math', this.mathService.sum([10, 20, 30, 40, 50, 60, 70, 80]));
+    console.log(this.mathService.average([10, 20, 30, 40, 50, 60, 70, 80]));
+    console.log(this.mathService.indexOfMax([10, 20, 30, 40, 50, 60, 70, 80]));
+  }
+}
