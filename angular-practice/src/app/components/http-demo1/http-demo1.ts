6b61364a67e26a5077f836900c60c69bfadf1452
22piyush <piyushaglawe05@gmail.com>
2026-07-07 23:11:49 +0530

    Complete  HTTP Request  examples...

--- angular-practice/src/app/components/http-demo1/http-demo1.ts
+++ angular-practice/src/app/components/http-demo1/http-demo1.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, inject } from '@angular/core';
 
 @Component({
   selector: 'app-http-demo1',
@@ -9,11 +10,18 @@ import { Component } from '@angular/core';
 export class HttpDemo1 {
   user_api = 'https://dummyjson.com/users';
 
+  //Dependency Injection
+  httpClient = inject(HttpClient);
+  // constructor(private http: HttpClient) {}
+
   ngOnInit() {
     this.fetchData();
   }
 
   fetchData() {
-    
+    // httpClient.get() return Observable, to get data we use subscribe()
+    this.httpClient.get(this.user_api).subscribe((res) => {
+      console.log(res);
+    });
   }
 }
