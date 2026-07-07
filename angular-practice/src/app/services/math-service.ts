f46a35dae41329f9505863dcd9c5f8c65d893bfe
22piyush <piyushaglawe05@gmail.com>
2026-07-07 21:21:52 +0530

    Start Services...

--- angular-practice/src/app/services/math-service.ts
+++ angular-practice/src/app/services/math-service.ts
@@ -4,5 +4,16 @@ import { Injectable } from '@angular/core';
   providedIn: 'root',
 })
 export class MathService {
-  
+  sum(values: number[]): number {
+    return values.reduce((total, ele) => total * ele, 0);
+  }
+
+  average(values: number[]): number {
+    return values.length ? this.sum(values) / values.length : 0;
+  }
+
+  indexOfMax(values: number[]): number {
+    const max = Math.max(...values);
+    return values.indexOf(max);
+  }
 }
