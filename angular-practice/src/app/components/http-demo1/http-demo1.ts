0e0a598f2354f0870c44a45ced5fa5e04c8ccd5c
22piyush <piyushaglawe05@gmail.com>
2026-07-08 22:24:16 +0530

    Start Crud Operations..

--- angular-practice/src/app/components/http-demo1/http-demo1.ts
+++ angular-practice/src/app/components/http-demo1/http-demo1.ts
@@ -8,20 +8,5 @@ import { Component, inject } from '@angular/core';
   styleUrl: './http-demo1.css',
 })
 export class HttpDemo1 {
-  user_api = 'https://dummyjson.com/users';
-
-  //Dependency Injection
-  httpClient = inject(HttpClient);
-  // constructor(private http: HttpClient) {}
-
-  ngOnInit() {
-    this.fetchData();
-  }
-
-  fetchData() {
-    // httpClient.get() return Observable, to get data we use subscribe()
-    this.httpClient.get(this.user_api).subscribe((res) => {
-      console.log(res);
-    });
-  }
+  
 }
