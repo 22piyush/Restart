8ca81ab7ef4cefe36196b064cd4ae20199dca4db
22piyush <piyushaglawe05@gmail.com>
2026-07-08 22:40:22 +0530

    Start Crud Operations examples...

--- angular-practice/src/app/services/employee-service.ts
+++ angular-practice/src/app/services/employee-service.ts
@@ -1,9 +1,28 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { retry } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class EmployeeService {
   constructor(private httpClient: HttpClient) {}
+
+  api_url = 'https://fakestoreapi.com/products/';
+
+  getAllEmployees() {
+    return this.httpClient.get(this.api_url);
+  }
+
+  getEmployeeById(id: any) {
+    return this.httpClient.get(`${this.api_url}${id}`);
+  }
+
+  addEmployee(empData: any) {
+    return this.httpClient.post(this.api_url, empData);
+  }
+
+  updateEmployee(id: any, empData: any) {
+    return this.httpClient.patch(`${this.api_url}${id}`, empData);
+  }
 }
