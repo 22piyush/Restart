d768c725d3cbc4eafa013909f594222432f9e714
22piyush <piyushaglawe05@gmail.com>
2026-07-08 23:08:56 +0530

    Complete Crud Operations

--- angular-practice/src/app/components/http-demo1/http-demo1.ts
+++ angular-practice/src/app/components/http-demo1/http-demo1.ts
@@ -1,12 +1,85 @@
-import { HttpClient } from '@angular/common/http';
+import { CommonModule } from '@angular/common';
 import { Component, inject } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { EmployeeService } from '../../services/employee-service';
 
 @Component({
   selector: 'app-http-demo1',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, FormsModule],
   templateUrl: './http-demo1.html',
   styleUrl: './http-demo1.css',
 })
 export class HttpDemo1 {
-  
+  employeeService = inject(EmployeeService);
+
+  employees: any[] = [];
+
+  employee: any = {
+    title: '',
+    price: '',
+    category: '',
+  };
+
+  isEdit = false;
+  editId: number = 0;
+
+  ngOnInit() {
+    this.fetchEmployees();
+  }
+
+  fetchEmployees() {
+    this.employeeService.getAllEmployees().subscribe({
+      next: (res: any) => {
+        this.employees = res;
+        console.log(this.employees);
+        
+      },
+      error: (err) => {
+        console.log(err);
+      },
+    });
+  }
+
+  saveEmployee() {
+    if (this.isEdit) {
+      this.employeeService.updateEmployee(this.editId, this.employee).subscribe(() => {
+        alert('Updated Successfully');
+        this.fetchEmployees();
+        this.resetForm();
+      });
+    } else {
+      this.employeeService.addEmployee(this.employee).subscribe(() => {
+        alert('Added Successfully');
+        this.fetchEmployees();
+        this.resetForm();
+      });
+    }
+  }
+
+  editEmployee(emp: any) {
+    this.employee = { ...emp };
+    this.editId = emp.id;
+    this.isEdit = true;
+  }
+
+  deleteEmployee(id: number) {
+    if (confirm('Delete Employee?')) {
+      this.employeeService.deleteEmployee(id).subscribe(() => {
+        alert('Deleted Successfully');
+        this.fetchEmployees();
+      });
+    }
+  }
+
+  resetForm() {
+    this.employee = {
+      title: '',
+      price: '',
+      category: '',
+    };
+
+    this.isEdit = false;
+    this.editId = 0;
+  }
 }
--- angular-practice/src/app/services/employee-service.ts
+++ angular-practice/src/app/services/employee-service.ts
@@ -1,20 +1,20 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { retry } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class EmployeeService {
-  constructor(private httpClient: HttpClient) {}
 
   api_url = 'https://fakestoreapi.com/products/';
+
+  constructor(private httpClient: HttpClient) {}
 
   getAllEmployees() {
     return this.httpClient.get(this.api_url);
   }
 
-  getEmployeeById(id: any) {
+  getEmployeeById(id: number) {
     return this.httpClient.get(`${this.api_url}${id}`);
   }
 
@@ -22,11 +22,11 @@ export class EmployeeService {
     return this.httpClient.post(this.api_url, empData);
   }
 
-  updateEmployee(id: any, empData: any) {
+  updateEmployee(id: number, empData: any) {
     return this.httpClient.patch(`${this.api_url}${id}`, empData);
   }
 
-  deleteEmployee(id: any) {
+  deleteEmployee(id: number) {
     return this.httpClient.delete(`${this.api_url}${id}`);
   }
 }
