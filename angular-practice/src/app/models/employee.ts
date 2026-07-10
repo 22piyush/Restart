35c8814e1cc624faf030be91c149a73e1abd7a4b
22piyush <piyushaglawe05@gmail.com>
2026-07-10 23:13:43 +0530

    Start Class Interceptors

--- angular-practice/src/app/models/employee.ts
+++ angular-practice/src/app/models/employee.ts
@@ -1,12 +1,19 @@
 export class Employee {
-  id: Number;
-  firstName: string;
-  lastName: string;
-  email: string;
-  gender: string;
-  sal: string;
+  id!: Number;
+  firstName!: string;
+  lastName!: string;
+  email!: string;
+  gender!: string;
+  sal!: number;
 
   constructor(data: any) {
     Object.assign(this, data);
+  }
+
+  fullName() {
+    return this.firstName + ' ' + this.lastName;
+  }
+  yearlySal() {
+    return this.sal * 12;
   }
 }
