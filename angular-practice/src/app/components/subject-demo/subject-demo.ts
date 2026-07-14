e0378f0ffd74061783d50f39188b8df1ee2ab50c
22piyush <piyushaglawe05@gmail.com>
2026-07-14 22:07:21 +0530

    Start Subject Operator

--- angular-practice/src/app/components/subject-demo/subject-demo.ts
+++ angular-practice/src/app/components/subject-demo/subject-demo.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { Subject } from 'rxjs';
+import { BehaviorSubject, Subject } from 'rxjs';
 
 @Component({
   selector: 'app-subject-demo',
@@ -11,6 +11,7 @@ export class SubjectDemo {
 
   ngOnInit() {
     this.subject_demo();
+    this.behaviourSubject_demo();
   }
 
   subject_demo() {
@@ -22,4 +23,13 @@ export class SubjectDemo {
     mySubject.next('Plan-C');
   }
 
+  behaviourSubject_demo() {
+    let mySubject = new BehaviorSubject('Default Plan');
+    mySubject.next('Plan-A');
+    mySubject.subscribe((val) => console.log('Subscriber-1', val));
+    mySubject.next('Plan-B');
+    mySubject.subscribe((val) => console.log('Subscriber-2', val));
+    mySubject.next('Plan-C');
+  }
+  
 }
