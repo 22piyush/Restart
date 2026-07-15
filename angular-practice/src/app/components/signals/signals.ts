0f9e273d89a01335596e3f419278b733cc15f619
22piyush <piyushaglawe05@gmail.com>
2026-07-15 22:26:27 +0530

    Start Signals

--- angular-practice/src/app/components/signals/signals.ts
+++ angular-practice/src/app/components/signals/signals.ts
@@ -13,6 +13,17 @@ export class Signals {
   userId: WritableSignal<number> = signal(1);
   userData: WritableSignal<any> = signal({});
 
+  userDetailsEffect = effect(() => {
+    const userId = this.userId();
+    this.fetchUserDetails(userId);
+  });
 
-
+  fetchUserDetails() {
+    this.httpClient
+      .get(`https://jsonplaceholder.typicode.com/users/${this.userId}`)
+      .subscribe((res) => {
+        this.userData.set(res);
+      });
+  }
+  
 }
