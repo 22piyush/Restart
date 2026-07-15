b6a5df7850b650564e850f1f3a9569f75401eac1
22piyush <piyushaglawe05@gmail.com>
2026-07-15 22:26:07 +0530

    Start Signals

--- angular-practice/src/app/components/signals/signals.ts
+++ angular-practice/src/app/components/signals/signals.ts
@@ -8,20 +8,11 @@ import { Component, effect, inject, signal, WritableSignal } from '@angular/core
   styleUrl: './signals.css',
 })
 export class Signals {
+
   httpClient = inject(HttpClient);
   userId: WritableSignal<number> = signal(1);
   userData: WritableSignal<any> = signal({});
 
-  userDetailsEffect = effect(() => {
-    const userId = this.userId();
-    this.fetchUserDetails(userId);
-  });
 
-  fetchUserDetails() {
-    this.httpClient
-      .get(`https://jsonplaceholder.typicode.com/users/${this.userId}`)
-      .subscribe((res) => {
-        this.userData.set(res);
-      });
-  }
+
 }
