51182b8159fc0ce701f918471e37e8ccc2bffc33
22piyush <piyushaglawe05@gmail.com>
2026-07-19 18:21:47 +0530

    Start Guards...

--- angular-practice/src/app/guards/upload-guard.ts
+++ angular-practice/src/app/guards/upload-guard.ts
@@ -1,5 +1,13 @@
 import { CanActivateFn } from '@angular/router';
+import { UserInformation } from '../services/user-information';
+import { inject } from '@angular/core';
 
 export const uploadGuard: CanActivateFn = (route, state) => {
-  return true;
+  let UserInformationService = inject(UserInformation);
+  if (UserInformationService.getUserRole() == 'trainer') {
+    return true;
+  } else {
+    alert('You Dont have Access to this page');
+    return false;
+  }
 };
