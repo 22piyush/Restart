e895a1715b1919c67454d615393fd8257e16269f
22piyush <piyushaglawe05@gmail.com>
2026-08-05 23:23:01 +0530

    Start WEB FORMS

---
+++ WebApplication2/WebApplication2/ControlCreation.aspx.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApplication2
+{
+    public partial class ControlCreation : Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+                Button btn = new Button();
+                btn.ID = "Button3";
+                btn.Text = "Click Me";
+
+                form1.Controls.Add(btn);
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
