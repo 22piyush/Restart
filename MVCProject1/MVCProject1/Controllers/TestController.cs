aa188d98ee5427c6fb3e78237978596d24908a8a
22piyush <piyushaglawe05@gmail.com>
2026-08-11 23:02:41 +0530

    Start ASP .NET

---
+++ MVCProject1/MVCProject1/Controllers/TestController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCProject1.Controllers
+{
+    public class TestController : Controller
+    {
+
+        public string Index()
+        {
+            return "Index";
+        }
+
+        public string Show()
+        {
+            return "Show";
+        }
+
+    }
+}
