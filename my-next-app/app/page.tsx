3eb48815ed6b491100a365c55ed9b717f7fb7eb4
22piyush <piyushaglawe05@gmail.com>
2026-09-24 21:55:59 +0530

    Start Nextjs Routing

--- my-next-app/app/page.tsx
+++ my-next-app/app/page.tsx
@@ -6,6 +6,7 @@ export default function Home() {
     <div>
       <h1 className="text-3xl font-bold underline">Hello world! 2</h1>
       <Link href="/about">About</Link>
+      <Link href="/services">Services</Link>
     </div>
   );
 }
