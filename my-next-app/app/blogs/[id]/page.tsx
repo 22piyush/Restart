70118782472c46a34f49c874d16aab7da22088b4
22piyush <piyushaglawe05@gmail.com>
2026-09-24 22:47:23 +0530

    Start Nextjs Routing

--- my-next-app/app/blogs/[id]/page.tsx
+++ my-next-app/app/blogs/[id]/page.tsx
@@ -1,13 +1,15 @@
 
 
+
 const Blog1 = async ({ params }) => {
+    const { id } = await params;
+
     return (
         <div>
-            <h1>Blog {params.id}</h1>
+            <h1>Blog {id}</h1>
             <p>This is the content of Blog 1.</p>
         </div>
     );
 };
 
 export default Blog1;
-
