8663f33dee1d5afab0400c1225b42cf679682667
22piyush <piyushaglawe05@gmail.com>
2026-09-26 21:35:55 +0530

    Dynamic Routing Start

---
+++ Next-JS/rendering-paradigm/app/page.tsx
@@ -0,0 +1,12 @@
+
+
+const Home =() => {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen py-2">
+      <h1 className="text-4xl font-bold">Welcome to Next.js!</h1>
+      <p className="mt-4 text-lg text-gray-600"></p>
+    </div>
+  )
+}    
+
+export default Home;
